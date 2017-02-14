cf943c03a002f878d79a1bdcc91ed96678bf5185
David Young <david@textflex.com>
2017-02-14 02:11:06 +0000

    checks for gcc, provides more user feedback

--- setup_env.sh
+++ setup_env.sh
@@ -7,20 +7,33 @@ echo $PWD
 
 if [ "`command -v jar`" == '' ]
 then
-	echo "Please add Java SDK directory to path. Exiting..."
+	echo "Please add JAVA_HOME and update path environment variables. Exiting..."
+	exit 0
+fi
+
+if [ "`command -v gcc`" == '' ]
+then
+	echo "Please install gcc. Exiting..."
 	exit 0
 fi
 
 if [ "`command -v conda`" == '' ]
 then
+	echo "Downloading and installing Miniconda..."
 	wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 	sh Miniconda3-latest-Linux-x86_64.sh
 	source ~/.bashrc
 fi
+
+echo "Creating new conda environment..."
 conda create --name clr01 python=3 pyqt=4
 source activate clr01
+
+echo "Conda installing Mayavi and Scikit-image..."
 conda install -c menpo mayavi
 conda install scikit-image
+
+echo "Pip installing Python-bioformats and exchanged Javabridge for GitHub version..."
 pip install python-bioformats
 pip uninstall javabridge
 conda install cython
@@ -29,6 +42,7 @@ cd python-javabridge
 pip install -e .
 cd ..
 
+echo "Exchanging Scikit-image for GitHub version with 3D blob pull request..."
 conda remove scikit-image
 git clone https://github.com/scikit-image/scikit-image.git
 cd scikit-image
@@ -37,4 +51,6 @@ git checkout blob3d
 git pull origin pull/2114/head
 pip install -e .
 cd ..
+
+echo "clrbrain environment setup complete!"
 

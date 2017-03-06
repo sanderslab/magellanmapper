6283c4ce3ba686e158b92339ced25111262df72e
David Young <david@textflex.com>
2017-03-06 12:43:08 -0800

    refactored close blob detection to detector; fixed chunking test for max_pixels_factor parameter

--- clrbrain/detector.py
+++ clrbrain/detector.py
@@ -91,3 +91,100 @@ def segment_blob(roi):
     confirmed = np.ones((blobs_log.shape[0], 1)) * -1
     blobs_log = np.concatenate((blobs_log, confirmed), axis=1)
     return blobs_log
+
+def remove_duplicate_blobs(blobs, region):
+    """Removes duplicate blobs.
+    
+    Params:
+        blobs: The blobs, given as 2D array of [n, [z, row, column, radius]].
+        region: Slice within each blob to check, such as slice(0, 2) to check
+           for (z, row, column).
+    
+    Return:
+        The blobs array with only unique elements.
+    """
+    # workaround while awaiting https://github.com/numpy/numpy/pull/7742
+    # to become a reality, presumably in Numpy 1.13
+    blobs_region = blobs[:, region]
+    blobs_contig = np.ascontiguousarray(blobs_region)
+    blobs_type = np.dtype((np.void, blobs_region.dtype.itemsize * blobs_region.shape[1]))
+    blobs_contig = blobs_contig.view(blobs_type)
+    _, unique_indices = np.unique(blobs_contig, return_index=True)
+    print("removed {} duplicate blobs".format(blobs.shape[0] - unique_indices.size))
+    return blobs[unique_indices]
+
+def remove_close_blobs(blobs, blobs_master, region, tol):
+    """Removes blobs that are close to one another.
+    
+    Params:
+        blobs: The blobs to add, given as 2D array of [n, [z, row, column, 
+            radius]].
+        blobs_master: The list by which to check for close blobs, in the same
+            format as blobs.
+        region: Slice within each blob to check, such as slice(0, 2) to check
+            for (z, row, column).
+        tol: Tolerance to check for closeness, given in the same format
+            as region. Blobs that are equal to or less than the the absolute
+            difference for all corresponding parameters will be pruned in
+            the returned array.
+    
+    Return:
+        The blobs array without blobs falling inside the tolerance range.
+    """
+    blobs_diffs = np.abs(blobs_master[:, region][:, None] - blobs[:, region])
+    close_master, close = np.nonzero((blobs_diffs <= tol).all(2))
+    pruned = np.delete(blobs, close, axis=0)
+    print("removed {} close blobs".format(blobs.shape[0] - pruned.shape[0]))
+    return pruned
+
+def remove_close_blobs_within_array(blobs, region, tol):
+    """Removes close blobs within a given array.
+    
+    Uses remove_close_blobs() to detect blobs close to one another inside
+    the master array.
+    
+    Params:
+        blobs: The blobs to add, given as 2D array of [n, [z, row, column, 
+            radius]].
+        region: Slice within each blob to check, such as slice(0, 2) to check
+            for (z, row, column).
+        tol: Tolerance to check for closeness, given in the same format
+            as region. Blobs that are equal to or less than the the absolute
+            difference for all corresponding parameters will be pruned in
+            the returned array.
+    
+    Return:
+        The blobs array without blobs falling inside the tolerance range.
+    """
+    if blobs is None:
+        return
+    blobs_all = None
+    for blob in blobs:
+        #print("blob: {}".format(blob))
+        if blobs_all is None:
+            blobs_all = np.array([blob])
+        else:
+            blobs_to_add = remove_close_blobs(np.array([blob]), blobs_all, region, tol)
+            blobs_all = np.concatenate((blobs_all, blobs_to_add))
+    return blobs_all
+
+if __name__ == "__main__":
+    print("Detector tests...")
+    
+    # tests blob duplication removal
+    blobs = np.array([[1, 3, 4, 2.2342], [1, 8, 5, 3.13452], [1, 3, 4, 5.1234],
+                      [1, 3, 5, 2.2342], [3, 8, 5, 3.13452]])
+    print("sample blobs:\n{}".format(blobs))
+    end = 3
+    blobs_unique = remove_duplicate_blobs(blobs, slice(0, end))
+    print("blobs_unique through first {} elements:\n{}".format(end, blobs_unique))
+    
+    # tests removal of blobs within a given tolerance level
+    tol = (1, 2, 2)
+    blobs = remove_close_blobs_within_array(blobs, slice(0, end), tol)
+    print("pruned sample blobs within tolerance {}:\n{}".format(tol, blobs))
+    blobs_to_add = np.array([[1, 3, 5, 2.2342], [2, 10, 5, 3.13452], 
+                             [2, 2, 4, 5.1234], [3, 3, 5, 2.2342]])
+    print("blobs to add:\n{}".format(blobs_to_add))
+    blobs_to_add = remove_close_blobs(blobs_to_add, blobs, slice(0, end), tol)
+    print("pruned blobs to add:\n{}".format(blobs_to_add))

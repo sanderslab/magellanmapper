436db2b7a5652d4c06dcf93b9cdc2859e19df79d
David Young <david@textflex.com>
2017-05-11 01:19:07 +0000

    changed bash argument parsing to getopts for simplification

--- process_aws.sh
+++ process_aws.sh
@@ -42,42 +42,28 @@ BASE_DIR="`dirname $0`"
 cd "$BASE_DIR"
 echo $PWD
 
-if [ $# -gt 0 ]
-then
-	echo "Parsing user arguments..."
-	for arg in "$@"
-	do
-		# reads arguments
-		if [ "x$arg" = "x--help" -o "x$arg" = "x-h" ] # help docs
-		then
-			if [ "`command -v more`" != '' ]
-			then
-				echo "$HELP" | more
-			elif [ "`command -v less`" != "" ]
-			then
-				echo "$HELP" | less
-			else
-				echo "$HELP"
-			fi
-			exit 0
-			
-		# image filename
-		elif [ ${arg:0:${#PAR_IMGNAME}} = "$PAR_IMGNAME" ]
-		then
-			IMG="${arg#${PAR_IMGNAME}=}"
-			echo "...set to use \"$IMG\" as the image filename"
-		# S3 path
-		elif [ ${arg:0:${#PAR_S3}} = "$PAR_S3" ]
-		then
-			S3_DIR="${arg#${PAR_S3}=}"
-			echo "...set to use \"$S3_DIR\" as the S3 directory path"
-		# extra arguments
-		else
-			EXTRA_ARGS+=" $arg"
-			echo "...adding \"$arg\" to clrbrain arguments"
-		fi
-	done
-fi
+OPTIND=1
+while getopts hf:s: opt; do
+    case $opt in
+        h)  echo $HELP
+            exit 0
+            ;;
+        f)  IMG="$OPTARG"
+            echo "Set image file to $IMG"
+            ;;
+        s)  S3="$OPTARG"
+            echo "Set AWS S3 base path to $S3"
+            ;;
+        :)  echo "Option -$OPTARG requires an argument"
+            exit 1
+            ;;
+        --) ;;
+    esac
+done
+
+# pass arguments after "--" to clrbrain
+shift "$((OPTIND-1))"
+EXTRA_ARGS="$@"
 
 FOUND_NPZ=0
 IMG_BASE=${IMG/.czi/_}$(printf %05d $SERIES)
@@ -85,35 +71,35 @@ NPZ_IMG=$IMG_BASE"_image5d.npz"
 NPZ_INFO=$IMG_BASE"_info.npz"
 for NPZ in $NPZ_IMG $NPZ_INFO
 do
-	if [ -e "$DEST"/"$NPZ" ]
-	then
-		echo "Found $DEST/$NPZ"
-		FOUND_NPZ=1
-	else
-		echo "Could not find $DEST/$NPZ, checking on s3..."
-		NPZ_LS=`aws s3 ls s3://"$S3_DIR"/"$NPZ"`
-		if [ "$NPZ_LS" != "" ]
-		then
-			aws s3 cp s3://"$S3_DIR"/"$NPZ" "$DEST"/"$NPZ"
-			ls -lh "$DEST"/"$NPZ"
-			FOUND_NPZ=1
-		else
-			echo "Could not find $DEST/$NPZ on s3, checking original image..."
-			if [ -e "$DEST"/"$IMG" ]
-			then
-				echo "Found $DEST/$IMG"
-			else
-				aws s3 cp s3://"$S3_DIR"/"$IMG" "$DEST"/"$IMG"
-				ls -lh "$DEST"/"$IMG"
-			fi
-		fi
-	fi
+    if [ -e "$DEST"/"$NPZ" ]
+    then
+        echo "Found $DEST/$NPZ"
+        FOUND_NPZ=1
+    else
+        echo "Could not find $DEST/$NPZ, checking on s3..."
+        NPZ_LS=`aws s3 ls s3://"$S3_DIR"/"$NPZ"`
+        if [ "$NPZ_LS" != "" ]
+        then
+            aws s3 cp s3://"$S3_DIR"/"$NPZ" "$DEST"/"$NPZ"
+            ls -lh "$DEST"/"$NPZ"
+            FOUND_NPZ=1
+        else
+            echo "Could not find $DEST/$NPZ on s3, checking original image..."
+            if [ -e "$DEST"/"$IMG" ]
+            then
+                echo "Found $DEST/$IMG"
+            else
+                aws s3 cp s3://"$S3_DIR"/"$IMG" "$DEST"/"$IMG"
+                ls -lh "$DEST"/"$IMG"
+            fi
+        fi
+    fi
 done
 
 # import raw image into Numpy array if not available
 if (( $FOUND_NPZ == 0)); then
-	echo "Importing $DEST/$IMG..."
-	python -m clrbrain.cli --img "$DEST"/"$IMG" --proc importonly $EXTRA_ARGS
+    echo "Importing $DEST/$IMG..."
+    python -m clrbrain.cli --img "$DEST"/"$IMG" --proc importonly $EXTRA_ARGS
 fi
 
 # process image and segments
@@ -124,6 +110,6 @@ NPZ_IMG_PROC=$IMG_BASE"_image5d_proc.npz"
 NPZ_INFO_PROC=$IMG_BASE"_info_proc.npz"
 for NPZ in $NPZ_IMG_PROC $NPZ_INFO_PROC
 do
-	aws s3 cp "$DEST"/"$NPZ" s3://"$S3_DIR"/"$NPZ"
+    aws s3 cp "$DEST"/"$NPZ" s3://"$S3_DIR"/"$NPZ"
 done
 

ee2f5a95daf67a4a18ecd42c0429b71a4cab3e5a
David Young <david@textflex.com>
2017-08-24 14:29:39 -0700

    New dependency: ImageMagick Fits output figure size to image dimensions; removes unnecessary image stack file saving; moves animation output path to parent of stack directory; saves animation (requires ImageMagick)

--- clrbrain/stack.py
+++ clrbrain/stack.py
@@ -23,31 +23,36 @@ def animated_gif(path):
     parent_path = os.path.dirname(files[0])
     name = os.path.dirname(files[0])
     tmp_path = os.path.join(parent_path, name + "_tmp.npy")
-    image3d = None
-    fig = plt.figure()
+    fig = plt.figure(frameon=False)
     ax = fig.add_subplot(111)
-    ax.set_axis_off()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plotted_imgs = []
+    img_size = None
     i = 0
     for f in files:
         print("importing {}".format(f))
         img = io.imread(f)
         img = transform.rescale(img, 0.2, mode="reflect", multichannel=False)
-        image3d = np.lib.format.open_memmap(
-            tmp_path, mode="w+", dtype=img.dtype, 
-            shape=(1, len(files), *img.shape))
-        image3d[i] = img
+        if img_size is None:
+            img_size = img.shape
         plotted_imgs.append([ax.imshow(img)])
-        ax.set_title("")
+        #ax.set_title("")
     
     # export to animated GIF
-    out_path = os.path.join(parent_path, "_animation.gif")
+    out_path = os.path.join(path, name + "_animation.gif")
     print(len(plotted_imgs))
+    plt.tight_layout(pad=0.0)
+    fig_size = fig.get_size_inches()
+    fig.set_size_inches(img_size[1] / fig.dpi, img_size[0] / fig.dpi)
     anim = animation.ArtistAnimation(fig, plotted_imgs, interval=100, repeat_delay=0, blit=False)
-    #anim.save(out_path)
+    try:
+        anim.save(out_path, writer="imagemagick")
+    except ValueError as e:
+        print(e)
+        print("No animation writer available for Matplotlib")
     print("saved animation file to {}".format(out_path))
     plt.show()
-    return image3d
 
 if __name__ == "__main__":
     print("Clrbrain stack manipulations")

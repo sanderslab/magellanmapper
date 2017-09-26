Fix xz orientation to use aspect ratio based on scaling and flipped image
Previously the aspect ratio for the overview image for xz orientation was based the image size, which did not depend on image scaling but rather the number of sections imaged, even though this aspect ratio had worked perhaps better for previous images. This fix uses scaling instead. The overview image is now also flipped when in xz orientation since our microscopy has typically imaged the ventral surface of the brain first. We may eventually want to flip all overview images or create a separate option.

clrbrain/plot_2d.py
@@ -303,11 +303,13 @@ def plot_2d_stack(vis, title, filename, image5d, channel, roi_size, offset, segm
     print("z_overview: {}".format(z_overview))
     
     # pick image based on chosen orientation 
+    origin = None
     if plane == "xz":
         # TODO: base on scaling vs image shape vs nothing?
-        #aspect = detector.resolutions[0, 0] / detector.resolutions[0, 2]
-        aspect = float(image5d.shape[3]) / image5d.shape[1]
+        aspect = detector.resolutions[0, 0] / detector.resolutions[0, 2]
+        #aspect = float(image5d.shape[3]) / image5d.shape[1]
         print(aspect)
+        origin = "lower"
         segments[:, [0, 1]] = segments[:, [1, 0]]
         if image5d.ndim >= 5:
             img2d = image5d[0, :, z_overview, :, channel]
@@ -368,7 +370,7 @@ def plot_2d_stack(vis, title, filename, image5d, channel, roi_size, offset, segm
             print(img2d_zoom.shape, origin, size)
             patch_offset = np.subtract(patch_offset, origin)
         # show the zoomed 2D image along with rectangle showing ROI
-        ax.imshow(img2d_zoom, cmap=colormap_2d, aspect=aspect)
+        ax.imshow(img2d_zoom, cmap=colormap_2d, aspect=aspect, origin=origin)
         ax.add_patch(patches.Rectangle(patch_offset, roi_size[0], roi_size[1], 
                                        fill=False, edgecolor="yellow"))
         add_scale_bar(ax)
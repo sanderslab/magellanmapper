Fix rotation of animation figure exports
Change z to become the "y" axis in figures and adjust figure size for aspect ratio depending on largest side.

clrbrain/plot_2d.py
@@ -660,20 +660,20 @@ def extract_plane(image5d, plane_n, plane=None, channel=0, savefig=None,
         origin = "lower"
         img2d = img3d[:, plane_n, :]
         if img2d.shape[1] > 1:
-            # stack of 2D plots such as for animation; make z the last axis
-            img2d = np.moveaxis(img2d, 0, -1)
-            #img2d = np.swapaxes(img2d, 0, 1)
-            aspect = 1 / aspect
+            # stack of 2D plots such as for animation; make z the "y" axis
+            #img2d = np.moveaxis(img2d, 0, -1)
+            img2d = np.swapaxes(img2d, 0, 1)
+            #aspect = 1 / aspect
     elif plane == PLANE[2]:
         # yz plane
         aspect = detector.resolutions[0, 0] / detector.resolutions[0, 1]
         origin = "lower"
         img2d = img3d[:, :, plane_n]
         if img2d.ndim > 2 and img2d.shape[2] > 1:
-            # stack of 2D plots such as for animation; make z the last axis
-            #img2d = np.moveaxis(img2d, -1, 0)
-            img2d = np.swapaxes(img2d, 0, 2)
-            aspect = 1 / aspect
+            # stack of 2D plots such as for animation; make z the "y" axis
+            img2d = np.moveaxis(img2d, -1, 0)
+            #img2d = np.swapaxes(img2d, 0, 2)
+            #aspect = 1 / aspect
     else:
         # defaults to "xy"
         aspect = detector.resolutions[0, 1] / detector.resolutions[0, 2]

clrbrain/stack.py
@@ -77,7 +77,12 @@ def _build_animated_gif(images, out_path, process_fnc, rescale, aspect=None,
     fig_size = fig.get_size_inches()
     if aspect is None:
         aspect = 1
-    fig.set_size_inches(img_size[1] / fig.dpi / aspect, img_size[0] / fig.dpi)
+    #print("img_size: {}".format(img_size))
+    img_size_dpi = np.divide(img_size, fig.dpi) # convert to inches
+    if img_size[0] < img_size[1]:
+        fig.set_size_inches(img_size_dpi[1], img_size_dpi[0] * aspect)
+    else:
+        fig.set_size_inches(img_size_dpi[1] / aspect, img_size_dpi[0])
     
     # export to animated GIF
     anim = animation.ArtistAnimation(
@@ -125,5 +130,5 @@ def animated_gif(path, series=0, interval=1, rescale=0.1):
 if __name__ == "__main__":
     print("Clrbrain stack manipulations")
     cli.main(True)
-    #animated_gif(cli.filename, 0, 10, 0.05)
-    animated_gif(cli.filename, 0, 1, 1)
+    animated_gif(cli.filename, 0, 1000, 0.05)
+    #animated_gif(cli.filename, 0, 5, 1)
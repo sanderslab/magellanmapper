597642475ee33aa2e14ee235090fd5945512b66d
David Young <david@textflex.com>
2017-10-31 15:46:25 -0700

    Command-line parameters to toggle computing overlap and writing fused file, minimum memory allocation
    - "-c" to compute overlap before stitching files
    - "-w" to write stitched file
    - Ensures a minimum amount of memory allocated

--- stitch.sh
+++ stitch.sh
@@ -5,6 +5,15 @@
 ################################################
 # Stitch files using ImageJ/Fiji plugin.
 #
+# Arguments:
+#   -h: Show help and exit.
+#   -f: Path to image file.
+#   -o: Output directory (DEPRECATED, not used).
+#   -c: Compute overlap and write registered tile configuration. 
+#     Defaults to use coordinates from TileConfiguration.txt 
+#     directly.
+#   -w: Write fused file. Defaults not to write.
+#
 # To run:
 # -Run the stitch/tile_config.py utility to build a positions
 #  configuration file since the .czi file may not contain
@@ -19,8 +28,7 @@
 #  reposition tiles if necessary:
 #   -Edit TileConfiguration.registered.txt with positions from
 #    surrounding tiles
-#   -Copy TileConfiguration.registered.txt to TileConfiguration.txt
-#    (TODO: check if necessary)
+#   -Move TileConfiguration.registered.txt to TileConfiguration.txt
 #   -Kill the current ImageJ process
 #   -Edit stitch/ij_stitch.py to remove "compute_overlap" option
 #   -Re-run this script
@@ -31,8 +39,11 @@ base_dir="`dirname $0`"
 cd "$base_dir"
 echo $PWD
 
+compute_overlap=0
+write_fused=0
+
 OPTIND=1
-while getopts hf:o: opt; do
+while getopts hf:o:cw opt; do
     case $opt in
         h)  echo $HELP
             exit 0
@@ -44,6 +55,12 @@ while getopts hf:o: opt; do
             echo "Set output directory to $OUT_DIR"
             echo "NOTE: This path will be ignored for now"
             echo "and based on image directory instead"
+            ;;
+        c)  compute_overlap=1
+            echo "Set to compute overlap between tiles"
+            ;;
+        w)  write_fused=1
+            echo "Set to fuse and write"
             ;;
         :)  echo "Option -$OPTARG requires an argument"
             exit 1
@@ -85,8 +102,14 @@ echo "Assumes Fiji executable is located at $IJ"
 # extra padding room (TODO: check if too much for large files)
 mem=`du "$IMG" | awk '{print $1}'`
 mem=$((mem/100))
+MIN_MEM=1000
+if ((mem < MIN_MEM))
+then
+    # ensure a minimum amount of RAM
+    mem=$MIN_MEM
+fi
 echo "Reserving $mem MB of memory"
 
 # evaluates the options directly from command-line;
 # does not appear to work when fed a separate script in "-macro" mode
-$IJ --mem "$mem"m --headless --run stitch/ij_stitch.py 'in_file="'"$IMG"'"'
+$IJ --mem "$mem"m --headless --run stitch/ij_stitch.py 'in_file="'"$IMG"'",compute_overlap="'"$compute_overlap"'",write_fused="'"$write_fused"'"'
--- stitch/ij_stitch.py
+++ stitch/ij_stitch.py
@@ -11,21 +11,32 @@ from ij import IJ
 import os
 
 #@String in_file
+#@int compute_overlap
+#@int write_fused
+
 out_dir = os.path.dirname(in_file)
 print("in_file: {}".format(in_file))
 print("out_dir: {}".format(out_dir))
+print("compute_overlap: {}".format(type(compute_overlap)))
+if write_fused == 1:
+    fusion_method = "Linear Blending"
+else:
+    fusion_method = "Do not fuse images (only write TileConfiguration)"
+print("fusion method: {}".format(fusion_method))
+
 options = ("type=[Positions from file] "
            "order=[Defined by TileConfiguration] "
            "multi_series_file=" + in_file + " "
            "output_directory=" + out_dir + " "
-           "fusion_method=[Linear Blending] "
+           "fusion_method=[" + fusion_method + "] "
            "regression_threshold=0.30 "
            "max/avg_displacement_threshold=2.50 "
            "absolute_displacement_threshold=3.50 "
-           "compute_overlap "
            "use_virtual_input_images "
            "computation_parameters=[Save memory (but be slower)] "
-           "image_output=[Write to disk]")
+           "image_output=[Write to disk] ")
+if compute_overlap == 1:
+    options += "compute_overlap "
 
 print(options)
 IJ.run("Grid/Collection stitching", options);

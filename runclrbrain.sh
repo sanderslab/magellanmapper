5bf0a042460f288f86b95f6bab68e54300557118
David Young <david@textflex.com>
2018-01-06 23:13:52 -0800

    Consolidate some parameter assignments in sample scenarios
    - Move stitching scenario naming toward head of script to reduce redundancy/confusion
    - Fix names of TIFF directory/folder
    - Rename stitched TIFF file to its experiment name
    - Add exit code
    - Add microscope parameters to scenarios where the parameters were missed
    - Update modified image name for new, more consistent scheme

--- runclrbrain.sh
+++ runclrbrain.sh
@@ -20,8 +20,14 @@ echo $PWD
 
 # Replace with your paths
 IMG="/path/to/your/image"
-EXT="${IMG##*.}"
 S3_DIR="path/to/your/bucket/artifact"
+
+# Parsing names from your image path
+OUT_DIR="`dirname $IMG`"
+NAME="`basename $IMG`"
+NAME_BASE="${IMG%.*}"
+IMG_PATH_BASE="${OUT_DIR}/${NAME_BASE}"
+EXT="${IMG##*.}"
 
 # Replace microscope type with available profiles, such as "lightsheet_5x", 
 # "2p_20x", or "lightsheet_5x_02"
@@ -35,24 +41,25 @@ OFFSET=50,580,230
 #python -m clrbrain.cli --img "$IMG" --channel 0 --proc importonly
 
 # Load ROI, starting at the given offset and ROI size
-#./run --img "$IMG" --channel 0 --offset $OFFSET --size $SIZE --savefig pdf
+#./run --img "$IMG" --channel 0 --offset $OFFSET --size $SIZE --savefig pdf --microscope "$MICROSCOPE"
 
 # Process an entire stack locally
 #python -m clrbrain.cli --img "$IMG" --proc processing_mp --channel 0 -v
 
-# Process an entire stack on AWS
+# Process an entire stack on AWS (run from within EC2 instance)
 #./process_aws.sh -f "$IMG" -s $S3_DIR --  --microscope "$MICROSCOPE"
 
 # Extract a single z-plane
-#python -m clrbrain.cli --img "$IMG" --proc extract --channel 0 --offset 0,0,0 -v --savefig jpeg
+#python -m clrbrain.cli --img "$IMG" --proc extract --channel 0 --offset 0,0,0 -v --savefig jpeg --microscope "$MICROSCOPE"
 
 # Process a sub-region of the stack and load it
 #python -m clrbrain.cli --img "$IMG" --proc processing_mp --channel 0 -v --offset $OFFSET --size $SIZE --microscope "$MICROSCOPE"
-#./run --img "$IMG($OFFSET)x($SIZE)" -v --channel 0 -v --proc load --offset $OFFSET --size $SIZE --savefig pdf --microscope "$MICROSCOPE"
+IMG_ROI="${IMG_PATH_BASE}_(${OFFSET})x(${SIZE}).${EXT}"
+#./run --img "$IMG_ROI" -v --channel 0 -v --proc load --offset $OFFSET --size $SIZE --savefig pdf --microscope "$MICROSCOPE"
 
 # Transpose an image and generate an animated GIF
 #python -u -m clrbrain.cli --img "$IMG" --proc transpose --rescale 0.05 --plane yz
-IMG_TRANSPOSED="${IMG%.*}_tranposed.${EXT}"
+IMG_TRANSPOSED="${IMG_PATH_BASE}_transposed.${EXT}"
 #python -u -m clrbrain.cli --img "$IMG_TRANSPOSED" --proc animated --interval 5 --rescale 1.0
 
 ####################################
@@ -61,9 +68,6 @@ IMG_TRANSPOSED="${IMG%.*}_tranposed.${EXT}"
 # Replace with your paths
 EXP="experiment_folder_name"
 OUT_DIR="/path/to/output/parent/$EXP"
-NAME_BASE="image_name_without_extension"
-NAME="${NAME_BASE}.czi"
-IMG="${OUT_DIR}/${NAME}"
 
 # Replace with your lens objective settings
 RESOLUTIONS="0.913,0.913,4.935"
@@ -81,7 +85,8 @@ ZOOM="1.0"
 # ALTERNATIVE 1: Stitching plugin (old)
 
 OUT_NAME_BASE="${NAME_BASE}_stitched"
-TIFF_DIR="${OUT_DIR}/${NAME_BASE}"
+TIFF_DIR="${OUT_DIR}/${OUT_NAME_BASE}"
+
 # Replace the tile parameters with your image's setup
 #python -m stitch.tile_config --img "$NAME" --target_dir "$OUT_DIR" --cols 6 --rows 7 --size 1920,1920,1000 --overlap 0.1 --directionality bi --start_direction right
 #./stitch.sh -f "$IMG" -o "$TIFF_DIR" -c
@@ -95,10 +100,15 @@ TIFF_DIR="${OUT_DIR}/${NAME_BASE}"
 # ALTERNATIVE 2: BigStitcher plugin
 
 OUT_NAME_BASE="${NAME_BASE}_bigstitched"
-TIFF_IMG="${OUT_DIR}/${NAME_BASE}.tiff"
+TIFF_IMG="${OUT_DIR}/${OUT_NAME_BASE}.tiff"
+
+# The plugin may not find paths on its initial run; if so, try re-running
 #./stitch.sh -f "$IMG" -b
+#mv "${OUT_DIR}/fused.tif" "$TIFF_IMG"
 #python -u -m clrbrain.cli --img "$TIFF_IMG" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --channel 0 --proc importonly
 
 
 # Upload stitched image to cloud
 #aws s3 cp $OUT_DIR s3://"${S3_DIR}/${EXP}" --recursive --exclude "*" --include *.npz
+
+exit 0

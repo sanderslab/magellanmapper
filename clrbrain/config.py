d00f68d744625266ef47390deb9bf4a364e05248
David Young <david@textflex.com>
2018-04-08 16:00:29 -0700

    Test isotropic z's from 0.1-1.4

--- clrbrain/config.py
+++ clrbrain/config.py
@@ -249,7 +249,7 @@ _scale_factors[:, 0] = _scale_zs
 #print(_scale_factors)
 
 # isotropic factors
-_isotropic_zs = np.arange(0.9, 1.5, 0.1)
+_isotropic_zs = np.arange(0.1, 1.5, 0.1)
 _isotropic_factors = np.ones((len(_isotropic_zs), 3))
 _isotropic_factors[:, 0] = _isotropic_zs
 #print(_isotropic_factors)
@@ -274,8 +274,8 @@ roc_dict = OrderedDict([
         #("scale_factor", _scale_factors),
         #("scale_factor", np.array([(0.6, 1, 1)])),
         #("segmenting_mean_thresh", -5),
-        #("isotropic", _isotropic_factors),
-        ("isotropic", np.array([(0.96, 1, 1)])),
+        ("isotropic", _isotropic_factors),
+        #("isotropic", np.array([(0.96, 1, 1)])),
         #("overlap", np.arange(0.1, 1.0, 0.1)),
         #("prune_tol_factor", np.array([(4, 1.3, 1.3)])),
         #("prune_tol_factor", _prune_tol_factors),

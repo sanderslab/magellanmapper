sanderslab/magellanmapper

commit 6c304e71eeb72c8c134ea113e827761d8c83866f

Load processed multichannel array of blobs
- Load processed blobs by channel or multichannel
- Generalize blob formatter for various input shapes
- Default channel to 0 for processed blob arrays that don't have channel column

clrbrain/visualizer.py
@@ -520,8 +520,10 @@ class Visualization(HasTraits):
                 # get all previously processed blobs in ROI plus additional 
                 # padding region to show surrounding blobs
                 segs_all, _ = detector.get_blobs_in_roi(
-                    cli.segments_proc, offset, 
-                    roi_size, plot_2d.padding)
+                    cli.segments_proc, offset, roi_size, plot_2d.padding)
+                segs_all = detector.format_blobs(segs_all)
+                segs_all = detector.blobs_in_channel(
+                    segs_all, config.channel)
                 # shift coordinates to be relative to offset
                 segs_all[:, :3] = np.subtract(segs_all[:, :3], offset[::-1])
             print("segs_all:\n{}".format(segs_all))
@@ -529,6 +531,7 @@ class Visualization(HasTraits):
             if not self._is_segs_none(segs):
                 # if segs provided (eg from ROI), use only these segs within 
                 # the ROI and add segs from the padding area outside the ROI
+                segs = detector.format_blobs(segs)
                 _, segs_in_mask = detector.get_blobs_in_roi(
                     segs_all, np.zeros(3), 
                     roi_size, np.multiply(self.border, -1))

clrbrain/detector.py
@@ -147,7 +147,7 @@ def detect_blobs(roi, channel):
             "time for 3D blob detection: {}".format(time() - time_start))
         if blobs_log.size < 1:
             print("no blobs detected")
-            return None
+            continue
         blobs_log[:, 3] = blobs_log[:, 3] * math.sqrt(3)
         blobs = format_blobs(blobs_log, i)
         #print(blobs)
@@ -155,27 +155,41 @@ def detect_blobs(roi, channel):
     blobs_all = np.vstack(blobs_all)
     return blobs_all
 
-def format_blobs(blobs, channel):
+def format_blobs(blobs, channel=None):
     """Format blobs with additional fields for confirmation, truth, and 
     channel, abs z, abs y, abs x values.
     
-    Blobs can be assumed to start with (z, y, x, radius) but should use 
-    ``detector`` functions to manipulate other fields of blob arrays to 
+    Blobs in Clrbrain can be assumed to start with (z, y, x, radius) but should 
+    use ``detector`` functions to manipulate other fields of blob arrays to 
     ensure that the correct columns are accessed.
     
     Args:
-        blobs: Numpy 2D array in [[z, y, x, radius], ...] format.
-        channel: Channel to set.
+        blobs: Numpy 2D array in [[z, y, x, radius, ...], ...] format.
+        channel: Channel to set. Defaults to None, in which case the channel 
+            will not be updated.
     
     Returns:
         Blobs array formatted as 
         [[z, y, x, radius, confirmation, truth, channel, 
           abs_z, abs_y, abs_x], ...].
     """
-    extras = np.ones((blobs.shape[0], 6)) * -1
-    extras[:, 2] = channel
-    extras[:, 3:] = blobs[:, :3]
+    # target num of cols minus current cols
+    shape = blobs.shape
+    extra_cols = 10 - shape[1]
+    #print("extra_cols: {}".format(extra_cols))
+    extras = np.ones((shape[0], extra_cols)) * -1
+    print(blobs)
     blobs = np.concatenate((blobs, extras), axis=1)
+    # copy relative coords to abs coords
+    blobs[:, -3:] = blobs[:, :3]
+    channel_dim = 6
+    if channel is not None:
+        # update channel if given
+        blobs[:, channel_dim] = channel
+    elif shape[1] <= channel_dim:
+        # if original shape of each blob was 6 or less as was the case 
+        # prior to v.0.6.0, need to update channel with default value
+        blobs[:, channel_dim] = 0
     return blobs
 
 def remove_abs_blob_coords(blobs):
@@ -198,6 +212,8 @@ def replace_rel_with_abs_blob_coords(blobs):
     return blobs
 
 def blobs_in_channel(blobs, channel):
+    if channel is None:
+        return blobs
     return blobs[blobs[:, 6] == channel]
 
 def remove_duplicate_blobs(blobs, region):

commit 53c7655250df5714b0923b4d84d3d226c76d39bf

Fix regression in detected segments' absolute coordinates
Commit 6c304e7 introduced a regression where detected blobs were formatted before shifting relative coordinates based on the offset, fixed here.

clrbrain/visualizer.py
@@ -585,11 +585,11 @@ class Visualization(HasTraits):
                 # padding region to show surrounding blobs
                 segs_all, _ = detector.get_blobs_in_roi(
                     cli.segments_proc, offset, roi_size, plot_2d.padding)
+                # shift coordinates to be relative to offset
+                segs_all[:, :3] = np.subtract(segs_all[:, :3], offset[::-1])
                 segs_all = detector.format_blobs(segs_all)
                 segs_all = detector.blobs_in_channel(
                     segs_all, config.channel)
-                # shift coordinates to be relative to offset
-                segs_all[:, :3] = np.subtract(segs_all[:, :3], offset[::-1])
             print("segs_all:\n{}".format(segs_all))
             
             if not self._is_segs_none(segs):
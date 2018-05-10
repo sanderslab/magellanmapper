a7af5fc310967dc0c0b4e2e8bd0d7b7ad95231bc
David Young <david@textflex.com>
2018-05-10 09:22:01 -0700

    Fill sample run script variables programatically
    Fill image and S3 directory variables in copy of sample run script by command-line arguments to facilitate scripting.

---
+++ setup_runclrbrain.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Prepare run script based on sample script
+# Author: David Young 2018
+
+HELP="
+Replace defaults in sample script with given arguments.
+
+Arguments:
+    -h: Show help and exit.
+    -s: Path with which to replace S3_DIR.
+    -i: Path with which to replace IMG.
+    -a: Abbreviation of image, used to name the output 
+        script.
+"
+
+RUN_SCRIPT="runclrbrain.sh"
+s3_dir=""
+img=""
+abbr=""
+
+OPTIND=1
+while getopts hs:i:a: opt; do
+    case $opt in
+        h)  echo $HELP
+            exit 0
+            ;;
+        s)  s3_dir="$OPTARG"
+            echo "Set S3 directory to $s3_dir"
+            ;;
+        i)  img="$OPTARG"
+            echo "Set image path to $img"
+            ;;
+        a)  abbr="$OPTARG"
+            echo "Set image abbreviation to $abbr"
+            ;;
+        --) ;;
+    esac
+done
+
+# pass arguments after "--" to another script if necessary
+shift "$((OPTIND-1))"
+EXTRA_ARGS="$@"
+
+# appendn abbreviation to run script filename before ext
+output="${RUN_SCRIPT%.*}_${abbr}.sh"
+
+# replace S3_DIR and IMG variable assignments
+sed -e "s:IMG=.*:IMG=\"${img}\":" \
+    -e "s:S3_DIR=.*:S3_DIR=\"${s3_dir}\":" \
+    "$RUN_SCRIPT" > "$output"
+chmod 755 "$output"
+
+echo "Updated run script, saved at $output"
+
+exit 0

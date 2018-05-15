9cb8abdb4a2b3b5ffe73e5987a0483c2a6946376
David Young <david@textflex.com>
2018-05-14 19:27:45 -0700

    Unit testing for processing whole image stack using "lightsheet_v02.2" process settings
    - Return blobs from process_file
    - Unit testing to check number of blobs found for test image with small ROI (commented out for whole image)

--- clrbrain/unit_testing.py
+++ clrbrain/unit_testing.py
@@ -9,6 +9,7 @@ import unittest
 from clrbrain import cli
 from clrbrain import config
 from clrbrain import importer
+from clrbrain import lib_clrbrain
 
 TEST_IMG = "test.czi"
 
@@ -18,6 +19,9 @@ class TestImageStackProcessing(unittest.TestCase):
         config.filename = TEST_IMG
         config.series = 0
         config.channel = None
+        config.update_process_settings(
+            config.process_settings, "lightsheet_v02.2")
+        cli.proc_type = cli.PROC_TYPES[2]
     
     def test_load_image(self):
         image5d = importer.read_file(
@@ -25,9 +29,13 @@ class TestImageStackProcessing(unittest.TestCase):
         self.assertEqual(image5d.shape, (1, 51, 200, 200, 2))
     
     def test_process_whole_image(self):
-        # TODO: simply checking for exceptions now; need to get segments
-        cli.main()
-        self.assertEqual(True, True)
+        ext = lib_clrbrain.get_filename_ext(config.filename)
+        filename_base = importer.filename_to_base(
+            config.filename, config.series, ext=ext)
+        #stats, fdbk, blobs = cli.process_file(filename_base, None, None)
+        #self.assertEqual(len(blobs), 4766)
+        _, _, blobs = cli.process_file(filename_base, (30, 30, 8), (70, 70, 10))
+        self.assertEqual(len(blobs), 195)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)

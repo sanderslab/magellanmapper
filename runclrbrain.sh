f51771e7f2167cda926daf2772f3af2c5341a17a
David Young <david@textflex.com>
2018-05-21 13:46:09 -0700

    Reorganize sample run script so that it can run through a full pipeline
    The sample run script has served as a template from which users can turn on various parts of the processing pipeline. This update reorganizes the script so that more commands can be turned on at once to allow a full processing pipeline.
    
    - Shift whole image processing to occur after the stitching
    - Allow selection of a specific stitching alternative
    - Update transposed file name for new plane and scale format
    - Clarify some settings for the user to change manually

--- runclrbrain.sh
+++ runclrbrain.sh
@@ -11,8 +11,9 @@
 ################################################
 
 
+
 ####################################
-# REPLACE WITH YOUR PATHS
+# REPLACE WITH SETTINGS FOR YOUR IMAGE
 
 # full path to original image, assumed to be within a directory 
 # that names the experiment and also used for S3; eg 
@@ -24,23 +25,6 @@ IMG="/path/to/your/image"
 # $S3_DIR/exp_yyyy-mm-dd
 S3_DIR="path/to/your/bucket/artifact"
 
-
-####################################
-# Basic Usage
-
-# Parsing names from your image path
-OUT_DIR="`dirname $IMG`"
-EXP="`basename $OUT_DIR`"
-NAME="`basename $IMG`"
-NAME_BASE="${NAME%.*}"
-IMG_PATH_BASE="${OUT_DIR}/${NAME_BASE}"
-EXT="${IMG##*.}"
-
-# run from script directory
-BASE_DIR="`dirname $0`"
-cd "$BASE_DIR"
-echo $PWD
-
 # Replace microscope type with available profiles, such as "lightsheet", 
 # "2p_20x", or "lightsheet_v02", or with modifiers, such as 
 # "lightsheet_contrast" or "lightsheet_contrast_cytoplasm". Multiple 
@@ -48,6 +32,28 @@ echo $PWD
 # "lightsheet lightsheet_cytoplasm" for a nuclear marker in channel 0 
 # and cytoplasmic marker in channel 1
 MICROSCOPE="lightsheet"
+
+
+
+####################################
+# Script setup
+
+# Parsing names from your image path
+OUT_DIR="`dirname $IMG`"
+EXP="`basename $OUT_DIR`"
+NAME="`basename $IMG`"
+IMG_PATH_BASE="${OUT_DIR}/${NAME%.*}"
+EXT="${IMG##*.}"
+
+# run from script's directory
+BASE_DIR="`dirname $0`"
+cd "$BASE_DIR"
+echo $PWD
+
+
+
+####################################
+# Basic Usage
 
 # Replace region of interest (ROI) size and offset
 SIZE=700,90,50
@@ -67,22 +73,6 @@ OFFSET=50,580,230
 IMG_ROI="${IMG_PATH_BASE}_(${OFFSET})x(${SIZE}).${EXT}"
 #./run --img "$IMG_ROI" -v --channel 0 -v --proc load --offset $OFFSET --size $SIZE --savefig pdf --microscope "$MICROSCOPE"
 
-# Transpose an image and generate an animated GIF
-#python -u -m clrbrain.cli --img "$IMG" --proc transpose --rescale 0.05 --plane yz
-IMG_TRANSPOSED="${IMG_PATH_BASE}_transposed.${EXT}"
-#python -u -m clrbrain.cli --img "$IMG_TRANSPOSED" --proc animated --interval 5 --rescale 1.0
-
-
-####################################
-# Whole Image Processing Workflow
-
-# Process an entire image locally on 1st channel, chunked into multiple 
-# smaller stacks to minimize RAM usage and multiprocessed for efficiency
-#python -u -m clrbrain.cli --img "$IMG" --proc processing_mp --channel 0 --microscope "$MICROSCOPE"
-
-# Similar processing but integrated with S3 access from AWS (run from 
-# within EC2 instance)
-#./process_aws.sh -f "$IMG" -s $S3_DIR --  --microscope "$MICROSCOPE" --channel 0
 
 
 ####################################
@@ -92,50 +82,98 @@ IMG_TRANSPOSED="${IMG_PATH_BASE}_transposed.${EXT}"
 RESOLUTIONS="0.913,0.913,4.935"
 MAGNIFICATION="5.0"
 ZOOM="1.0"
+# Choose "stitching" or "bigstitcher" for alternative stitching plugin pathways
+stitch_pathway="bigstitcher"
 
-
-# Get large, unstitched image file from cloud; image should be fused for 
-# Alternative 1 and unfused for 2
-#echo "Stitching $IMG to $OUT_DIR with resolutions $RESOLUTIONS, magnification $MAGNIFICATION, and zoom $ZOOM"
+# Get large, unstitched image file from cloud, where the fused (all 
+# illuminators merged) image is used for the Stitching pathway, and 
+# the unfused, original image is used for the BigStitcher pathway
 #mkdir $OUT_DIR
 #aws s3 cp s3://"${S3_DIR}/${EXP}/${NAME}" $OUT_DIR
 
+out_name_base=""
+clr_img=""
+if [[ "$stitch_pathway$" == "stitching" ]]; then
+    # ALTERNATIVE 1: Stitching plugin (old)
+    
+    OUT_NAME_BASE="${NAME_BASE}_stitched"
+    TIFF_DIR="${OUT_DIR}/${OUT_NAME_BASE}"
+    
+    # Replace the tile parameters with your image's setup; set up tile 
+    # configuration manually and compute alignment refinement
+    #python -m stitch.tile_config --img "$NAME" --target_dir "$OUT_DIR" --cols 6 --rows 7 --size 1920,1920,1000 --overlap 0.1 --directionality bi --start_direction right
+    #./stitch.sh -f "$IMG" -o "$TIFF_DIR" -w 0
+    
+    # Before the next steps, please manually check alignments to ensure that they 
+    # fit properly, especially since unregistered tiles may be shifted to (0, 0, 0)
+    #./stitch.sh -f "$IMG" -o "$TIFF_DIR" -w 1
+    #python -u -m clrbrain.cli --img "$TIFF_DIR" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --channel 0 --proc importonly
+    clr_img="${OUT_DIR}/${OUT_NAME_BASE}.${EXT}"
+    
+elif [[ "$stitch_pathway$" == "bigstitcher" ]]; then
+    # ALTERNATIVE 2: BigStitcher plugin
+    
+    OUT_NAME_BASE="${NAME_BASE}_bigstitched"
+    TIFF_IMG="${OUT_DIR}/${OUT_NAME_BASE}.tiff"
+    
+    # Import file into BigStitcher HDF5 format (warning: large file, just 
+    # under size of original file) and find alignments
+    #./stitch.sh -f "$IMG" -b -w 0
+    
+    # Before writing stitched file, advise checking alignments; when 
+    # satisfied, then run this fusion step
+    #./stitch.sh -f "$IMG" -b -w 1
+    
+    # Rename output file(s):
+    FUSED="fused_tp_0"
+    #for f in ${OUT_DIR}/${FUSED}*.tif; do mv $f ${f/$FUSED/$OUT_NAME_BASE}; done
+    
+    # Import stacked TIFF file(s) into Numpy arrays for Clrbrain
+    #python -u -m clrbrain.cli --img "$TIFF_IMG" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --proc importonly
+    clr_img="${OUT_DIR}/${OUT_NAME_BASE}.${EXT}"
+fi
 
-# ALTERNATIVE 1: Stitching plugin (old)
-
-OUT_NAME_BASE="${NAME_BASE}_stitched"
-TIFF_DIR="${OUT_DIR}/${OUT_NAME_BASE}"
-
-# Replace the tile parameters with your image's setup; set up tile 
-# configuration manually and compute alignment refinement
-#python -m stitch.tile_config --img "$NAME" --target_dir "$OUT_DIR" --cols 6 --rows 7 --size 1920,1920,1000 --overlap 0.1 --directionality bi --start_direction right
-#./stitch.sh -f "$IMG" -o "$TIFF_DIR" -w 0
-
-# Before the next steps, please manually check alignments to ensure that they 
-# fit properly, especially since unregistered tiles may be shifted to (0, 0, 0)
-#./stitch.sh -f "$IMG" -o "$TIFF_DIR" -w 1
-#python -u -m clrbrain.cli --img "$TIFF_DIR" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --channel 0 --proc importonly
+# At this point, you can delete the TIFF image since it has been exported into a Numpy-based 
+# format for loading into Clrbrain
 
 
-# ALTERNATIVE 2: BigStitcher plugin
+####################################
+# Transpose/Resize Image Workflow
 
-OUT_NAME_BASE="${NAME_BASE}_bigstitched"
-TIFF_IMG="${OUT_DIR}/${OUT_NAME_BASE}.tiff"
+# Replace with your rescaling factor here
+scale=0.05
+plane="yx"
 
-# Import file into BigStitcher HDF5 format (warning: large file, just 
-# under size of original file) and find alignments
-#./stitch.sh -f "$IMG" -b -w 0
+clr_img_base="${clr_img%.*}"
 
-# Before writing stitched file, advise checking alignments; when 
-# satisfied, then run this fusion step
-#./stitch.sh -f "$IMG" -b -w 1
+# Rescale an image to downsample by the scale factor
+#python -u -m clrbrain.cli --img "$clr_img" --proc transpose --rescale ${scale}
+img_transposed="${clr_img_base}_scale${scale}.${EXT}"
 
-# Rename output file(s):
-FUSED="fused_tp_0"
-#for f in ${OUT_DIR}/${FUSED}*.tif; do mv $f ${f/$FUSED/$OUT_NAME_BASE}; done
+# Both rescale and transpose an image from z-axis (xy plane) to x-axis (yz plane) orientation
+#python -u -m clrbrain.cli --img "$clr_img" --proc transpose --rescale ${scale} --plane "$plane"
+#img_transposed="${clr_img_base}_plane${plane}_scale${scale}.${EXT}"
 
-# Import stacked TIFF file(s) into Numpy arrays for Clrbrain
-#python -u -m clrbrain.cli --img "$TIFF_IMG" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --proc importonly
+scale=1.0
+
+# Export transposed image to an animated GIF (requires ImageMagick)
+#python -u -m clrbrain.cli --img "$img_transposed" --proc animated --interval 5 --rescale ${scale}
+
+# Alternatively, export to an MP4 video
+#python -u -m clrbrain.cli --img "$img_transposed" --proc animated --interval 5 --rescale ${scale} --savefig "mp4"
+
+
+
+####################################
+# Whole Image Processing Workflow
+
+# Process an entire image locally on 1st channel, chunked into multiple 
+# smaller stacks to minimize RAM usage and multiprocessed for efficiency
+#python -u -m clrbrain.cli --img "$clr_img" --proc processing_mp --channel 0 --microscope "$MICROSCOPE"
+
+# Similar processing but integrated with S3 access from AWS (run from 
+# within EC2 instance)
+#./process_aws.sh -f "$clr_img" -s $S3_DIR --  --microscope "$MICROSCOPE" --channel 0
 
 
 # Upload stitched image to cloud

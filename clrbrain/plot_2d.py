sanderslab/magellanmapper

commit 74832cf909718b42c559f8cc9ca623e983960e1b

Refactor plot_2d module variables to avoid import from importer
Move plot_2d plane and vmax_overview variables to config to avoid requiring plot_2d import in importer. This refactoring allows removes a dependency on Matplotlib if only running stack processing.

clrbrain/plot_2d.py
@@ -53,10 +53,7 @@ padding = (5, 5, 3) # human (x, y, z) order
 SEG_LINEWIDTH = 1
 ZOOM_COLS = 9
 Z_LEVELS = ("bottom", "middle", "top")
-PLANE = ("xy", "xz", "yz")
-plane = None
 CIRCLES = ("Circles", "Repeat circles", "No circles", "Full annotation")
-vmax_overview = 1.0
 _DOWNSAMPLE_THRESH = 1000
 
 # need to store DraggableCircles objects to prevent premature garbage collection
@@ -401,10 +398,10 @@ def show_subplot(fig, gs, row, col, image5d, channel, roi_size, offset,
     # swap columns if showing a different plane
     plane_axis = get_plane_axis(plane)
     image5d_shape_offset = 1 if image5d.ndim >= 4 else 0
-    if plane == PLANE[1]:
+    if plane == config.PLANE[1]:
         # "xz" planes
         size = lib_clrbrain.swap_elements(size, 0, 1, image5d_shape_offset)
-    elif plane == PLANE[2]:
+    elif plane == config.PLANE[2]:
         # "yz" planes
         size = lib_clrbrain.swap_elements(size, 0, 2, image5d_shape_offset)
         size = lib_clrbrain.swap_elements(size, 0, 1, image5d_shape_offset)
@@ -434,9 +431,9 @@ def show_subplot(fig, gs, row, col, image5d, channel, roi_size, offset,
             region = [z_relative, slice(0, roi_size[1]), 
                       slice(0, roi_size[0])]
         # swap columns if showing a different plane
-        if plane == PLANE[1]:
+        if plane == config.PLANE[1]:
             region = lib_clrbrain.swap_elements(region, 0, 1)
-        elif plane == PLANE[2]:
+        elif plane == config.PLANE[2]:
             region = lib_clrbrain.swap_elements(region, 0, 2)
             region = lib_clrbrain.swap_elements(region, 0, 1)
         # get the zoomed region
@@ -460,7 +457,7 @@ def show_subplot(fig, gs, row, col, image5d, channel, roi_size, offset,
         # show the ROI, which is now a 2D zoomed image
         colormaps = config.process_settings["channel_colors"]
         imshow_multichannel(
-            ax, roi, channel, colormaps, aspect, alpha)#, 0.0, vmax_overview)
+            ax, roi, channel, colormaps, aspect, alpha)#, 0.0, config.vmax_overview)
         #print("roi shape: {} for z_relative: {}".format(roi.shape, z_relative))
         
         # show labels if provided and within ROI
@@ -681,7 +678,7 @@ def plot_2d_stack(fn_update_seg, title, filename, image5d, channel, roi_size,
     # adjust array order based on which plane to show
     border_full = np.copy(border)
     border[2] = 0
-    if plane == PLANE[1]:
+    if plane == config.PLANE[1]:
         # "xz" planes; flip y-z to give y-planes instead of z
         roi_size = lib_clrbrain.swap_elements(roi_size, 1, 2)
         offset = lib_clrbrain.swap_elements(offset, 1, 2)
@@ -689,7 +686,7 @@ def plot_2d_stack(fn_update_seg, title, filename, image5d, channel, roi_size,
         border_full = lib_clrbrain.swap_elements(border_full, 1, 2)
         if segments is not None and len(segments) > 0:
             segments[:, [0, 1]] = segments[:, [1, 0]]
-    elif plane == PLANE[2]:
+    elif plane == config.PLANE[2]:
         # "yz" planes; roll backward to flip x-z and x-y
         roi_size = lib_clrbrain.roll_elements(roi_size, -1)
         offset = lib_clrbrain.roll_elements(offset, -1)
@@ -801,7 +798,7 @@ def plot_2d_stack(fn_update_seg, title, filename, image5d, channel, roi_size,
         if downsample < 1: 
             downsample = 1
         min_show = plot_3d.near_min
-        max_show = vmax_overview
+        max_show = config.vmax_overview
         #print(img2d_ov.shape, roi_size)
         #print(np.prod(img2d_ov.shape[1:3]), np.prod(roi_size[:2]))
         if np.prod(img2d_ov.shape[1:3]) < 2 * np.prod(roi_size[:2]):
@@ -1054,7 +1051,7 @@ def extract_plane(image5d, plane_n, plane=None):
         plane_n: Slice of planes to extract, which can be a single index 
             or multiple indices such as would be used for an animation.
         plane: Type of plane to extract, which should be one of 
-            :attribute:`PLANES`.
+            :attribute:`config.PLANES`.
     """
     origin = None
     aspect = None # aspect ratio
@@ -1066,7 +1063,7 @@ def extract_plane(image5d, plane_n, plane=None):
     # extract a single 2D plane or a stack of planes if plane_n is a slice, 
     # which would be used for animations
     img2d = None
-    if plane == PLANE[1]:
+    if plane == config.PLANE[1]:
         # xz plane
         aspect = detector.resolutions[0, 0] / detector.resolutions[0, 2]
         origin = "lower"
@@ -1075,7 +1072,7 @@ def extract_plane(image5d, plane_n, plane=None):
         if img2d.ndim > 2 and img2d.shape[1] > 1:
             # make y the "z" axis for stack of 2D plots, such as animations
             img2d = np.swapaxes(img2d, 0, 1)
-    elif plane == PLANE[2]:
+    elif plane == config.PLANE[2]:
         # yz plane
         aspect = detector.resolutions[0, 0] / detector.resolutions[0, 1]
         origin = "lower"
@@ -1097,15 +1094,15 @@ def max_plane(img3d, plane):
     
     Args:
         img3d: Image array in (z, y, x) order.
-        plane: Plane as a value from :attr:``PLANE``.
+        plane: Plane as a value from :attr:``config.PLANE``.
     
     Returns:
         Number of elements along ``plane``'s axis.
     """
     shape = img3d.shape
-    if plane == PLANE[1]:
+    if plane == config.PLANE[1]:
         return shape[1]
-    elif plane == PLANE[2]:
+    elif plane == config.PLANE[2]:
         return shape[2]
     else:
         return shape[0]
@@ -1114,15 +1111,15 @@ def get_plane_axis(plane):
     """Gets the name of the plane corresponding to the given axis.
     
     Args:
-        plane: An element of :attr:``PLANE``.
+        plane: An element of :attr:``config.PLANE``.
     
     Returns:
-        The axis name orthogonal to :attr:``PLANE``.
+        The axis name orthogonal to :attr:``config.PLANE``.
     """
     plane_axis = "z"
-    if plane == PLANE[1]:
+    if plane == config.PLANE[1]:
         plane_axis = "y"
-    elif plane == PLANE[2]:
+    elif plane == config.PLANE[2]:
         plane_axis = "x"
     return plane_axis
 
@@ -1200,11 +1197,11 @@ def _show_overlay(ax, img, plane_i, cmap, out_plane, aspect=1.0, alpha=1.0,
         title: Subplot title; defaults to None, in which case no title will 
             be shown.
     """
-    if out_plane == PLANE[1]:
+    if out_plane == config.PLANE[1]:
         # xz plane
         img_2d = img[:, plane_i]
         img_2d = np.flipud(img_2d)
-    elif out_plane == PLANE[2]:
+    elif out_plane == config.PLANE[2]:
         # yz plane, which requires a flip when original orientation is 
         # horizontal section
         # TODO: generalize to other original orientations
@@ -1286,7 +1283,7 @@ def plot_overlays_reg(exp, atlas, atlas_reg, labels_reg, cmap_exp,
     plane_frac = 2#5 / 2
     if out_plane is None:
         out_plane = plane
-    if out_plane == PLANE[1]:
+    if out_plane == config.PLANE[1]:
         # xz plane
         aspect = resolution[0] / resolution[2]
         z = exp.shape[1] // plane_frac
@@ -1294,7 +1291,7 @@ def plot_overlays_reg(exp, atlas, atlas_reg, labels_reg, cmap_exp,
             atlas_z = atlas.shape[1] // plane_frac
         else:
             atlas_z = int(z - translation[1])
-    elif out_plane == PLANE[2]:
+    elif out_plane == config.PLANE[2]:
         # yz plane
         aspect = resolution[0] / resolution[1]
         z = exp.shape[2] // plane_frac

clrbrain/cli.py
@@ -90,7 +90,6 @@ from clrbrain import plot_3d
 from clrbrain import detector
 from clrbrain import chunking
 from clrbrain import mlearn
-from clrbrain import exporter
 
 roi_size = None # current region of interest
 roi_sizes = None # list of regions of interest
@@ -528,8 +527,8 @@ def main(process_args_only=False):
           .format(config.register_settings["register_type"]))
     if args.plane is not None:
         from clrbrain import plot_2d
-        plot_2d.plane = args.plane
-        print("Set plane to {}".format(plot_2d.plane))
+        config.plane = args.plane
+        print("Set plane to {}".format(config.plane))
     if args.saveroi:
         config.saveroi = args.saveroi
         print("Set save ROI to file to ".format(config.saveroi))
@@ -804,15 +803,15 @@ def process_file(filename_base, offset, roi_size):
         # export ROIs; assumes that info_proc was already loaded to 
         # give smaller region from which smaller ROIs from the truth DB 
         # will be extracted
+        from clrbrain import exporter
         db = config.db if config.truth_db is None else config.truth_db
         exporter.export_rois(
             db, image5d, config.channel, filename_base, config.border)
         
     elif proc_type == PROC_TYPES[6]:
         # transpose Numpy array
-        from clrbrain import plot_2d
         importer.transpose_npy(
-            config.filename, config.series, plane=plot_2d.plane, 
+            config.filename, config.series, plane=config.plane, 
             rescale=config.rescale)
         
     elif proc_type == PROC_TYPES[7]:
@@ -824,6 +823,7 @@ def process_file(filename_base, offset, roi_size):
     
     elif proc_type == PROC_TYPES[8]:
         # export blobs to CSV file
+        from clrbrain import exporter
         exporter.blobs_to_csv(segments_proc, filename_info_proc)
         
     elif proc_type == PROC_TYPES[1] or proc_type == PROC_TYPES[2]:

clrbrain/config.py
@@ -445,6 +445,9 @@ rescale = None # rescale image
 interval = None # interval
 delay = None # delay time between images
 
-# EXPORT IMAGES
+# 2D IMAGES
 
+PLANE = ("xy", "xz", "yz")
+plane = None
+vmax_overview = 1.0
 border = None # clip ROI to border (x,y,z order)

clrbrain/importer.py
@@ -40,7 +40,6 @@ except ImportError as e:
 from clrbrain import chunking
 from clrbrain import config
 from clrbrain import detector
-from clrbrain import plot_2d
 from clrbrain import plot_3d
 from clrbrain import lib_clrbrain
 
@@ -307,7 +306,7 @@ def _update_image5d_np_ver(curr_ver, image5d, info, filename_info_npz):
         info_up["near_min"] = near_mins
         info_up["near_max"] = near_maxs
         info_up["scaling"] = scaling
-        info_up["plane"] = plot_2d.plane
+        info_up["plane"] = config.plane
     
     # backup and save updated info
     lib_clrbrain.backup_file(
@@ -378,8 +377,8 @@ def read_info(filename_info_npz):
     try:
         plot_3d.near_max = output["near_max"]
         print("set near_max to {}".format(plot_3d.near_max))
-        plot_2d.vmax_overview = plot_3d.near_max * 1.1
-        print("Set vmax_overview to {}".format(plot_2d.vmax_overview))
+        config.vmax_overview = plot_3d.near_max * 1.1
+        print("Set vmax_overview to {}".format(config.vmax_overview))
     except KeyError:
         print("could not find near_max")
     return output, image5d_ver_num
@@ -719,12 +718,12 @@ def transpose_npy(filename, series, plane=None, rescale=None):
     multichannel = image5d.ndim >=5
     image5d_swapped = image5d
     
-    if plane is not None and plane != plot_2d.PLANE[0]:
+    if plane is not None and plane != config.PLANE[0]:
         # swap z-y to get (y, z, x) order for xz orientation
         image5d_swapped = np.swapaxes(image5d_swapped, offset, offset + 1)
         detector.resolutions[0] = lib_clrbrain.swap_elements(
             detector.resolutions[0], 0, 1)
-        if plane == plot_2d.PLANE[2]:
+        if plane == config.PLANE[2]:
             # swap new y-x to get (x, z, y) order for yz orientation
             image5d_swapped = np.swapaxes(image5d_swapped, offset, offset + 2)
             detector.resolutions[0] = lib_clrbrain.swap_elements(
@@ -779,7 +778,7 @@ def transpose_npy(filename, series, plane=None, rescale=None):
         image5d_transposed = np.lib.format.open_memmap(
             filename_image5d_npz, mode="w+", dtype=image5d_swapped.dtype, 
             shape=image5d_swapped.shape)
-        if plane == plot_2d.PLANE[1] or plane == plot_2d.PLANE[2]:
+        if plane == config.PLANE[1] or plane == config.PLANE[2]:
             # flip upside-down if re-orienting planes
             if offset:
                 image5d_transposed[0, :] = np.fliplr(image5d_swapped[0, :])

clrbrain/register.py
@@ -314,7 +314,7 @@ def transpose_img(img_sitk, plane, rotate=False, target_size=None):
     
     Args:
         img_sitk: Image in SimpleITK format.
-        plane: One of :attr:``plot_2d.PLANES`` elements, specifying the 
+        plane: One of :attr:``config.PLANES`` elements, specifying the 
             planar orientation in which to transpose the image. The current 
             orientation is taken to be "xy".
         rotate: Rotate the final output image by 180 degrees; defaults to False.
@@ -329,19 +329,19 @@ def transpose_img(img_sitk, plane, rotate=False, target_size=None):
     spacing = img_sitk.GetSpacing()
     origin = img_sitk.GetOrigin()
     transposed = img
-    if plane is not None and plane != plot_2d.PLANE[0]:
+    if plane is not None and plane != config.PLANE[0]:
         # swap z-y to get (y, z, x) order for xz orientation
         transposed = np.swapaxes(transposed, 0, 1)
         # sitk convention is opposite of numpy with (x, y, z) order
         spacing = lib_clrbrain.swap_elements(spacing, 1, 2)
         origin = lib_clrbrain.swap_elements(origin, 1, 2)
-        if plane == plot_2d.PLANE[1]:
+        if plane == config.PLANE[1]:
             # rotate
             transposed = transposed[..., ::-1]
             transposed = np.swapaxes(transposed, 1, 2)
             spacing = lib_clrbrain.swap_elements(spacing, 0, 1)
             origin = lib_clrbrain.swap_elements(origin, 0, 1)
-        elif plane == plot_2d.PLANE[2]:
+        elif plane == config.PLANE[2]:
             # swap new y-x to get (x, z, y) order for yz orientation
             transposed = np.swapaxes(transposed, 0, 2)
             spacing = lib_clrbrain.swap_elements(spacing, 0, 2)
@@ -349,7 +349,7 @@ def transpose_img(img_sitk, plane, rotate=False, target_size=None):
             # rotate
             transposed = np.swapaxes(transposed, 1, 2)
             spacing = lib_clrbrain.swap_elements(spacing, 0, 1)
-        if plane == plot_2d.PLANE[1] or plane == plot_2d.PLANE[2]:
+        if plane == config.PLANE[1] or plane == config.PLANE[2]:
             # flip upside-down
             transposed[:] = np.flipud(transposed[:])
         else:
@@ -1454,8 +1454,8 @@ if __name__ == "__main__":
         print("Please choose a registration type")
     elif config.register_type == config.REGISTER_TYPES[0]:
         # "single", basic registration of 1st to 2nd image, transposing the 
-        # second image according to plot_2d.plane and config.flip_horiz
-        register(*config.filenames[0:2], plane=plot_2d.plane, 
+        # second image according to config.plane and config.flip_horiz
+        register(*config.filenames[0:2], plane=config.plane, 
                  flip=flip, name_prefix=prefix)
     elif config.register_type == config.REGISTER_TYPES[1]:
         # groupwise registration, which assumes that the last image 
@@ -1465,9 +1465,9 @@ if __name__ == "__main__":
             config.filenames[:-1], flip=config.flip, name_prefix=prefix)
     elif config.register_type == config.REGISTER_TYPES[2]:
         # overlay registered images in each orthogonal plane
-        for out_plane in plot_2d.PLANE:
+        for out_plane in config.PLANE:
             overlay_registered_imgs(
-                *config.filenames[0:2], plane=plot_2d.plane, 
+                *config.filenames[0:2], plane=config.plane, 
                 flip=flip, name_prefix=prefix, 
                 out_plane=out_plane)
     elif config.register_type in (

clrbrain/stack.py
@@ -88,7 +88,7 @@ def _build_animated_gif(images, out_path, process_fnc, rescale, aspect=None,
             if img_size is None:
                 img_size = img.shape
             # tweak max values to change saturation
-            vmax = plot_2d.vmax_overview
+            vmax = config.vmax_overview
             #vmax = np.multiply(vmax, (0.9, 1.0))
             plotted_imgs[i] = plot_2d.imshow_multichannel(
                 ax, img, config.channel, colormaps, aspect, 1, 
@@ -137,7 +137,7 @@ def animated_gif(path, series=0, interval=None, rescale=None, delay=None):
             a directory, all images from this directory will be imported in 
             Python sorted order. If the path is a saved Numpy array (eg .npy 
             file), animations will be built by plane, using the plane 
-            orientation set in :const:`plot_2d.plane`.
+            orientation set in :const:`config.plane`.
         series: Stack to build for multiseries files; defaults to 0.
         interval: Every nth image will be incorporated into the animation; 
             defaults to None, in which case 1 will be used.
@@ -163,7 +163,7 @@ def animated_gif(path, series=0, interval=None, rescale=None, delay=None):
     else:
         image5d = importer.read_file(path, series)
         planes, aspect, origin = plot_2d.extract_plane(
-            image5d, slice(None, None, interval), plane=plot_2d.plane)
+            image5d, slice(None, None, interval), plane=config.plane)
         out_name = name.replace(".czi", "_").rstrip("_")
         fnc = _process_plane
     ext = plot_2d.savefig
@@ -189,7 +189,7 @@ def save_plane(image5d, offset, roi_size=None, name=None):
     """
     plane_n = offset[2]
     img2d, aspect, origin = plot_2d.extract_plane(
-        image5d, plane_n, plane=plot_2d.plane)
+        image5d, plane_n, plane=config.plane)
     if roi_size is not None:
         img2d = img2d[
             offset[1]:offset[1]+roi_size[1], offset[0]:offset[0]+roi_size[0]]
@@ -201,7 +201,7 @@ def save_plane(image5d, offset, roi_size=None, name=None):
     # artifacts over whole image; also use no interpolation for cleanest image
     plot_2d.imshow_multichannel(
         ax, img2d, config.channel, colormaps, aspect, 1, vmin=plot_3d.near_min, 
-        vmax=plot_2d.vmax_overview*0.8, 
+        vmax=config.vmax_overview*0.8, 
         origin=origin, interpolation="none")
     fit_frame_to_image(fig, img2d.shape, aspect)
     if not name:

commit 343f82ee5976962116f21189b7759fd093d02540

Fix regression in displaying overlays after registration
Fix small regression introduced in commit 74832cf where the plain variable needed to be changed from plot_2d to config module variable.

clrbrain/plot_2d.py
@@ -1281,7 +1281,7 @@ def plot_overlays_reg(exp, atlas, atlas_reg, labels_reg, cmap_exp,
     atlas_z = 0
     plane_frac = 2#5 / 2
     if out_plane is None:
-        out_plane = plane
+        out_plane = config.plane
     if out_plane == config.PLANE[1]:
         # xz plane
         aspect = resolution[0] / resolution[2]
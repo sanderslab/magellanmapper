719a50bf77bb9b4e684fe5ab7f4c3fc9e95ccd43
David Young <david@textflex.com>
2018-07-24 10:12:24 -0700

    Basic plot editor
    PlotEditor class for basic figure editing by click and dragging the mouse to copy a given pixel value into new areas.

--- clrbrain/plot_2d.py
+++ clrbrain/plot_2d.py
@@ -1148,6 +1148,86 @@ class PixelDisplay(object):
         z = self.img[int(y), int(x)]
         return "x={:.01f}, y={:.01f}, z={:.01f}".format(x, y, z)
 
+class PlotEditor:
+    def __init__(self, img3d):
+        self.img3d = img3d
+        self.intensity = None
+    
+    def set_plane(self, ax_img, plane_n, plane=None):
+        self.ax_img = ax_img
+        self.plane_n = plane_n
+        self.plane = plane
+        self.connect()
+    
+    def connect(self):
+        """Connect events to functions.
+        """
+        self.cidpress = self.ax_img.figure.canvas.mpl_connect(
+            "button_press_event", self.on_press)
+        self.cidrelease = self.ax_img.figure.canvas.mpl_connect(
+            "button_release_event", self.on_release)
+        self.cidmotion = self.ax_img.figure.canvas.mpl_connect(
+            "motion_notify_event", self.on_motion)
+        #print("connected circle at {}".format(self.circle.center))
+    
+    def on_press(self, event):
+        """Initiate drag events with Shift- or Alt-click inside a circle.
+        
+        Shift-click to move a circle, and Alt-click to resize a circle's radius.
+        """
+        x = int(event.xdata)
+        y = int(event.ydata)
+        self.intensity = self.img3d[self.plane_n, y, x]
+        print("got intensity {} at x,y,z = {},{},{}".format(self.intensity, x, y, self.plane_n))
+        PlotEditor.lock = self
+        
+        # draw everywhere except the circle itself, store the pixel buffer 
+        # in background, and draw the circle
+        canvas = self.ax_img.figure.canvas
+        ax = self.ax_img.axes
+        self.ax_img.set_animated(True)
+        canvas.draw()
+        self.background = canvas.copy_from_bbox(self.ax_img.axes.bbox)
+        #ax.draw_artist(self.ax_img)
+        canvas.blit(ax.bbox)
+    
+    def on_motion(self, event):
+        """Move the circle if the drag event has been initiated.
+        """
+        if event.inaxes != self.ax_img.axes or self.intensity is None: return
+        x = int(event.xdata)
+        y = int(event.ydata)
+        self.img3d[self.plane_n, y, x] = self.intensity
+        print("changed intensity to {} at x,y,z = {},{},{}".format(self.intensity, x, y, self.plane_n))
+        
+
+        # restore the saved background and redraw the circle at its new position
+        canvas = self.ax_img.figure.canvas
+        ax = self.ax_img.axes
+        canvas.restore_region(self.background)
+        self.ax_img.set_data(self.img3d[self.plane_n])
+        ax.draw_artist(self.ax_img)
+        canvas.blit(ax.bbox)
+    
+    def on_release(self, event):
+        """Finalize the circle and segment's position after a drag event
+        is completed with a button release.
+        """
+        # turn off animation property, reset background
+        PlotEditor.lock = None
+        self.ax_img.set_animated(False)
+        self.background = None
+        self.intensity = None
+        self.ax_img.figure.canvas.draw()
+        print("released!")
+    
+    def disconnect(self):
+        """Disconnect event listeners.
+        """
+        self.ax_img.figure.canvas.mpl_disconnect(self.cidpress)
+        self.ax_img.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.ax_img.figure.canvas.mpl_disconnect(self.cidmotion)
+
 def plot_atlas_editor(image5d, labels_img, channel, offset, plane=None):
     """Plot ROI as sequence of z-planes containing only the ROI itself.
     
@@ -1175,6 +1255,7 @@ def plot_atlas_editor(image5d, labels_img, channel, offset, plane=None):
     cmap_labels = _get_labels_colormap(labels_img)
     if not plane:
         plane = config.PLANE[0]
+    plot_ed = PlotEditor(labels_img)
     
     def show_overview(z_overview):
         img2d, aspect, origin = extract_plane(image5d, z_overview)
@@ -1183,10 +1264,11 @@ def plot_atlas_editor(image5d, labels_img, channel, offset, plane=None):
         
         labels_image5d = importer.roi_to_image5d(labels_img)
         img2d, aspect, origin = extract_plane(labels_image5d, z_overview)
-        imshow_multichannel(
+        label_ax_img = imshow_multichannel(
             ax, img2d, 0, [cmap_labels], aspect, 0.7, interpolation="none")
         ax.format_coord = PixelDisplay(img2d)
         _set_overview_title(ax, plane, z_overview)
+        plot_ed.set_plane(label_ax_img[0], z_overview, plane)
     
     def scroll_overview(event):
         """Scroll through overview images along their orthogonal axis.

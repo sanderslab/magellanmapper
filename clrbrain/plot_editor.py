4be99a6aa0a072e9738e6e3ca36a9a6bd8c3e0bd
David Young <david@textflex.com>
2018-07-24 18:08:46 -0700

    Change pen radius in plot editor
    - Use bracket keys to change circle radius for plot editor pen
    - Show pen radius whenever the mouse is over the plot editor image
    - Refactor animation setup to depend on axes entry events and updates to be called from press or motion events
    - Avoid animation update if no change in pixel position
    - Refactor listener disconnections to remove redundancy

--- clrbrain/plot_editor.py
+++ clrbrain/plot_editor.py
@@ -3,6 +3,8 @@
 """2D plot editor.
 """
 
+import matplotlib.patches as patches
+from skimage import draw
 
 class PlotEditor:
     def __init__(self, img3d):
@@ -11,6 +13,13 @@ class PlotEditor:
         self.cidpress = None
         self.cidrelease = None
         self.cidmotion = None
+        self.cidenter = None
+        self.cidleave = None
+        self.cidkeypress = None
+        self.radius = 5
+        self.circle = None
+        self.background = None
+        self.last_loc = None
     
     def set_plane(self, ax_img, plane_n, plane=None):
         self.ax_img = ax_img
@@ -27,66 +36,133 @@ class PlotEditor:
             "button_release_event", self.on_release)
         self.cidmotion = self.ax_img.figure.canvas.mpl_connect(
             "motion_notify_event", self.on_motion)
-        #print("connected circle at {}".format(self.circle.center))
+        self.cidenter = self.ax_img.figure.canvas.mpl_connect(
+            "axes_enter_event", self.on_axes_enter)
+        self.cidleave = self.ax_img.figure.canvas.mpl_connect(
+            "axes_leave_event", self.on_axes_exit)
+        self.cidkeypress = self.ax_img.figure.canvas.mpl_connect(
+            "key_press_event", self.on_key_press)
+    
+    def setup_animation(self):
+        """Store the pixel buffer in background to prep for animations.
+        """
+        PlotEditor.lock = self
+        canvas = self.ax_img.figure.canvas
+        self.ax_img.set_animated(True)
+        canvas.draw()
+        self.background = canvas.copy_from_bbox(self.ax_img.axes.bbox)
+        canvas.blit(self.ax_img.axes.bbox)
+    
+    def update_animation(self):
+        """Update animations used cached artists and background and only in 
+        the axes area for improved efficiency.
+        """
+        canvas = self.ax_img.figure.canvas
+        canvas.restore_region(self.background)
+        axes = self.ax_img.axes
+        axes.draw_artist(self.ax_img)
+        # transparency for some reason lost during the update until canvas 
+        # is fully redrawn in reset_animation, so need to manually set alpha
+        axes.set_alpha(0.5)
+        if self.circle:
+            axes.draw_artist(self.circle)
+        canvas.blit(self.ax_img.axes.bbox)
+        print("updated animation")
+    
+    def reset_animation(self):
+        """Turn off animations and redraw entire figure.
+        """
+        PlotEditor.lock = None
+        self.ax_img.set_animated(False)
+        self.background = None
+        self.intensity = None
+        self.ax_img.figure.canvas.draw()
+        print("reset animation")
     
     def on_press(self, event):
         """Initiate drag events with Shift- or Alt-click inside a circle.
         
         Shift-click to move a circle, and Alt-click to resize a circle's radius.
         """
+        if event.inaxes != self.ax_img.axes: return
         x = int(event.xdata)
         y = int(event.ydata)
         self.intensity = self.img3d[self.plane_n, y, x]
         print("got intensity {} at x,y,z = {},{},{}"
               .format(self.intensity, x, y, self.plane_n))
-        PlotEditor.lock = self
-        
-        # draw everywhere except the circle itself, store the pixel buffer 
-        # in background, and draw the circle
-        canvas = self.ax_img.figure.canvas
-        ax = self.ax_img.axes
-        self.ax_img.set_animated(True)
-        canvas.draw()
-        self.background = canvas.copy_from_bbox(self.ax_img.axes.bbox)
-        #ax.draw_artist(self.ax_img)
-        canvas.blit(ax.bbox)
+        self.circle.radius += 1
+        self.update_animation()
+    
+    def on_axes_enter(self, event):
+        self.setup_animation()
+    
+    def on_axes_exit(self, event):
+        if self.circle:
+            self.circle.remove()
+            self.circle = None
+        self.reset_animation()
     
     def on_motion(self, event):
-        """Move the circle if the drag event has been initiated.
+        """Move the editing pen's circle and draw with the chosen intensity 
+        value if set.
         """
-        if event.inaxes != self.ax_img.axes or self.intensity is None: return
+        if event.inaxes != self.ax_img.axes: return
+        
+        # get mouse position and return if no change from last pixel coord
         x = int(event.xdata)
         y = int(event.ydata)
-        self.img3d[self.plane_n, y, x] = self.intensity
-        print("changed intensity to {} at x,y,z = {},{},{}"
-              .format(self.intensity, x, y, self.plane_n))
-
-        # restore the saved background and redraw the circle at its new position
-        canvas = self.ax_img.figure.canvas
-        ax = self.ax_img.axes
-        canvas.restore_region(self.background)
-        self.ax_img.set_data(self.img3d[self.plane_n])
-        ax.draw_artist(self.ax_img)
-        canvas.blit(ax.bbox)
+        if self.last_loc is not None and self.last_loc == (x, y):
+            print("didn't move")
+            return
+        self.last_loc = (x, y)
+        
+        if self.circle:
+            # update pen circle position
+            self.circle.center = x, y
+        else:
+            # generate new circle if not yet present
+            self.circle = patches.Circle(
+                (x, y), radius=self.radius, linestyle=":", fill=False, 
+                edgecolor="w")
+            self.ax_img.axes.add_patch(self.circle)
+        
+        if self.intensity is not None:
+            # use the chosen intensity value to overwrite the image with 
+            # a pen of the chosen radius
+            rr, cc = draw.circle(
+                y, x, self.radius, self.img3d[self.plane_n].shape)
+            self.img3d[self.plane_n, rr, cc] = self.intensity
+            print("changed intensity to {} at x,y,z = {},{},{}"
+                  .format(self.intensity, x, y, self.plane_n))
+            self.ax_img.set_data(self.img3d[self.plane_n])
+        
+        self.update_animation()
     
     def on_release(self, event):
-        """Finalize the circle and segment's position after a drag event
-        is completed with a button release.
+        """Reset the intensity value to prevent further edits.
         """
-        # turn off animation property, reset background
-        PlotEditor.lock = None
-        self.ax_img.set_animated(False)
-        self.background = None
         self.intensity = None
-        self.ax_img.figure.canvas.draw()
         print("released!")
+    
+    def on_key_press(self, event):
+        """Change pen radius with bracket ([/]) buttons.
+        """
+        rad_orig = self.radius
+        if event.key == "[" and self.radius > 1:
+            self.radius -= 1
+        elif event.key == "]":
+            self.radius += 1
+        print("radius: {}".format(self.radius))
+        if rad_orig != self.radius and self.circle:
+            self.circle.radius = self.radius
     
     def disconnect(self):
         """Disconnect event listeners.
         """
-        if self.cidpress:
-            self.ax_img.figure.canvas.mpl_disconnect(self.cidpress)
-        if self.cidrelease:
-            self.ax_img.figure.canvas.mpl_disconnect(self.cidrelease)
-        if self.cidmotion:
-            self.ax_img.figure.canvas.mpl_disconnect(self.cidmotion)
+        self.circle = None
+        listeners = [
+            self.cidpress, self.cidrelease, self.cidmotion, self.cidenter, 
+            self.cidleave, self.cidkeypress]
+        for listener in listeners:
+            if listener:
+                self.ax_img.figure.canvas.mpl_disconnect(listener)

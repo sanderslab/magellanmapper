sanderslab/magellanmapper

commit 85e2d7251c044dc1717d6f6befbf128720848bca

Groupwise registration sharpening
- Sharpen groupwise registered image by removing pixels under threshold and filling holes
- Replace mean-based threshold with absolute threshold for groupwise image for now
- Add "groupwise" as a base register profile with lower bspline grid space voxels for stronger deformation

clrbrain/config.py
@@ -321,6 +321,10 @@ def update_register_settings(settings, settings_type):
         settings["settings_name"] = "finer"
         settings["bspline_iter_max"] = "512"
     
+    elif settings_type.startswith("groupwise"):
+        settings["settings_name"] = "groupwise"
+        settings["bspline_grid_space_voxels"] = "30"
+    
     elif settings_type.startswith("test"):
         settings["settings_name"] = "test"
         settings["target_size"] = (50, 50, 50)

clrbrain/register.py
@@ -54,6 +54,7 @@ except ImportError as e:
     print("WARNING: SimpleElastix could not be found, so there will be error "
           "when attempting to register images or load registered images")
 import numpy as np
+from skimage import img_as_uint
 from skimage import filters
 from skimage import measure
 from skimage import morphology
@@ -914,20 +915,35 @@ def register_group(img_files, flip=None, show_imgs=True,
     for i in range(num_images):
         extract_filter.SetIndex([0, 0, 0, i]) # x, y, z, t
         img = extract_filter.Execute(transformed_img)
+        img_np = sitk.GetArrayFromImage(img)
+        #means.append(filters.threshold_mean(img_np))
+        means.append(np.mean(img_np))
         # resize to original shape of first image, all aligned to position 
         # of subject within first image
-        img_np = np.zeros(size_orig[::-1])
-        img_np[:, start_y:] = sitk.GetArrayFromImage(img)
-        means.append(np.mean(img_np))
+        img_large_np = np.zeros(size_orig[::-1])
+        img_large_np[:, start_y:] = img_np
         if show_imgs:
-            sitk.Show(replace_sitk_with_numpy(img, img_np))
-        imgs.append(img_np)
+            sitk.Show(replace_sitk_with_numpy(img, img_large_np))
+        imgs.append(img_large_np)
     # combine all images by taking their mean
     img_mean = np.mean(imgs, axis=0)
     mean_of_means = np.mean(means)
-    thresh = mean_of_means / (num_images * 0.2)
+    thresh = mean_of_means / num_images * 2
     print("zeroing out pixels below {} based on means {}".format(thresh, means))
-    img_mean[img_mean < thresh] = 0
+    #img_mean[img_mean < thresh] = 0
+    
+    sitk.Show(replace_sitk_with_numpy(transformed_img, img_mean))
+    img_mean_orig = np.copy(img_mean)
+    mask = img_mean < 0.009
+    img_mean[mask] = 0
+    img_mean_unfilled = replace_sitk_with_numpy(transformed_img, img_mean)
+    sitk.Show(img_mean_unfilled)
+    
+    img_mean_uint = img_as_uint(img_mean)
+    labels = measure.label(img_mean_uint)
+    to_fill = np.logical_and(morphology.remove_small_holes(labels, 10000), mask)
+    img_mean[to_fill] = img_mean_orig[to_fill]
+    
     transformed_img = replace_sitk_with_numpy(transformed_img, img_mean)
     
     if show_imgs:

commit 4dc6621a8354e0bd98c152b542351fa97c621eff

Fix regression in groupwise bspline voxels that caused internal structure over-warping
Fix regression introduced in commit 85e2d72 that caused internal structures to become overly deformed. Increase rather than decrase bspline voxels from default to improve internal registration.

clrbrain/config.py
@@ -332,9 +332,11 @@ def update_register_settings(settings, settings_type):
         settings["bspline_iter_max"] = "512"
     
     elif settings_type.startswith("groupwise"):
+        # groupwise registration, with larger bspline voxels to avoid over 
+        # deformation of internal structures and increased spacing schedule 
+        # to improve internal alignment
         settings["settings_name"] = "groupwise"
-        # groupwise registration
-        settings["bspline_grid_space_voxels"] = "30"
+        settings["bspline_grid_space_voxels"] = "70"
         settings["carve_threshold"] = 0.009
         settings["holes_area"] = 10000
         # manually add variable tissue area from first image to create 

clrbrain/register.py
@@ -572,6 +572,15 @@ def register(fixed_file, moving_file_dir, plane=None, flip=False,
     # load fixed image, assumed to be experimental image
     fixed_img = _load_numpy_to_sitk(fixed_file)
     
+    '''
+    img_np = sitk.GetArrayFromImage(fixed_img)
+    img_np = plot_3d.carve(
+        img_np, thresh=0.0095, holes_area=10000)
+    fixed_img = replace_sitk_with_numpy(fixed_img, img_np)
+    sitk.Show(fixed_img)
+    return
+    '''
+       
     # preprocessing; store original fixed image for overlap measure
     fixed_img_orig = fixed_img
     if settings["preprocess"]:
@@ -969,6 +978,7 @@ def register_group(img_files, flip=None, show_imgs=True,
     if show_imgs:
         for img in imgs_to_show: sitk.Show(img)
     
+    #transformed_img = img_raw
     if write_imgs:
         # write both the .mhd and Numpy array files to a separate folder to 
         # mimic the atlas folder format
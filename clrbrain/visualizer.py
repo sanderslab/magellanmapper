sanderslab/magellanmapper

commit 19851bffb8eda8de6a24ee365f01807a4547909c

Add 3D render type option to GUI
- Add "Surface" check box to render 3D model as a surface rather than as points
- Remove the "mlab_3d" command-line argument since it duplicates the configuration setting and can now be overridden in the GUI
- Removed commented out references to "verify" argument

clrbrain/visualizer.py
@@ -167,7 +167,7 @@ class Visualization(HasTraits):
     segs_feedback = Str("Segments output")
     labels = None # segmentation labels
     _check_list_3d = List
-    _DEFAULTS_3D = ["Side panes", "Side circles", "Raw"]
+    _DEFAULTS_3D = ["Side panes", "Side circles", "Raw", "Surface"]
     _check_list_2d = List
     _DEFAULTS_2D = [
         "Filtered", "Border zone", "Segmentation", "Grid", "Max inten proj"]
@@ -423,9 +423,7 @@ class Visualization(HasTraits):
             self.roi = plot_3d.prepare_roi(
                 cli.image5d, curr_roi_size, curr_offset)
             
-            settings = config.process_settings
-            vis = (plot_3d.mlab_3d, settings["vis_3d"])
-            if plot_3d.MLAB_3D_TYPES[0] in vis:
+            if self._DEFAULTS_3D[3] in self._check_list_3d:
                 # surface rendering
                 plot_3d.plot_3d_surface(
                     self.roi, self.scene.mlab, config.channel)
@@ -539,6 +537,10 @@ class Visualization(HasTraits):
         self._styles_2d = [self._DEFAULTS_STYLES_2D[0]]
         self._check_list_2d = [self._DEFAULTS_2D[1]]
         self._check_list_3d = [self._DEFAULTS_3D[2]]
+        if (config.process_settings["vis_3d"].lower() 
+            == self._DEFAULTS_3D[3].lower()):
+            # check "surface" if set in profile
+            self._check_list_3d.append(self._DEFAULTS_3D[3])
         #self._structure_scale = self._structure_scale_high
         
         # setup interface for image
@@ -1110,7 +1112,7 @@ class Visualization(HasTraits):
                 ),
                 Item(
                      "_check_list_3d", 
-                     editor=CheckListEditor(values=_DEFAULTS_3D, cols=3), 
+                     editor=CheckListEditor(values=_DEFAULTS_3D, cols=4), 
                      style="custom",
                      label="3D options"
                 ),

clrbrain/cli.py
@@ -22,7 +22,6 @@ Command-line arguments in addition to those from attributes listed below:
     * load_labels: Path to labels reference file, which also serves as a flag 
         to load the labels image as well 
         (see :attr:`config.load_labels`).
-    * mlab_3d: 3D visualization mode (see plot_3d.py).
     * padding_2d: Padding around the ROI given as (x, y, z) from which to 
         include segments and and show further 2D planes.
     * plane: Plane type (see :const:``config.PLANE``).
@@ -459,11 +458,9 @@ def main(process_args_only=False):
     parser.add_argument("--series")
     parser.add_argument("--savefig")
     parser.add_argument("--padding_2d")
-    #parser.add_argument("--verify", action="store_true")
     parser.add_argument("--offset", nargs="*")
     parser.add_argument("--size", nargs="*")
     parser.add_argument("--proc")
-    parser.add_argument("--mlab_3d")
     parser.add_argument("--res")
     parser.add_argument("--mag")
     parser.add_argument("--zoom")
@@ -522,12 +519,6 @@ def main(process_args_only=False):
     if args.savefig is not None:
         config.savefig = args.savefig
         print("Set savefig extension to {}".format(config.savefig))
-    '''
-    if args.verify:
-        from clrbrain import plot_2d
-        plot_2d.verify = args.verify
-        print("Set verify to {}".format(plot_2d.verify))
-    '''
     if args.verbose:
         config.verbose = args.verbose
         print("Set verbose to {}".format(config.verbose))
@@ -559,13 +550,6 @@ def main(process_args_only=False):
         else:
             print("Did not recognize processing type: {}"
                   .format(args.proc))
-    if args.mlab_3d is not None:
-        if args.mlab_3d in plot_3d.MLAB_3D_TYPES:
-            plot_3d.mlab_3d = args.mlab_3d
-            print("3D rendering set to {}".format(plot_3d.mlab_3d))
-        else:
-            print("Did not recognize 3D rendering type: {}"
-                  .format(args.mlab_3d))
     if args.res is not None:
         res_split = args.res.split(",")
         if len(res_split) >= 3:

clrbrain/config.py
@@ -90,7 +90,7 @@ class ProcessSettings(SettingsDict):
     def __init__(self, *args, **kwargs):
         super().__init__(self)
         self["settings_name"] = "default"
-        self["vis_3d"] = "points"
+        self["vis_3d"] = "points" # "points" or "surface" 3D visualization
         self["points_3d_thresh"] = 0.85 # frac of thresh (changed in v.0.6.6)
         self["clip_vmax"] = 99.5
         self["clip_min"] = 0.2

clrbrain/plot_3d.py
@@ -6,13 +6,6 @@ Provides options for drawing as surfaces or points.
 
 Attributes:
     mask_dividend: Maximum number of points to show.
-    MLAB_3D_TYPES: Tuple of types of 3D visualizations for both the image
-        and segmentation.
-        * "surface": Renders surfaces in Mayavi contour and
-          surface.
-        * "point": Renders as raw points, minus points under
-          the intensity_min threshold.
-    mlab_3d: The chosen type.
 """
 
 from time import time
@@ -34,8 +27,6 @@ from clrbrain import lib_clrbrain
 
 _MASK_DIVIDEND = 10000.0 # 3D max points
 _MAYAVI_COLORMAPS = ("Greens", "Reds", "Blues", "Oranges")
-MLAB_3D_TYPES = ("surface", "point")
-mlab_3d = MLAB_3D_TYPES[1]
 
 def setup_channels(roi, channel, dim_channel):
     """Setup channels array for the given ROI dimensions.

commit f75c44cf62b928e34a3d8c6eb2c581074aee01b4

Fix regression in detecting blobs
Commit 19851bf introduced a regression for remaining references to plot_3d.mlab_3d. Change this reference when detecting blobs via the Visualization GUI to use the "segmentation" 2D option setting instead.

clrbrain/visualizer.py
@@ -646,9 +646,8 @@ class Visualization(HasTraits):
         return segs is None or not isinstance(segs, np.ndarray)
     
     def _btn_segment_trait_fired(self, segs=None):
-        if plot_3d.mlab_3d == plot_3d.MLAB_3D_TYPES[0]:
-            # segments using the Random-Walker algorithm
-            # TODO: also check ProcessSettings and/or do away with mlab_3d flag
+        if self._DEFAULTS_2D[2] in self._check_list_3d:
+            # "segmentation" option to segment using the Random-Walker
             self.labels, self.walker = detector.segment_rw(self.roi)
             self.segs_cmap = plot_3d.show_surface_labels(self.labels, self)
         else:
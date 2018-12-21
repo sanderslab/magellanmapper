sanderslab/magellanmapper

commit cf47982ccb7f6323377165090f25892570a29234

Change labels data type during curation only to fit required range
Use the dtype library function to identify the necessary type to capture the curated labels signed range. This change avoids undershooting the required size in some cases and reduces file size in many other cases.

clrbrain/register.py
@@ -379,10 +379,18 @@ def _curate_labels(img, img_ref, mirror=None, edge=None, expand=None,
         a tuple of the indices at which the edge was extended and the 
         image was mirrored, respectively.
     """
-    # TODO: check to make sure values don't get wrapped around if np.int32
-    # max value is less than data max val
-    img_np = sitk.GetArrayFromImage(img).astype(np.int32)
-    img_ref_np = sitk.GetArrayFromImage(img_ref).astype(np.int32)
+    # cast to signed int that takes the full range of the labels image
+    img_np = sitk.GetArrayFromImage(img)
+    try:
+        dtype = lib_clrbrain.dtype_within_range(0, np.amax(img_np), True, True)
+        if dtype != img_np.dtype:
+            print("casting labels image to type", dtype)
+            img_np = img_np.astype(dtype)
+    except TypeError as e:
+        # fallback to large signed data type
+        print(e)
+        img_np.astype(np.int32)
+    img_ref_np = sitk.GetArrayFromImage(img_ref)
     tot_planes = len(img_np)
     
     # extend near edges: lateral edges of atlas labels (ie low z) are missing 

commit c65a6de3ffb59e0008d1ba4c35b220a6381968ef

Fix regression in type match for overlap filter with labels watershed image
Commit cf47982 introduced a regression where the type of the labels image is often different from that of the labels watershed image. Cast the watershed image to the labels image if their types differ.

clrbrain/register.py
@@ -1997,8 +1997,10 @@ def merge_atlas_segmentations(path_fixed, show=True):
         # by opening operation for overall smoothing with profile setting
         smooth_labels(labels_seg, 2, SMOOTHING_MODES[2])
         smooth_labels(labels_seg, smoothing, SMOOTHING_MODES[0])
-    # mirror back to other half
+    # mirror back to other half and attempt to cast to a smaller type
     labels_seg = _mirror_imported_labels(labels_seg, len_half)
+    if labels_seg.dtype != labels_img_np.dtype:
+        labels_seg = labels_seg.astype(labels_img_np.dtype)
     labels_sitk_seg = replace_sitk_with_numpy(labels_sitk, labels_seg)
     
     # measure distance between edges of original and new segmentations
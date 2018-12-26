sanderslab/magellanmapper

commit fd60c9f5d2a38bc7667e9e6099f5a674b1c929cb

Measure single label metrics for experimental images
Extend measurement of labels metrics from atlases to individual experiments. This functionality essentially replicates volumes_by_id when finding volumes and densities for level "None" but uses multiprocessing during processing of each experiment rather than only for multiple experiments. Data bookkeping is also simplified through a Pandas data frame, which also allows direct output of this "None" level to CSV. Corresponding sides can also be combined into a single label.

clrbrain/vols.py
@@ -183,8 +183,9 @@ class LabelMetrics(object):
             blobs_per_px = cls.heat_map[label_mask]
             var_dens = np.std(blobs_per_px)
             blobs = np.sum(blobs_per_px)
-        print("variation within label {} (size {}): intensity: {}, density: {}"
-              .format(label_id, label_size, var_inten, var_dens))
+        print("variation within label {} (size {}): intensity: {}, "
+              "density: {}, blobs: {}".format(
+                  label_id, label_size, var_inten, var_dens, blobs))
         return label_id, var_inten, label_size, var_dens, blobs
 
     @classmethod
@@ -207,7 +208,9 @@ class LabelMetrics(object):
         return label_id, mean_dist, region_dists.size
 
 def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge, 
-                           labels_edge, dist_to_orig, heat_map=None):
+                           labels_edge, dist_to_orig, heat_map=None, 
+                           spacing=None, condition=None, unit_factor=None, 
+                           combine_sides=True, label_ids=None):
     """Compute metrics such as variation and distances within regions 
     based on maps corresponding to labels image.
     
@@ -221,11 +224,24 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
             in the same placement as in ``labels_edge``.
         heat_map: Numpy array as a density map; defaults to None to ignore 
             density measurements.
+        spacing: Sequence of image spacing for each pixel in the images.
+        unit_factor: Factor by which volumes will be divided to adjust units; 
+            defaults to None.
+        combine_stats: True to combine corresponding labels from opposite 
+            sides of the sample; defaults to True. Corresponding labels 
+            are assumed to have the same absolute numerical number and 
+            differ only in signage.
+        labels_ids: Sequence of label IDs to include. Defaults to None, 
+            in which case the labels will be taken from unique values 
+            in ``labels_img_np``.
     
     Returns:
         Pandas data frame of the regions and weighted means for the metrics.
     """
     start_time = time()
+    physical_mult = None
+    if spacing is not None:
+        physical_mult = np.prod(spacing)
     
     # use a class to set and process the label without having to 
     # reference the labels image as a global variable
@@ -235,30 +251,40 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
         heat_map)
     
     metrics = {}
-    cols = ("Sample", "Region", "Nuclei", "VarNuclei", "VarIntensity", 
-            "EdgeDist")
+    cols = ("Sample", "Region", "Volume", "Nuclei", "Density", "VarNuclei", 
+            "VarIntensity", "EdgeDist")
     pool = mp.Pool()
     pool_results = []
-    label_ids = np.unique(labels_img_np)
+    if label_ids is None:
+        label_ids = np.unique(labels_img_np)
+        if combine_sides: label_ids = label_ids[label_ids >= 0]
     for label_id in label_ids:
         # include corresponding labels from opposite sides while skipping 
         # background
         if label_id == 0: continue
+        if combine_sides: label_id = [label_id, -label_id]
         pool_results.append(
             pool.apply_async(
-                label_metrics.label_metrics, args=([label_id, -label_id], )))
+                label_metrics.label_metrics, args=(label_id, )))
     tot_dist = 0
     tot_edges = 0
     tot_var_inten = 0
     tot_var_dens = 0
     tot_vol = 0
+    tot_vol_physical = 0
     tot_nuc = 0
     for result in pool_results:
         # get metrics by label
         #label_id, blobs, var_dens, var_inten, dist = result.get()
         (label_id, var_inten, label_size, var_dens, nuc, edge_dist, 
          edge_size) = result.get()
-        vals = (sample, label_id[0], nuc, var_dens, var_inten, edge_dist)
+        vol_physical = label_size
+        if physical_mult is not None:
+            vol_physical *= physical_mult
+        if unit_factor is not None:
+            vol_physical /= unit_factor
+        vals = (sample, condition, label_id[0], vol_physical, nuc, 
+                nuc / vol_physical, var_dens, var_inten, edge_dist)
         for col, val in zip(cols, vals):
             metrics.setdefault(col, []).append(val)
         
@@ -267,6 +293,7 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
         tot_edges += edge_size
         tot_var_inten += var_inten * label_size
         tot_vol += label_size
+        tot_vol_physical += vol_physical
         if nuc is not None:
             tot_var_dens += var_dens * label_size
             tot_nuc += nuc
@@ -274,11 +301,13 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
     pool.join()
     
     # add row for total metrics from weighted means
-    vals = (sample, "all", tot_nuc, tot_var_dens / tot_vol, 
+    vals = (sample, condition, "all", tot_vol_physical, tot_nuc, 
+            tot_nuc / tot_vol_physical, tot_var_dens / tot_vol, 
             tot_var_inten / tot_vol, tot_dist / tot_edges)
     for col, val in zip(cols, vals):
         metrics.setdefault(col, []).append(val)
     df = pd.DataFrame(metrics)
+    df["Nuclei"] = df["Nuclei"].astype(int)
     print(df.to_csv())
     
     print("time elapsed to measure variation:", time() - start_time)

clrbrain/config.py
@@ -845,7 +845,7 @@ REGISTER_TYPES = (
     "single", "group", "overlays", "volumes", "densities", "export_vols", 
     "export_regions", "new_atlas", "import_atlas", "export_common_labels", 
     "make_edge_images", "reg_labels_to_atlas", "merge_atlas_segs", 
-    "make_edge_images_exp")
+    "make_edge_images_exp", "vol_stats")
 register_type = None
 ABA_NAME = "name"
 VOL_KEY = "volume"

clrbrain/register.py
@@ -86,6 +86,8 @@ IMG_LABELS_SEG = "annotationSeg.mhd"
 IMG_LABELS_SEG_DIST = "annotationSegDist.mhd"
 _SEG_TYPES = {IMG_LABELS_DIST: "manual", IMG_LABELS_SEG_DIST: "waterfall"}
 
+SAMPLE_VOLS = "vols_by_sample"
+
 NODE = "node"
 PARENT_IDS = "parent_ids"
 MIRRORED = "mirrored"
@@ -3151,6 +3153,69 @@ def group_volumes(labels_ref_lookup, vol_dicts):
     #print("grouped_vol_dict: {}".format(grouped_vol_dict))
     return grouped_vol_dict
 
+def volumes_by_id2(img_paths, labels_ref_lookup, suffix=None, unit_factor=None):
+    """Get volumes and additional label metrics for each single labels ID.
+    
+    Args:
+        img_paths: Sequence of images.
+        labels_ref_lookup: The labels reference lookup, assumed to be an 
+            OrderedDict generated by :func:`create_reverse_lookup` to look up 
+            by ID while preserving key order to ensure that parents of any 
+            child will be reached prior to the child.
+        suffix: Modifier to append to end of ``img_path`` basename for 
+            registered image files that were output to a modified name; 
+            defaults to None to use "original" as the condition.
+        unit_factor: Factor by which volumes will be divided to adjust units; 
+            defaults to None.
+    
+    Returns:
+        Pandas data frame of volume-related metrics for each sample.
+    """
+    start_time = time()
+    
+    # prepare data frame output path and condition column based on suffix
+    out_path = SAMPLE_VOLS
+    if suffix is None:
+        condition = "original" 
+    else:
+        condition = config.suffix.replace("_", "")
+        out_path += config.suffix
+    
+    # use all labels in reference to ensure that no label is missing, even 
+    # if labels images do not contain all of these label
+    label_ids = list(labels_ref_lookup.keys())
+    dfs = []
+    for img_path in img_paths:
+        # adjust image path with suffix
+        mod_path = img_path
+        if suffix is not None:
+            mod_path = lib_clrbrain.insert_before_ext(img_path, suffix)
+        
+        # open images registered to the main image
+        atlas_sitk = load_registered_img(
+            mod_path, get_sitk=True, reg_name=IMG_ATLAS)
+        atlas_img_np = sitk.GetArrayFromImage(atlas_sitk)
+        atlas_edge = load_registered_img(mod_path, reg_name=IMG_ATLAS_EDGE)
+        labels_img_np = load_registered_img(mod_path, reg_name=IMG_LABELS_TRUNC)
+        labels_edge = load_registered_img(mod_path,reg_name=IMG_LABELS_EDGE)
+        dist_to_orig = load_registered_img(mod_path,reg_name=IMG_LABELS_DIST)
+        heat_map = load_registered_img(mod_path,reg_name=IMG_HEAT_MAP)
+        print("tot blobs", np.sum(heat_map))
+        
+        # measure stats per label for the given sample
+        sample = lib_clrbrain.get_filename_without_ext(mod_path)
+        df = vols.measure_labels_metrics(
+            sample, atlas_img_np, labels_img_np, atlas_edge, 
+            labels_edge, dist_to_orig, heat_map, atlas_sitk.GetSpacing(), 
+            condition, unit_factor, True, label_ids)
+        dfs.append(df)
+    
+    # combine data frames from all samples
+    stats.data_frames_to_csv(dfs, out_path)
+    print("time elapsed for volumes by ID: {}".format(time() - start_time))
+    return dfs
+
+
 def export_region_ids(labels_ref_lookup, path, level):
     """Export region IDs from annotation reference reverse mapped dictionary 
     to CSV file.
@@ -3539,3 +3604,11 @@ if __name__ == "__main__":
     elif config.register_type == config.REGISTER_TYPES[12]:
         # merge various forms of atlas segmentations
         merge_atlas_segmentations(config.filename, show=show)
+
+    elif config.register_type == config.REGISTER_TYPES[14]:
+        # volumes stats
+        # TODO: replace volumes/densities function
+        ref = load_labels_ref(config.load_labels)
+        labels_ref_lookup = create_aba_reverse_lookup(ref)
+        volumes_by_id2(
+            config.filenames, labels_ref_lookup, config.suffix, unit_factor)

commit 92c471984a2cfc27720d91d9bd8d988fda805ceb

Fix regressions in measuring label metrics
Commit fd60c9f introduced a few regressions, fixed here:

- Fix exception during label metric density calculation if the number of blobs were 0; now default the density to None and avoid calculating if blobs are None
- Remove casting "Nuclei" column to int in case values are missing
- Fix missing "Condition" header

clrbrain/vols.py
@@ -251,8 +251,8 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
         heat_map)
     
     metrics = {}
-    cols = ("Sample", "Region", "Volume", "Nuclei", "Density", "VarNuclei", 
-            "VarIntensity", "EdgeDist")
+    cols = ("Sample", "Condition", "Region", "Volume", "Nuclei", "Density", 
+            "VarNuclei", "VarIntensity", "EdgeDist")
     pool = mp.Pool()
     pool_results = []
     if label_ids is None:
@@ -283,8 +283,11 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
             vol_physical *= physical_mult
         if unit_factor is not None:
             vol_physical /= unit_factor
+        density = None
+        if nuc is not None:
+            density = nuc / vol_physical
         vals = (sample, condition, label_id[0], vol_physical, nuc, 
-                nuc / vol_physical, var_dens, var_inten, edge_dist)
+                density, var_dens, var_inten, edge_dist)
         for col, val in zip(cols, vals):
             metrics.setdefault(col, []).append(val)
         
@@ -307,7 +310,6 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np, atlas_edge,
     for col, val in zip(cols, vals):
         metrics.setdefault(col, []).append(val)
     df = pd.DataFrame(metrics)
-    df["Nuclei"] = df["Nuclei"].astype(int)
     print(df.to_csv())
     
     print("time elapsed to measure variation:", time() - start_time)
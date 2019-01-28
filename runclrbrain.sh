sanderslab/magellanmapper

commit e3f94fd2b38c1b3c343414666c8b2bae571768ee

Option to set ImageJ/Fiji executable path as command-line argument
- "-f" option in runclrbrain.sh and "-i" in stitch.sh, differing because of existing options, to set path to executable
- Replace platform detection with library function in stitch.sh
- Set base directory based on absolute path in case scripts are called from relative paths

runclrbrain.sh
@@ -14,6 +14,8 @@ instead.
 Arguments:
     -h: Show help documentation.
     -i [path]: Set image path.
+    -f [path]: Path to ImageJ/Fiji executable file. If not given, the 
+        path will be auto-detected as described in stitch.sh.
     -a [path]: Set AWS S3 path (excluding s://)
     -p [pipeline]: Set pipeline, which takes precedence over individual 
         pathways.
@@ -97,6 +99,9 @@ url_notify=""
 # Server clean-up, to perform post-processing tasks including 
 # upload and shutdown
 clean_up=0
+
+# ImageJ/Fiji main executable path
+ij=""
 
 # Supported compression formats
 COMPRESSION_EXTS=("tar.zst" "zip")
@@ -250,7 +255,7 @@ upload_images() {
 
 # override pathway settings with user arguments
 OPTIND=1
-while getopts hi:a:p:s:t:w:o:n:cz:m: opt; do
+while getopts hi:a:p:s:t:w:o:n:cz:m:f: opt; do
     case $opt in
         h)  echo "$HELP"
             exit 0
@@ -288,6 +293,9 @@ while getopts hi:a:p:s:t:w:o:n:cz:m: opt; do
         z)  size="$OPTARG"
             echo "Set size to $size"
             ;;
+        f)  ij="$OPTARG"
+            echo "Set ImageJ/Fiji path to $ij"
+            ;;
         :)  echo "Option -$OPTARG requires an argument"
             exit 1
             ;;
@@ -313,7 +321,7 @@ s3_exp_path=s3://"${S3_DIR}/${EXP}"
 # run from script's directory
 BASE_DIR="`dirname $0`"
 cd "$BASE_DIR"
-echo $PWD
+BASE_DIR="$PWD"
 
 # set pat combinations for common grouped pathways
 if [[ "$pipeline" = "${PIPELINES[0]}" ]]; then
@@ -417,11 +425,11 @@ if [[ "$stitch_pathway" = "${STITCH_PATHWAYS[0]}" ]]; then
     # Replace the tile parameters with your image's setup; set up tile 
     # configuration manually and compute alignment refinement
     python -m stitch.tile_config --img "$NAME" --target_dir "$OUT_DIR" --cols 6 --rows 7 --size 1920,1920,1000 --overlap 0.1 --directionality bi --start_direction right
-    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 0
+    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 0 -i "$ij"
     
     # Before the next steps, please manually check alignments to ensure that they 
     # fit properly, especially since unregistered tiles may be shifted to (0, 0, 0)
-    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 1
+    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 1 -i "$ij"
     python -u -m clrbrain.cli --img "$TIFF_DIR" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --channel 0 --proc importonly
     clr_img="${OUT_DIR}/${OUT_NAME_BASE}.${EXT}"
     
@@ -432,7 +440,7 @@ elif [[ "$stitch_pathway" = "${STITCH_PATHWAYS[1]}" ]]; then
     
     # Import file into BigStitcher HDF5 format (warning: large file, just 
     # under size of original file) and find alignments
-    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 0
+    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 0 -i "$ij"
     
     # notify user via Slack and open ImageJ/Fiji for review, which will 
     # also keep script from continuing until user closes ImageJ/Fiji 
@@ -443,11 +451,11 @@ elif [[ "$stitch_pathway" = "${STITCH_PATHWAYS[1]}" ]]; then
     fi
     echo "=================================="
     echo "$msg"
-    ./stitch.sh -s "none"
+    ./stitch.sh -s "none" -i "$ij"
     
     # Before writing stitched file, advise checking alignments; when 
     # satisfied, then run this fusion step
-    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 1
+    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 1 -i "$ij"
     
     # Rename output file(s):
     FUSED="fused_tp_0"

stitch.sh
@@ -8,6 +8,9 @@ Stitch files using ImageJ/Fiji plugin.
 Arguments:
   -h: Show help and exit.
   -f: Path to image file.
+  -i [path]: Path to ImageJ/Fiji executable file. If not given, the 
+      the path will be auto-detected, assuming that the app folder is in 
+      /Applications on Mac or the parent folder of Clrbrain on Windows/Linux.
   -w [0|1|2]: 0 = Stitch but do not write fused file. Stitching 
       plugin will compute overlap and write registered tile 
       configuration. BigStitcher will import and calculate 
@@ -43,18 +46,14 @@ To run in BigStitcher mode:
 -Run ./stitch.sh -f \"/path/to/img.czi\" -b
 "
 
-# run from parent directory
-base_dir="`dirname $0`"
-cd "$base_dir"
-echo $PWD
-
 write_fused=2
 out_dir=""
 STITCH_TYPES=("none" "stitching" "bigstitcher")
 stitch=STITCH_TYPES[2]
+ij=""
 
 OPTIND=1
-while getopts hf:w:s: opt; do
+while getopts hf:w:s:i: opt; do
     case $opt in
         h)  echo $HELP
             exit 0
@@ -68,6 +67,9 @@ while getopts hf:w:s: opt; do
         s)  stitch="$OPTARG"
             echo "Set stitch type to $stitch"
             ;;
+        i)  ij="$OPTARG"
+            echo "Set ImageJ/Fiji path to to $ij"
+            ;;
         :)  echo "Option -$OPTARG requires an argument"
             exit 1
             ;;
@@ -79,32 +81,28 @@ done
 shift "$((OPTIND-1))"
 EXTRA_ARGS="$@"
 
-# find ImageJ binary name; assumes Fiji.app folder is either in 
-# the standard Mac Applications directory or in the Clrbrain 
-# parent directory
-echo -n "Detecting environment..."
-SYSTEM=`uname -a`
-bit="32"
-if [[ "$SYSTEM" =~ "x86_64" ]]
-then
-    bit="64"
+# run from script directory
+BASE_DIR="`dirname $0`"
+cd "$BASE_DIR"
+BASE_DIR="$PWD"
+
+# load dependencies
+source libclr.sh
+
+detect_platform
+if [[ -z "$ij" ]]; then
+    # auto-detect ImageJ binary path if not already set; assume that 
+    # Fiji.app folder is in the standard Mac Applications directory (Mac)
+    # or in the Clrbrain parent directory (Windows/Linux)
+    if [[ "$os" = "Windows" ]]; then
+        ij=../Fiji.app/ImageJ-win$bit
+    elif [[ "$os" = "MacOSX" ]]; then
+        ij=/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx
+    elif [[ "$os" = "Linux" ]]; then
+        ij=../Fiji.app/ImageJ-linux$bit
+    fi
 fi
-platform=""
-if [[ "$SYSTEM" =~ "CYGWIN" ]] || [[ "$SYSTEM" =~ "WINDOWS" ]]
-then
-    platform="Windows"
-    IJ=../Fiji.app/ImageJ-win$bit
-elif [[ "$SYSTEM" =~ "Darwin" ]]
-then
-    platform="MacOSX"
-    IJ=/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx
-elif [[ "$SYSTEM" =~ "Linux" ]]
-then
-    platform="Linux"
-    IJ=../Fiji.app/ImageJ-linux$bit
-fi
-echo "will use $platform platform with $bit bit for ImageJ"
-echo "Assumes Fiji executable is located at $IJ"
+echo "Assumes Fiji executable is located at $ij"
 
 # calculates memory to reserve based on image file size with generous
 # extra padding room (TODO: check if too much for large files)
@@ -125,7 +123,7 @@ echo "Reserving $mem MB of memory"
 
 if [[ "$stitch" == "${STITCH_TYPES[1]}" ]]; then
     # Fiji Stitching plugin; does not appear to work when fed a separate script in "-macro" mode
-    $IJ --mem "$mem"m --headless --run stitch/ij_stitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
+    "$ij" --mem "$mem"m --headless --run stitch/ij_stitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
     
     # manually move files to output directory since specifying this directory
     # within the Stitching plugin requires the tile configuration file to be
@@ -145,9 +143,9 @@ if [[ "$stitch" == "${STITCH_TYPES[1]}" ]]; then
     mv "$in_dir"/img_t* "$out_dir"
 elif [[ "$stitch" == "${STITCH_TYPES[2]}" ]]; then
     # BigStitcher; not working in headless mode so will require GUI availability
-    $IJ --ij2 --mem "$mem"m --run stitch/ij_bigstitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
+    "$ij" --ij2 --mem "$mem"m --run stitch/ij_bigstitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
 else
     # no stitching, just open ImageJ/Fiji
-    $IJ --ij2
+    "$ij" --ij2
 fi
 

commit b815107998676a47dc600a9a71ed967680ca15f6

Fix regression in applying ImageJ script with custom ImageJ launch script
Commit e3f94fd introduced a regression where a custom ImageJ launch script could be given, but this script would typically launch ImageJ without passing additional arguements to it such as those given in the stitch shell script. To fix this issue and avoid the need for a custom launch script, change the launch script arguement to a Java home argument that will be passed to ImageJ.

runclrbrain.sh
@@ -14,8 +14,7 @@ instead.
 Arguments:
     -h: Show help documentation.
     -i [path]: Set image path.
-    -f [path]: Path to ImageJ/Fiji executable file. If not given, the 
-        path will be auto-detected as described in stitch.sh.
+    -j [path]: Path to custom JAVA_HOME for ImageJ/Fiji.
     -a [path]: Set AWS S3 path (excluding s://)
     -p [pipeline]: Set pipeline, which takes precedence over individual 
         pathways.
@@ -100,8 +99,8 @@ url_notify=""
 # upload and shutdown
 clean_up=0
 
-# ImageJ/Fiji main executable path
-ij=""
+# Java home for ImageJ/Fiji
+java_home=""
 
 # Supported compression formats
 COMPRESSION_EXTS=("tar.zst" "zip")
@@ -255,7 +254,7 @@ upload_images() {
 
 # override pathway settings with user arguments
 OPTIND=1
-while getopts hi:a:p:s:t:w:o:n:cz:m:f: opt; do
+while getopts hi:a:p:s:t:w:o:n:cz:m:j: opt; do
     case $opt in
         h)  echo "$HELP"
             exit 0
@@ -293,8 +292,8 @@ while getopts hi:a:p:s:t:w:o:n:cz:m:f: opt; do
         z)  size="$OPTARG"
             echo "Set size to $size"
             ;;
-        f)  ij="$OPTARG"
-            echo "Set ImageJ/Fiji path to $ij"
+        j)  java_home="$OPTARG"
+            echo "Set JAVA_HOME for ImageJ/Fiji to $java_home"
             ;;
         :)  echo "Option -$OPTARG requires an argument"
             exit 1
@@ -425,11 +424,11 @@ if [[ "$stitch_pathway" = "${STITCH_PATHWAYS[0]}" ]]; then
     # Replace the tile parameters with your image's setup; set up tile 
     # configuration manually and compute alignment refinement
     python -m stitch.tile_config --img "$NAME" --target_dir "$OUT_DIR" --cols 6 --rows 7 --size 1920,1920,1000 --overlap 0.1 --directionality bi --start_direction right
-    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 0 -i "$ij"
+    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 0 -j "$java_home"
     
     # Before the next steps, please manually check alignments to ensure that they 
     # fit properly, especially since unregistered tiles may be shifted to (0, 0, 0)
-    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 1 -i "$ij"
+    ./stitch.sh -f "$IMG" -o "$TIFF_DIR" -s "stitching" -w 1 -j "$java_home"
     python -u -m clrbrain.cli --img "$TIFF_DIR" --res "$RESOLUTIONS" --mag "$MAGNIFICATION" --zoom "$ZOOM" -v --channel 0 --proc importonly
     clr_img="${OUT_DIR}/${OUT_NAME_BASE}.${EXT}"
     
@@ -440,7 +439,7 @@ elif [[ "$stitch_pathway" = "${STITCH_PATHWAYS[1]}" ]]; then
     
     # Import file into BigStitcher HDF5 format (warning: large file, just 
     # under size of original file) and find alignments
-    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 0 -i "$ij"
+    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 0 -j "$java_home"
     
     # notify user via Slack and open ImageJ/Fiji for review, which will 
     # also keep script from continuing until user closes ImageJ/Fiji 
@@ -451,11 +450,11 @@ elif [[ "$stitch_pathway" = "${STITCH_PATHWAYS[1]}" ]]; then
     fi
     echo "=================================="
     echo "$msg"
-    ./stitch.sh -s "none" -i "$ij"
+    ./stitch.sh -s "none" -j "$java_home"
     
     # Before writing stitched file, advise checking alignments; when 
     # satisfied, then run this fusion step
-    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 1 -i "$ij"
+    ./stitch.sh -f "$IMG" -s "bigstitcher" -w 1 -j "$java_home"
     
     # Rename output file(s):
     FUSED="fused_tp_0"

stitch.sh
@@ -7,10 +7,9 @@ Stitch files using ImageJ/Fiji plugin.
 
 Arguments:
   -h: Show help and exit.
-  -f: Path to image file.
-  -i [path]: Path to ImageJ/Fiji executable file. If not given, the 
-      the path will be auto-detected, assuming that the app folder is in 
-      /Applications on Mac or the parent folder of Clrbrain on Windows/Linux.
+  -f [path]: Path to image file.
+  -j [path]: Path to custom JAVA_HOME for ImageJ/Fiji. If not given 
+      empty, the default Java installation will be used instead.
   -w [0|1|2]: 0 = Stitch but do not write fused file. Stitching 
       plugin will compute overlap and write registered tile 
       configuration. BigStitcher will import and calculate 
@@ -50,10 +49,10 @@ write_fused=2
 out_dir=""
 STITCH_TYPES=("none" "stitching" "bigstitcher")
 stitch=STITCH_TYPES[2]
-ij=""
+java_home=""
 
 OPTIND=1
-while getopts hf:w:s:i: opt; do
+while getopts hf:w:s:j: opt; do
     case $opt in
         h)  echo $HELP
             exit 0
@@ -67,8 +66,8 @@ while getopts hf:w:s:i: opt; do
         s)  stitch="$OPTARG"
             echo "Set stitch type to $stitch"
             ;;
-        i)  ij="$OPTARG"
-            echo "Set ImageJ/Fiji path to to $ij"
+        j)  java_home="$OPTARG"
+            echo "Set JAVA_HOME for ImageJ/Fiji to to $j"
             ;;
         :)  echo "Option -$OPTARG requires an argument"
             exit 1
@@ -90,23 +89,26 @@ BASE_DIR="$PWD"
 source libclr.sh
 
 detect_platform
-if [[ -z "$ij" ]]; then
-    # auto-detect ImageJ binary path if not already set; assume that 
-    # Fiji.app folder is in the standard Mac Applications directory (Mac)
-    # or in the Clrbrain parent directory (Windows/Linux)
-    bit_short="64"
-    if [[ "$bit" =~ "32" ]]; then
-        bit_short="32"
-    fi
-    if [[ "$os" = "Windows" ]]; then
-        ij="../Fiji.app/ImageJ-win$bit_short"
-    elif [[ "$os" = "MacOSX" ]]; then
-        ij="/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx"
-    elif [[ "$os" = "Linux" ]]; then
-        ij="../Fiji.app/ImageJ-linux$bit_short"
-    fi
+# auto-detect ImageJ binary path if not already set; assume that 
+# Fiji.app folder is in the standard Mac Applications directory (Mac)
+# or in the Clrbrain parent directory (Windows/Linux)
+bit_short="64"
+if [[ "$bit" =~ "32" ]]; then
+    bit_short="32"
 fi
-echo "Assumes Fiji executable is located at $ij"
+if [[ "$os" = "Windows" ]]; then
+    ij="../Fiji.app/ImageJ-win$bit_short"
+elif [[ "$os" = "MacOSX" ]]; then
+    ij="/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx"
+elif [[ "$os" = "Linux" ]]; then
+    ij="../Fiji.app/ImageJ-linux$bit_short"
+fi
+ij=("$ij")
+if [[ ! -z "$java_home" ]]; then
+    ij+=(--java-home "$java_home")
+fi
+echo "Will run ImageJ/Fiji executable as:"
+echo "${ij[@]}"
 
 # calculates memory to reserve based on image file size with generous
 # extra padding room (TODO: check if too much for large files)
@@ -127,7 +129,7 @@ echo "Reserving $mem MB of memory"
 
 if [[ "$stitch" == "${STITCH_TYPES[1]}" ]]; then
     # Fiji Stitching plugin; does not appear to work when fed a separate script in "-macro" mode
-    "$ij" --mem "$mem"m --headless --run stitch/ij_stitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
+    "${ij[@]}" --mem "$mem"m --headless --run stitch/ij_stitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
     
     # manually move files to output directory since specifying this directory
     # within the Stitching plugin requires the tile configuration file to be
@@ -147,9 +149,9 @@ if [[ "$stitch" == "${STITCH_TYPES[1]}" ]]; then
     mv "$in_dir"/img_t* "$out_dir"
 elif [[ "$stitch" == "${STITCH_TYPES[2]}" ]]; then
     # BigStitcher; not working in headless mode so will require GUI availability
-    "$ij" --ij2 --mem "$mem"m --run stitch/ij_bigstitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
+    "${ij[@]}" --ij2 --mem "$mem"m --run stitch/ij_bigstitch.py 'in_file="'"$IMG"'",write_fused="'"$write_fused"'"'
 else
     # no stitching, just open ImageJ/Fiji
-    "$ij" --ij2
+    "${ij[@]}" --ij2
 fi
 
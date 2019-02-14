sanderslab/magellanmapper

commit 35f651a6b7f10bc550ed15ab2622fc8342a7d341

Measure DSC of original images and save to CSV file
- Use the original files for DSC caculation during atlas import when the profile includes the "_raw" modifier
- Export DSC to file via Pandas data frame
- Only include vector of regions to ignore when importing levels to avoid skipping main region for summary stats
- Make "geno" column option for jitter plots
- Remove side sorting so that the first listed condition, typically "original", will be displayed first
- Replace underscores with spaces in jitter plot y-axis label

stats/clrstats.R
@@ -21,7 +21,7 @@ kModel = c("logit", "linregr", "gee", "logit.ord", "ttest", "wilcoxon",
 
 # measurements, which correspond to columns in main data frame
 kMeas = c("Volume", "Density", "Nuclei", "VarNuclei", "VarIntensity", 
-          "EdgeDistSum", "EdgeDistMean")
+          "EdgeDistSum", "EdgeDistMean", "DSC_atlas_labels")
 
 # ordered genotype levels
 kGenoLevels <- c(0, 0.5, 1)
@@ -34,7 +34,7 @@ kRegionsIgnore <- c(15564)
 
 # raw values from Clrbrain
 kStatsFilesIn <- c("vols_by_sample.csv", "vols_by_sample_levels.csv", 
-                   "vols_by_sample_summary.csv")
+                   "vols_by_sample_summary.csv", "dsc_summary.csv")
 kStatsPathOut <- "../vols_stats.csv" # output stats
 
 # region-ID map from Clrbrain, which should contain all regions including 
@@ -263,7 +263,8 @@ setupPairing <- function(df.region, col, split.col) {
   return(df.filtered)
 }
 
-statsByRegion <- function(df, col, model, split.by.side=TRUE) {
+statsByRegion <- function(df, col, model, split.by.side=TRUE, 
+                          regions.ignore=NULL) {
   # Calculate statistics given by region for columns starting with the given 
   # string using the selected model.
   #
@@ -278,6 +279,7 @@ statsByRegion <- function(df, col, model, split.by.side=TRUE) {
   #   split.by.side: True to keep data split by sides, False to combine 
   #     corresponding regions from opposite sides into single regions; 
   #     defaults to True.
+  #   regions.ignore: Vector of regions to ignore; default to NULL.
   
   # find all regions
   regions <- unique(df$Region)
@@ -289,7 +291,7 @@ statsByRegion <- function(df, col, model, split.by.side=TRUE) {
   
   for (i in seq_along(regions)) {
     region <- regions[i]
-    if (is.element(region, kRegionsIgnore)) next
+    if (!is.null(regions.ignore) & is.element(region, kRegionsIgnore)) next
     
     # filter data frame for the given region and get mask to filter out 
     # NaNs and 0's as they indicate that the label for the region was suppressed
@@ -407,18 +409,22 @@ jitterPlot <- function(df.region, col, title, split.by.side=TRUE,
     # default column name by which to split
     split.col <- "Side"
   }
-  genos <- df.region$Geno
+  if (is.element("Geno", names(df.region))) {
+    genos <- df.region$Geno
+  } else {
+    genos <- c("")
+  }
   genos.unique <- sort(unique(genos))
   multiple.geno <- length(genos.unique) > 1
   sides <- df.region[[split.col]]
-  sides.unique <- sort(unique(sides))
+  sides.unique <- unique(sides)
   if (!split.by.side | length(sides.unique) == 0) {
     # use a single side for one for-loop pass
     sides.unique = c("")
   }
   vals <- df.region[[col]]
   maxes <- c(length(genos.unique) * length(sides.unique), max(vals))
-  plot(NULL, frame.plot=TRUE, xlab=title, ylab=col, xaxt="n", 
+  plot(NULL, frame.plot=TRUE, xlab=title, ylab=gsub("_", " ", col), xaxt="n", 
            xlim=range(-0.5, maxes[1] - 0.5), ylim=range(0, maxes[2]))
   names <- list()
   i <- 0
@@ -655,7 +661,13 @@ calcVolStats <- function(path.in, path.out, meas, model, region.ids,
   cat("\n\n")
   
   # calculate stats, filter out NAs and extract effects and p-values
-  stats <- statsByRegion(df, meas, model, split.by.side=split.by.side)
+  regions.ignore <- NULL
+  if (path.in == kStatsFilesIn[2]) {
+    # ignore duplicate when including all levels
+    regions.ignore <- kRegionsIgnore
+  }
+  stats <- statsByRegion(
+    df, meas, model, split.by.side=split.by.side, regions.ignore=regions.ignore)
   stats.filtered <- filterStats(stats, corr=corr)
   stats.filtered <- merge(stats.filtered, region.ids, by="Region")
   #print(stats.filtered)

clrbrain/register.py
@@ -1267,11 +1267,29 @@ def match_atlas_labels(img_atlas, img_labels, flip=False):
     return img_atlas, img_labels, img_borders
 
 def import_atlas(atlas_dir, show=True):
+    """Import atlas from the given directory, processing it according 
+    to the register settings specified at :attr:``config.register_settings``.
+    
+    The imported atlas will be saved to a directory of the same path as 
+    ``atlas_dir`` except with ``_import`` appended to the end. DSC 
+    will be calculated and saved as a CSV file in this directory as well.
+    
+    Args:
+        atlas_dir: Path to atlas directory.
+        show: True to show the imported atlas.
+    """
     # load atlas and corresponding labels
     img_atlas = sitk.ReadImage(os.path.join(atlas_dir, IMG_ATLAS))
     img_labels = sitk.ReadImage(os.path.join(atlas_dir, IMG_LABELS))
+    orig = "_raw" in config.register_settings["settings_name"]
+    if orig:
+        # baseline DSC of atlas to labels before any processing
+        cond = "original"  
+        dsc = _measure_overlap_combined_labels(img_atlas, img_labels)
+    else:
+        # defer DSC until after processing
+        cond = "extended" 
     
-    #img_labels_np = None
     img_atlas, img_labels, img_borders = match_atlas_labels(
         img_atlas, img_labels)
     
@@ -1288,7 +1306,17 @@ def import_atlas(atlas_dir, show=True):
     print("number of labels: {}".format(label_ids.size))
     print(label_ids)
     
-    _measure_overlap_combined_labels(img_atlas, img_labels)
+    # prep export paths
+    target_dir = atlas_dir + "_import"
+    basename = os.path.basename(atlas_dir)
+    df_path = os.path.join(target_dir, basename) + "_stats_dsc.csv"
+    name_prefix = os.path.join(target_dir, basename) + ".czi"
+    
+    # measure DSC if processed and prep dict for data frame
+    if not orig:
+        dsc = _measure_overlap_combined_labels(img_atlas, img_labels)
+    metrics = {"Sample": [basename], "Region": "all", "Condition": cond, 
+               "DSC_atlas_labels": [dsc]}
     
     if show:
        sitk.Show(img_atlas)
@@ -1296,8 +1324,6 @@ def import_atlas(atlas_dir, show=True):
     
     # write images with atlas saved as Clrbrain/Numpy format to 
     # allow opening as an image within Clrbrain alongside the labels image
-    target_dir = atlas_dir + "_import"
-    name_prefix = os.path.join(target_dir, os.path.basename(atlas_dir)) + ".czi"
     imgs_write = {
         IMG_ATLAS: img_atlas, IMG_LABELS: img_labels, IMG_BORDERS: img_borders}
     write_reg_images(imgs_write, name_prefix, copy_to_suffix=True)
@@ -1305,6 +1331,7 @@ def import_atlas(atlas_dir, show=True):
     img_ref_np = sitk.GetArrayFromImage(img_atlas)
     img_ref_np = img_ref_np[None]
     importer.save_np_image(img_ref_np, name_prefix, 0)
+    stats.dict_to_data_frame(metrics, df_path)
 
 def register(fixed_file, moving_file_dir, plane=None, flip=False, 
              show_imgs=True, write_imgs=True, name_prefix=None, 
@@ -1562,7 +1589,7 @@ def _measure_overlap_combined_labels(fixed_img, labels_img):
     # when using labels img to curate fixed img
     result_img_for_overlap = make_labels_fg(labels_img)
     print("\nDSC of thresholded fixed image compared with combined labels:")
-    measure_overlap(
+    return measure_overlap(
         fixed_img, result_img_for_overlap, transformed_thresh=1)
 
 def _crop_image(img_np, labels_img, axis, eraser=None):

commit 6b4d2c7c7d6cfae48adeb14be0397a3a8e429e56

Fix ignoring vector of regions to ignore during R stats
Commit 35f651a introduced a regression where no given path would match those of the input paths constant since the given path includes a relative directory. Use the basename of this path instead.

stats/clrstats.R
@@ -734,7 +734,7 @@ calcVolStats <- function(path.in, path.out, meas, model, region.ids,
   
   # calculate stats, filter out NAs and extract effects and p-values
   regions.ignore <- NULL
-  if (path.in == kStatsFilesIn[2]) {
+  if (basename(path.in) == kStatsFilesIn[2]) {
     # ignore duplicate when including all levels
     regions.ignore <- kRegionsIgnore
   }
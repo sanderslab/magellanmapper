fd5d8e6c97dc11e17082ebeab53d1002b69de700
David Young <david@textflex.com>
2019-02-21 10:59:33 +0800

    Export weighted average of pruning ratios
    Calculate weighted mean for pruning ratios based on number of pre-pruned blobs.

--- clrbrain/cli.py
+++ clrbrain/cli.py
@@ -1119,10 +1119,24 @@ def process_file(filename_base, offset, roi_size):
         time_pruning_start = time()
         segments_all, df = _prune_blobs_mp(
             seg_rois, overlap, tol, super_rois, super_rois_offsets, channels)
+        
+        # combine pruning data frames and get weighted mean of ratios
         dfs.append(df)
         df_all = stats.data_frames_to_csv(dfs, "blob_ratios.csv")
+        blob_pruning_means = {}
+        blobs_unpruned = df_all["blobs"]
+        num_blobs_unpruned = np.sum(blobs_unpruned)
+        cols = df_all.columns.tolist()
+        for col in cols[1:]:
+            blob_pruning_means["mean_{}".format(col)] = [
+                np.sum(np.multiply(df_all[col], blobs_unpruned)) 
+                / num_blobs_unpruned]
+        df_means = stats.dict_to_data_frame(
+            blob_pruning_means, "blob_ratios_means.csv")
         print(df_all.to_csv())
+        print(df_means.to_csv())
         pruning_time = time() - time_pruning_start
+        
         '''# report any remaining duplicates
         np.set_printoptions(linewidth=500, threshold=10000000)
         print("all blobs (len {}):".format(len(segments_all)))

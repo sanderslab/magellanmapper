Fix off-by-one errors from rounding blob coordinates
Blob coordinate rounding to rescale blobs from isotropic to original coordinate space can cause off-by-one errors, where the highest blobs may be out of the original region. To correct this error and distribute blobs evenly, simply round down. Also output max blob coordinates after detections.

clrbrain/detector.py
@@ -225,14 +225,14 @@ def shift_blob_abs_coords(blobs, offset):
 def multiply_blob_rel_coords(blobs, factor):
     if blobs is not None:
         rel_coords = blobs[..., :3] * factor
-        blobs[..., :3] = np.around(rel_coords).astype(np.int)
+        blobs[..., :3] = rel_coords.astype(np.int)
     return blobs
 
 def multiply_blob_abs_coords(blobs, factor):
     if blobs is not None:
         start = -1*len(factor)
         abs_coords = blobs[..., start:] * factor
-        blobs[..., start:] = np.around(abs_coords).astype(np.int)
+        blobs[..., start:] = abs_coords.astype(np.int)
     return blobs
 
 def get_blob_confirmed(blob):
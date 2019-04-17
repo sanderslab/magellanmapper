e0b97a2c8a695383f1054f83e5cc02549dde9b50
David Young <david@textflex.com>
2019-04-17 17:54:25 +0800

    Use slightly higher value than target in min/max sigma ROC
    Floating point in `arange` leads to inconsistencies in whether the stop value will be included or not. As a workaround, using a slightly higher stop value will ensure that the rounded value is included. Apply this workaround to min/max sigma ROC tests.

--- clrbrain/config.py
+++ clrbrain/config.py
@@ -972,8 +972,8 @@ roc_dict = OrderedDict([
         #"denoise_size", np.arange(5, 25, 2)
         #("unsharp_strength", np.arange(0.0, 1.1, 0.1)),
         #("tot_var_denoise", (False, True)),
-        #("min_sigma_factor", np.arange(2, 2.7, 0.1)),
-        #("max_sigma_factor", np.arange(2.7, 3.2, 0.1)),
+        #("min_sigma_factor", np.arange(2, 2.71, 0.1)),
+        #("max_sigma_factor", np.arange(2.7, 3.21, 0.1)),
         #("num_sigma", np.arange(5, 16, 1)),
         #("detection_threshold", np.arange(0.001, 0.01, 0.001)),
     ]))

sanderslab/magellanmapper

commit 619fcc42ccc912b780a2388f45af8b10dc3ddf96

Move channel and vmin/vmax setup to image overlay function
- Refactor channel, vim, and vmax setup from `stack` to `plot_support.overlay_images` as common setup for `plot_editor` overview image display, which also incorporates `vmax_overview` setting for `plot_editor`
- Fill vmin with new `config.vmins` attribute for first image

clrbrain/plot_support.py
@@ -30,8 +30,8 @@ def imshow_multichannel(ax, img2d, channel, cmaps, aspect, alpha,
             can be the names of specific maps in :mod:``config``.
         aspect: Aspect ratio.
         alpha: Default alpha transparency level.
-        vim: Imshow vmin level; defaults to None.
-        vmax: Imshow vmax level; defaults to None.
+        vim: Sequence of vmin levels for each channel; defaults to None.
+        vmax: Sequence of vmax levels for each channel; defaults to None.
         origin: Image origin; defaults to None.
         interpolation: Type of interpolation; defaults to None.
         norms: List of normalizations, which should correspond to ``cmaps``.
@@ -75,25 +75,29 @@ def overlay_images(ax, aspect, origin, imgs2d, channels, cmaps, alphas,
     """Show multiple, overlaid images.
     
     Wrapper function calling :func:``imshow_multichannel`` for multiple 
-    images.
+    images. The first image is treated as a sample image with potential 
+    for multiple channels. Subsequent images are typically label images, 
+    which may or may not have multple channels.
     
     Args:
         ax: Axes.
         aspect: Aspect ratio.
         origin: Image origin.
         imgs2d: List of 2D images to display.
-        channels: Either a single channel for all images or a list of 
-            channels corresponding to each image.
+        channels: A list of channels designators for each image, or None 
+            to use :attr:``config.channel`` for the first image and 0 
+            for all subsequent images.
         cmaps: Either a single colormap for all images or a list of 
             colormaps corresponding to each image. Colormaps of type 
             :class:``colormaps.DiscreteColormap`` will have their 
             normalization object applied as well.
         alphas: Either a single alpha for all images or a list of 
             alphas corresponding to each image.
-        vmins: Either a single vmin for all images or a list of 
-            vmins corresponding to each image; defaults to None.
-        vmaxs: Either a single vmax for all images or a list of 
-            vmaxs corresponding to each image; defaults to None.
+        vmins: A list of vmins for each image; defaults to None to use 
+            :attr:``config.vmins`` for the first image and None for all others.
+        vmaxs: A list of vmaxs for each image; defaults to None to use 
+            :attr:``config.vmax_overview`` for the first image and None 
+            for all others.
     
     Returns:
         Nested list containing a list of ``AxesImage`` objects 
@@ -101,18 +105,34 @@ def overlay_images(ax, aspect, origin, imgs2d, channels, cmaps, alphas,
     """
     ax_imgs = []
     num_imgs2d = len(imgs2d)
+    if num_imgs2d < 1: return None
     
-    def ensure_list(val):
-        # convert scalars to lists of repeated vals
-        return [val] * num_imgs2d if not isinstance(val, (tuple, list)) else val
+    def fill(fill_with, chls):
+        # make a sequence with vals corresponding to each 2D image, with 
+        # the first val as another seq corresponding to the given number 
+        # of channels
+        filled = [None] * num_imgs2d
+        if fill_with:
+            filled[0] = lib_clrbrain.pad_seq(list(fill_with), len(chls))
+        return filled
     
-    # convert args that accept scalars but require lists
-    channels = ensure_list(channels)
-    alphas = ensure_list(alphas)
-    vmins = ensure_list(vmins)
-    vmaxs = ensure_list(vmaxs)
+    if channels is None:
+        # channels are designators rather than lists of specific channels
+        channels = [0] * num_imgs2d
+        channels[0] = config.channel
+    if vmins is None or vmaxs is None:
+        # fill vmin/vmax with None for each 2D image and config vals for 
+        # each channel for the first image
+        _, channels_main = plot_3d.setup_channels(imgs2d[0], config.channel, 2)
+        if vmins is None:
+            fill_with = (config.near_min if config.vmins is None 
+                         else config.vmins)
+            vmins = fill(config.vmins, channels_main)
+        if vmaxs is None:
+            vmaxs = fill(config.vmax_overview, channels_main)
     
     for i in range(num_imgs2d):
+        # generate a multichannel display image for each 2D image
         cmap = cmaps[i]
         norm = None
         if isinstance(cmap, colormaps.DiscreteColormap):

clrbrain/plot_editor.py
@@ -1,6 +1,9 @@
-# 2D plot editor
-# Author: David Young, 2018
-"""2D plot editor.
+# 2D overlaid plot editor
+# Author: David Young, 2018, 2019
+"""Editor for 2D plot with overlaid planes.
+
+Integrates with :class:``atlas_editor.AtlasEditor`` for synchronized 3D 
+view of orthogonal planes.
 """
 
 import matplotlib.patches as patches
@@ -125,9 +128,8 @@ class PlotEditor:
                 alphas.append(1)
         
         # overlay all images and set labels for footer value on mouseover
-        # TODO: make main image's vmax adjustable
         ax_imgs = plot_support.overlay_images(
-            self.axes, self.aspect, self.origin, imgs2d, 0, cmaps, alphas)
+            self.axes, self.aspect, self.origin, imgs2d, None, cmaps, alphas)
         self.axes.format_coord = PixelDisplay(imgs2d)
         self.plane_slider.set_val(self.coord[0])
         if len(ax_imgs) > 1: self.ax_img = ax_imgs[1][0]

clrbrain/stack.py
@@ -108,16 +108,7 @@ def _build_stack(images, out_path, process_fnc, rescale, aspect=None,
     # setup imshow parameters
     cmaps = config.process_settings["channel_colors"]
     cmaps_all = [cmaps, *cmaps_labels]
-    imgs_ordered = [None] * len(pool_results)
-    channels = [0] * num_image_types
-    channels[0] = config.channel
-    near_mins = [None] * num_image_types
-    near_mins[0] = config.near_min
-    vmaxs = [None] * num_image_types
-    vmaxs[0] = config.vmax_overview
-    num_alphas = len(config.alphas)
-    alphas = [config.alphas[i] if num_alphas > i else 0.9 
-              for i in range(num_image_types)]
+    alphas = lib_clrbrain.pad_seq(config.alphas, num_image_types, 0.9)
     
     for result in pool_results:
         i, imgs = result.get()
@@ -128,8 +119,7 @@ def _build_stack(images, out_path, process_fnc, rescale, aspect=None,
         # a nested list containing a list for each image, which in turn 
         # contains a list of artists for each channel
         ax_imgs = plot_support.overlay_images(
-            ax, aspect, origin, imgs, channels, cmaps_all, alphas, 
-            near_mins, vmaxs)
+            ax, aspect, origin, imgs, None, cmaps_all, alphas)
         plotted_imgs[i] = np.array(ax_imgs).flatten()
     pool.close()
     pool.join()

commit fb6d31c333523e396a0c065d9a0f747b79c8f6da

Fix overlaying images where the first channel is multichannel
Commit 619fcc4 introduced where filling settings for multichannel images led to errors from None checking a sequence and filling values for only a single channel if one was selected rather than always for all channels, fixed here.

clrbrain/lib_clrbrain.py
@@ -103,7 +103,7 @@ def pad_seq(seq, n, pad=None):
     Args:
         seq: Tuple or list to fill in-place.
         n: Target length.
-        pad: Value with which to fill.
+        pad: Value with which to fill; defaults to None.
     
     Returns:
         A truncated view of ``seq`` if the sequence is longer than ``n`` 

clrbrain/plot_support.py
@@ -108,11 +108,11 @@ def overlay_images(ax, aspect, origin, imgs2d, channels, cmaps, alphas,
     if num_imgs2d < 1: return None
     
     def fill(fill_with, chls):
-        # make a sequence with vals corresponding to each 2D image, with 
-        # the first val as another seq corresponding to the given number 
-        # of channels
+        # make a sequence with vals corresponding to each 2D image, where 
+        # the first val is another seq wholse values correspond to each of 
+        # the channels in that image, starting with fill_with
         filled = [None] * num_imgs2d
-        if fill_with:
+        if fill_with is not None:
             filled[0] = lib_clrbrain.pad_seq(list(fill_with), len(chls))
         return filled
     
@@ -123,7 +123,7 @@ def overlay_images(ax, aspect, origin, imgs2d, channels, cmaps, alphas,
     if vmins is None or vmaxs is None:
         # fill vmin/vmax with None for each 2D image and config vals for 
         # each channel for the first image
-        _, channels_main = plot_3d.setup_channels(imgs2d[0], config.channel, 2)
+        _, channels_main = plot_3d.setup_channels(imgs2d[0], None, 2)
         if vmins is None:
             fill_with = (config.near_min if config.vmins is None 
                          else config.vmins)
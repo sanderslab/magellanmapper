sanderslab/magellanmapper

commit 10fa3c725d162c2134d456f6f869ddb94d2a6215

Replace library method to get extension with splitting method support extensions with multiple periods
`get_filename_ext` essentially duplicated the functionality of `os.path.splitext` and was not widely used. Replace it with a method that expands upon the latter method by support extensions with multiple dots. This new method works under the assumption that certain extensions are merely the start of additional extensions, such as ".tar" for ".tar.gz" and ".nii" for ".nii.gz". Support these particular extension starts when splitting a path to split on the full extension rather than only the last component.

clrbrain/importer.py
@@ -470,7 +470,8 @@ def read_file(filename, series, load=True, z_max=-1,
         image5d, the array of image data. If ``return_info`` is True, a 
         second value a dictionary of image properties will be returned.
     """
-    ext = lib_clrbrain.get_filename_ext(filename)
+    path_split = lib_clrbrain.splitext(filename)
+    ext = path_split[1]
     filename_image5d_npz, filename_info_npz = make_filenames(
         filename, series)
     if load:

clrbrain/lib_clrbrain.py
@@ -23,6 +23,9 @@ _DTYPES = {
     "float": [np.float16, np.float32, np.float64]
 }
 
+# the start of extensions that may have multiple periods
+_EXTENSIONS_MULTIPLE = (".tar", ".nii")
+
 def swap_elements(arr, axis0, axis1, offset=0):
     """Swap elements within an list or tuple.
     
@@ -131,12 +134,28 @@ def insert_before_ext(name, insert, sep=""):
     """
     return "{0}{2}{3}.{1}".format(*name.rsplit(".", 1), sep, insert)
 
-def get_filename_ext(filename):
-    ext = ""
-    filename_split = filename.rsplit(".", 1)
-    if len(filename_split) > 1:
-        ext = filename_split[1]
-    return ext
+def splitext(path):
+    """Split a path at its extension in a way that supports extensions 
+    with multiple periods as identified in :const:``_EXTENSIONS_MULTIPLE``.
+    
+    Args:
+        path: Path to split.
+    
+    Returns:
+        Tuple of path prior to extension and the extension, including 
+        leading period. If an extension start is not found in 
+        :const:``_EXTENSIONS_MULTIPLE``, the path will simply be split 
+        by :meth:``os.path.splitext``.
+    """
+    i = -1
+    for ext in _EXTENSIONS_MULTIPLE:
+        i = path.rfind(ext)
+        if i != -1: break
+    if i == -1:
+        path_split = os.path.splitext(path)
+    else:
+        path_split = (path[:i], path[i:])
+    return path_split
 
 def get_filename_without_ext(path):
     """Get filename without extension.

commit 2d53296a2e86f04deffa12b6846e98f9b26f056e

Fix checking for `.tiff` extensions when import image to Numpy format
Commit 10fa3c7 introduced a regression where checked extensions during import to Numpy/Clrbrain format did not include the preceding dot, fixed here.

clrbrain/importer.py
@@ -555,7 +555,7 @@ def read_file(filename, series, load=True, z_max=-1,
     if offset is None:
         offset = (0, 0, 0) # (x, y, z)
     num_files = 1
-    if ext == "tiff" or ext == "tif":
+    if ext == ".tiff" or ext == ".tif":
         # import multipage TIFFs
         print("Loading multipage TIFF...")
         
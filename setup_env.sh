sanderslab/magellanmapper

commit f217747500a884cde5c8f54010ada53452a2934a

Set strict channel priority when generating Conda environment
`conda env` used to import a `.yml` environment configuration file does not appear to allow setting the channel priority. Rather than setting a global configuration, separate environment generation from `.yml` import by first creating an environment, setting an environment-specific config setting with strict channel priority, and finally importing the `.yml` configuration from with the environment.

- Create a basic environment first to set strict channel priority within it
- Load `.yml` file within the environment
- Remove the environment name from `.yml` files as they are no longer needed when loaded from within the environment and could theoretically load packages into the incorrect environment if the names don't match

setup_env.sh
@@ -36,7 +36,7 @@ env_name=""
 # default .yml files
 ENV_CONFIG="environment.yml"
 ENV_CONFIG_LIGHT="environment_light.yml"
-config_default="$ENV_CONFIG"
+config="$ENV_CONFIG"
 
 build_simple_elastix=0
 lightweight=0
@@ -61,7 +61,7 @@ while getopts hn:slad opt; do
     l)
       lightweight=1
       env_default="$CONDA_ENV_LIGHT"
-      config_default="$ENV_CONFIG_LIGHT"
+      config="$ENV_CONFIG_LIGHT"
       echo "Set to create lightweight (no GUI) environment"
       ;;
     a)
@@ -173,23 +173,20 @@ then
 fi
 
 # create or update Conda environment
-config="$config_default"
-if [[ "$env_name" == "" ]]; then
-  echo "Setting up default Conda environment, $env_default"
-  env_name="$env_default"
-else
-  # change name in environment file with user-defined name
-  echo "Updating environment configuration for $env_name"
-  config="env_${env_name}.yml"
-  sed -e "s/$env_default/$env_name/g" "$config_default" > "$config"
-fi
 check_env="`conda env list | grep -w $env_name`"
 if [[ "$check_env" == "" ]]
 then
+	# create an empty environment before setting channel priority to 
+	# generate an env-specific .condarc file; Python version duplicated in 
+	# .yml for those who want to create env directly from .yml
 	echo "Creating new Conda environment from $config..."
-	conda env create -f "$config"
+	conda create -n "$env_name" python=3.6
+	source activate "$env_name"
+	conda config --env --set channel_priority strict # for mixed channels
+	conda env update -f "$config"
 else
 	echo "$env_name already exists, will update"
+	source activate "$env_name"
 	conda env update -f "$config"
 fi
 
@@ -201,10 +198,7 @@ then
 	echo "$env_name could not be found, exiting."
 	exit 1
 fi
-# need to reload conda script for unclear reasons; assume that 
-# CONDA_PREFIX has already been set by base environment
-#. "$CONDA_PREFIX/etc/profile.d/conda.sh"
-source activate $env_name
+
 
 ############################################
 # Download a shallow Git clone and pip install its Python package.

environment.yml
@@ -1,4 +1,3 @@
-name: clr3
 channels:
   - defaults
 dependencies:

environment_light.yml
@@ -1,4 +1,3 @@
-name: clrclu
 channels:
   - defaults
 dependencies:

commit bf512f4a84681b2ca5097878e9652bc9d0e64479

Fix default Conda environment name
Commit f217747 introduced a regression where the default environment name was left empty, fixed here.

setup_env.sh
@@ -30,8 +30,7 @@ Arguments:
 # default Conda environment names as found in .yml configs
 CONDA_ENV="clr3"
 CONDA_ENV_LIGHT="clrclu"
-env_default="$CONDA_ENV"
-env_name=""
+env_name="$CONDA_ENV"
 
 # default .yml files
 ENV_CONFIG="environment.yml"
@@ -60,7 +59,7 @@ while getopts hn:slad opt; do
       ;;
     l)
       lightweight=1
-      env_default="$CONDA_ENV_LIGHT"
+      env_name="$CONDA_ENV_LIGHT"
       config="$ENV_CONFIG_LIGHT"
       echo "Set to create lightweight (no GUI) environment"
       ;;
ed658fa90edcd31408b9605ddf1d8d46b2aab54a
David Young <david@textflex.com>
2019-08-07 10:51:32 +0800

    Refactor `build_se.bat` batch script settings into variables
    - Move paths and generator settings into variables to reduce redundancy and ease customization
    - Add quotes around variables  to allow spaces
    - Start from script's directory
    - Note on tested CMake

--- build_se.bat
+++ build_se.bat
@@ -3,20 +3,28 @@
 
 : Build SimpleElastix for Windows using MSVC build tools.
 : Assumes that the SimpleElastix git repo has been cloned to the parent 
-: folder of this script
+: folder of this script's folder. Tested with CMake 3.15; has *not* 
+: worked with MSVS CMake 3.14 on our testing.
+
+SET "cmake=cmake.exe"
+SET "build_dir=build_se"
+SET "generator=Visual Studio 16 2019"
+SET "arch=x64"
 
 : make build folder
+pushd "%~dp0"
 cd ..
-mkdir build_se
-cd build_se
+mkdir "%build_dir%"
+cd "%build_dir%"
 
 : configure with CMake to generate only a Python wrapper, rerunning 
 : since some paths may not be exposed until the 2nd run
-cmake -G "Visual Studio 16 2019" -A x64 -T host=x64 -DWRAP_JAVA:BOOL=OFF^
+"%cmake%" -G "%generator%" -A "%arch%" -T host="%arch%" -DWRAP_JAVA:BOOL=OFF^
  -DWRAP_LUA:BOOL=OFF -DWRAP_R:BOOL=OFF -DWRAP_RUBY:BOOL=OFF -DWRAP_TCL:BOOL=OFF^
  -DWRAP_CSHARP:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF^
  -DSimpleITK_PYTHON_USE_VIRTUALENV:BOOL=OFF ..\SimpleElastix\SuperBuild
-cmake.exe -G "Visual Studio 16 2019" -A x64 -T host=x64 ..\SimpleElastix\SuperBuild
+"%cmake%" -G "%generator%" -A "%arch%" -T host="%arch%" ..\SimpleElastix\SuperBuild
 
 : build SimpleElastix
 msbuild ALL_BUILD.vcxproj /p:Configuration=Release
+popd

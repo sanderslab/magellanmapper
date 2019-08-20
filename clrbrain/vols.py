sanderslab/magellanmapper

commit 50c1f0fa2d97152601f514dac8ae30fdb451a801

Remove all-NaN columns from volume stats
Clean up volume stats output by dropping columns with all NaNs.

clrbrain/vols.py
@@ -738,7 +738,10 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np,
             reg_nuc_mean * label_size)
     pool.close()
     pool.join()
+    
+    # make data frame of raw metrics, dropping columns of all NaNs
     df = pd.DataFrame(metrics)
+    df = df.dropna(axis=1, how="all")
     print(df.to_csv())
     
     # build data frame of total metrics from weighted means

commit 6338a14c13f9eddf9f0e84bb8ef5168226c95ce1

Fix combining volume stats into levels with missing columns
Commit 50c1f0f introduced a regression when accessing stats for columns that were removed because the metric was not measured. Fix by keeping or converting the stored metric to NaN if it is not measured.

clrbrain/vols.py
@@ -321,7 +321,8 @@ class MeasureLabel(object):
                 cls.df[LabelMetrics.Region.name].isin(label_ids)]
             label_size = np.nansum(labels[LabelMetrics.Volume.name])
             intens = np.nansum(labels[LabelMetrics.Intensity.name])
-            nuclei = np.nansum(labels[LabelMetrics.Nuclei.name])
+            if LabelMetrics.Nuclei.name in labels:
+                nuclei = np.nansum(labels[LabelMetrics.Nuclei.name])
         if label_size > 0:
             metrics[LabelMetrics.Volume] = label_size
             metrics[LabelMetrics.Intensity] = intens
@@ -459,7 +460,10 @@ class MeasureLabel(object):
             for i, row in labels.iterrows():
                 if row[LabelMetrics.RegVolMean.name] > 0:
                     for metric in VAR_METRICS:
-                        metrics[metric].append(row[metric.name])
+                        if metric.name in row:
+                            metrics[metric].append(row[metric.name])
+                        else:
+                            metrics[metric] = np.nan
         
         # weighted average, with weights given by frac of region or 
         # sub-region size from total size
@@ -470,7 +474,7 @@ class MeasureLabel(object):
         tot_nucs = np.nansum(nucs)
         for key in metrics.keys():
             #print("{} {}: {}".format(disp_id, key.name, metrics[key]))
-            if tot_size > 0:
+            if tot_size > 0 and metrics[key] != np.nan:
                 # take weighted mean
                 if key in NUC_METRICS:
                     # use weighting from nuclei for nuclei-oriented metrics
@@ -729,13 +733,16 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np,
             label_metrics[LabelMetrics.VarIntensity] * label_size)
         totals.setdefault("vol", []).append(label_size)
         totals.setdefault(LabelMetrics.Volume, []).append(vol_physical)
-        totals.setdefault(LabelMetrics.VarNuclei, []).append(
-            label_metrics[LabelMetrics.VarNuclei] * label_size)
-        totals.setdefault(LabelMetrics.Nuclei, []).append(nuc)
         totals.setdefault(LabelMetrics.RegVolMean, []).append(
             vol_mean_physical * label_size)
-        totals.setdefault(LabelMetrics.RegNucMean, []).append(
-            reg_nuc_mean * label_size)
+        var_nuc = label_metrics[LabelMetrics.VarNuclei]
+        if var_nuc != np.nan:
+            totals.setdefault(LabelMetrics.VarNuclei, []).append(
+                label_metrics[LabelMetrics.VarNuclei] * label_size)
+            totals.setdefault(LabelMetrics.Nuclei, []).append(nuc)
+        if reg_nuc_mean != np.nan:
+            totals.setdefault(LabelMetrics.RegNucMean, []).append(
+                reg_nuc_mean * label_size)
     pool.close()
     pool.join()
     
@@ -756,13 +763,16 @@ def measure_labels_metrics(sample, atlas_img_np, labels_img_np,
     
     # divide weighted values by sum of corresponding weights
     totals[LabelMetrics.Region] = "all"
-    totals[LabelMetrics.Density] = (
-        totals[LabelMetrics.Nuclei] / totals[LabelMetrics.Volume])
     totals[LabelMetrics.RegVolMean] /= totals["vol"]
-    totals[LabelMetrics.RegNucMean] /= totals["vol"]
-    totals[LabelMetrics.RegDensityMean] = (
-        totals[LabelMetrics.RegNucMean] / totals[LabelMetrics.RegVolMean])
-    totals[LabelMetrics.VarNuclei] /= totals["vol"]
+    if LabelMetrics.Nuclei in totals:
+        totals[LabelMetrics.Density] = (
+            totals[LabelMetrics.Nuclei] / totals[LabelMetrics.Volume])
+    if LabelMetrics.RegNucMean in totals:
+        totals[LabelMetrics.RegNucMean] /= totals["vol"]
+        totals[LabelMetrics.RegDensityMean] = (
+            totals[LabelMetrics.RegNucMean] / totals[LabelMetrics.RegVolMean])
+    if LabelMetrics.VarNuclei in totals:
+        totals[LabelMetrics.VarNuclei] /= totals["vol"]
     totals[LabelMetrics.VarIntensity] /= totals["vol"]
     totals[LabelMetrics.EdgeDistMean] /= totals[LabelMetrics.EdgeSize]
     for col in LabelMetrics:
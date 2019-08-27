sanderslab/magellanmapper

commit 0765dffc31dec816a868ff7f2f01f848e6eb2c49

Ensure that right hemisphere labels are negative for volume stats
For atlases whose right hemisphere is largely filled but positive rather than negative, such as unmirrorred atlases that are labeled from the start, make these labels negative when performing stats to distinguish sidedness.

clrbrain/register.py
@@ -3248,7 +3248,8 @@ def volumes_by_id(img_paths, labels_ref_lookup, suffix=None, unit_factor=None,
             img_np = sitk.GetArrayFromImage(img_sitk)
             spacing = img_sitk.GetSpacing()[::-1]
             
-            # load labels in order of priority: full labels > truncated labels
+            # load labels in order of priority: full labels > truncated labels; 
+            # labels are required and will give exception if not found
             try:
                 labels_img_np = load_registered_img(
                     mod_path, reg_name=IMG_LABELS)
@@ -3287,18 +3288,33 @@ def volumes_by_id(img_paths, labels_ref_lookup, suffix=None, unit_factor=None,
             # load sub-segmentation labels if available
             try:
                 subseg = load_registered_img(
-                    mod_path,reg_name=IMG_LABELS_SUBSEG)
+                    mod_path, reg_name=IMG_LABELS_SUBSEG)
             except FileNotFoundError as e:
                 print(e)
                 lib_clrbrain.warn("will ignore labels sub-segmentations")
             print("tot blobs", np.sum(heat_map))
+            
+            mirror = config.register_settings["labels_mirror"]
+            if mirror is None or mirror == -1: mirror = 0.5
+            mirrori = int(labels_img_np.shape[2] * mirror)
+            half_lbls = labels_img_np[:, :, mirrori:]
+            if (np.sum(half_lbls < 0) == 0 and 
+                    np.sum(half_lbls != 0) > np.sum(labels_img_np != 0) / 3):
+                # unmirrored images may have bilateral labels that are all pos, 
+                # while these metrics assume that R hem is neg; invert pos R 
+                # labels if they are at least 1/3 of total labels and not just 
+                # spillover from the L side into an otherwise unlabeled R
+                print("less than half of labels in right hemisphere are neg; "
+                      "inverting all pos labels in x >= {} (shape {}) for "
+                      "sided metrics".format(mirrori, labels_img_np.shape))
+                half_lbls[half_lbls > 0] *= -1
         
         # prepare sample name with original name for comparison across 
         # conditions and add an arbitrary number of metadata grouping cols
         sample = lib_clrbrain.get_filename_without_ext(img_path)
         if groups is not None:
-             for key in groups.keys():
-                 grouping[key] = groups[key][i]
+            for key in groups.keys():
+                grouping[key] = groups[key][i]
             
         # measure stats per label for the given sample; max_level already 
         # takes care of combining sides

commit 03a0089db87c148d426302d496c5a7e8e276e943

Limit right hemisphere label inversion
Commit 0765dff introduced a regression where images that may only have positive labels or even images oriented so that their right hemisphere is on the left side of the image would undergo label inversion during atlas metrics. Since this label inversion is only necessary for a very specific case -- importing bilaterally labeled atlases with only positive labels -- the inversion can be shifted to atlas import, with a new profile setting to turn it on only for specific atlases.

- Move label inversion from stats to atlas import, where the far hemisphere is also assumed to be higher numbers in the z-axis
- Turn off label inversion by default
- Turn on label inversion for P56-based atlases

clrbrain/atlas_refiner.py
@@ -979,6 +979,7 @@ def match_atlas_labels(img_atlas, img_labels, flip=False, metrics=None):
     smooth = config.register_settings["smooth"]
     crop = config.register_settings["crop_to_labels"]
     affine = config.register_settings["affine"]
+    far_hem_neg = config.register_settings["make_far_hem_neg"]
     
     if pre_plane:
         # images in the correct desired orientation may need to be 
@@ -1042,6 +1043,18 @@ def match_atlas_labels(img_atlas, img_labels, flip=False, metrics=None):
             extis = tuple(n - crop_sl[0].start for n in extis)
             if mask_lbls is not None:
                 mask_lbls = mask_lbls[tuple(crop_sl)]
+
+    if far_hem_neg and np.all(img_labels_np >= 0):
+        # unmirrored images may have only pos labels, while metrics assume 
+        # that the far hem is neg; invert pos labels there if they are >=1/3 of 
+        # total labels, not just spillover from the near side
+        half_lbls = img_labels_np[extis[1]:]
+        if (np.sum(half_lbls < 0) == 0 and
+                np.sum(half_lbls != 0) > np.sum(img_labels_np != 0) / 3):
+            print("less than half of labels in right hemisphere are neg; "
+                  "inverting all pos labels in x >= {} (shape {}) for "
+                  "sided metrics".format(extis[1], img_labels_np.shape))
+            half_lbls[half_lbls > 0] *= -1
     
     if metrics is not None:
         

clrbrain/config.py
@@ -708,7 +708,7 @@ class RegisterSettings(SettingsDict):
         
         # crop labels back to their original background after smoothing 
         # (ignored during atlas import if no smoothing), given as the filter 
-        # size used to open up the backgound before cropping, 0 to use 
+        # size used to open up the background before cropping, 0 to use 
         # the original background as-is, or False not to crop
         self["crop_to_orig"] = 1
         
@@ -720,6 +720,10 @@ class RegisterSettings(SettingsDict):
         # a data frame that can be used for fast aggregation when 
         # grouping into levels
         self["combine_sides"] = False
+        
+        # make the far hemisphere neg if it is not, for atlases (eg P56) with 
+        # bilateral pos labels where one half should be made neg for stats
+        self["make_far_hem_neg"] = False
         
         # planar orientation for transposition prior rather than after import
         self["pre_plane"] = None
@@ -746,7 +750,7 @@ def update_register_settings(settings, settings_type):
             {
                 "bspline_iter_max": "512", 
                 "truncate_labels": (None, (0.2, 1.0), (0.45, 1.0)), 
-                "holes_area": 5000
+                "holes_area": 5000, 
             }, 
             profile)
 
@@ -955,7 +959,8 @@ def update_register_settings(settings, settings_type):
              # partially complete; smallest closing to close ventricles
              "labels_edge": {
                  "start": 0.138, "surr_size": 12, "closing_size": 4}, 
-             "smooth": 2
+             "smooth": 2, 
+             "make_far_hem_neg": True, 
             }, 
             profile)
         
@@ -970,7 +975,8 @@ def update_register_settings(settings, settings_type):
              # sections and labels symmetric and aligned with one another
              "labels_mirror": 0.5,
              "labels_edge": None, 
-             "smooth": 2
+             "smooth": 2, 
+             "make_far_hem_neg": True, 
             }, 
             profile)
         

clrbrain/register.py
@@ -1298,21 +1298,6 @@ def volumes_by_id(img_paths, labels_ref_lookup, suffix=None, unit_factor=None,
                 lib_clrbrain.warn("will ignore labels sub-segmentations")
             print("tot blobs", np.sum(heat_map))
             
-            mirror = config.register_settings["labels_mirror"]
-            if mirror is None or mirror == -1: mirror = 0.5
-            mirrori = int(labels_img_np.shape[2] * mirror)
-            half_lbls = labels_img_np[:, :, mirrori:]
-            if (np.sum(half_lbls < 0) == 0 and 
-                    np.sum(half_lbls != 0) > np.sum(labels_img_np != 0) / 3):
-                # unmirrored images may have bilateral labels that are all pos, 
-                # while these metrics assume that R hem is neg; invert pos R 
-                # labels if they are at least 1/3 of total labels and not just 
-                # spillover from the L side into an otherwise unlabeled R
-                print("less than half of labels in right hemisphere are neg; "
-                      "inverting all pos labels in x >= {} (shape {}) for "
-                      "sided metrics".format(mirrori, labels_img_np.shape))
-                half_lbls[half_lbls > 0] *= -1
-        
         # prepare sample name with original name for comparison across 
         # conditions and add an arbitrary number of metadata grouping cols
         sample = lib_clrbrain.get_filename_without_ext(img_path)
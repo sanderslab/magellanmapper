13ff5d1b06e649204bec50a0a6b9b993d0964b06
David Young <david@textflex.com>
2019-08-29 17:17:51 +0800

    Clean up server deployment script
    - Move Fiji update sites into array to simplify adding more sites
    - Change backticks to command substitution
    - Exit on inappropriate command-line option
    - Fix formatting
    - Double quote file deployment array

--- deploy.sh
+++ deploy.sh
@@ -22,62 +22,70 @@ Arguments:
 "
 
 FIJI="http://downloads.imagej.net/fiji/latest/fiji-nojre.zip"
+FIJI_SITES=("BigStitcher http://sites.imagej.net/BigStitcher/")
 update=0 # update Clrbrain
 update_fiji=0 # update Fiji/ImageJ
 run_script="" # run script, which will be executed as last cmd
 deploy_files=() # files to deploy
 git_hash="" # git commit, including short hashes
-quiet=0
 username="ec2-user" # default on many EC2 distros
 
 # run from parent directory
-base_dir="`dirname $0`"
+base_dir="$(dirname "$0")"
 cd "$base_dir"
-echo $PWD
 
 OPTIND=1
 while getopts hi:p:ufr:d:g:n: opt; do
   case $opt in
-    h)  echo "$HELP"
+    h)
+      echo "$HELP"
       exit 0
       ;;
-    i)  ip="$OPTARG"
+    i)
+      ip="$OPTARG"
       echo "Set IP to $ip"
       ;;
-    p)  pem="$OPTARG"
+    p)
+      pem="$OPTARG"
       echo "Set pem key file to $pem"
       ;;
-    u)  update=1
+    u)
+      update=1
       echo "Set to update Clrbrain only"
       ;;
-    f)  update_fiji=1
+    f)
+      update_fiji=1
       echo "Set to update Fiji only"
       ;;
-    r)  run_script="$OPTARG"
+    r)
+      run_script="$OPTARG"
       echo "Set run script to $run_script"
       ;;
-    d)  deploy_files+=("$OPTARG")
+    d)
+      deploy_files+=("$OPTARG")
       echo "Adding $OPTARG to file deployment list"
       ;;
-    g)  git_hash="$OPTARG"
+    g)
+      git_hash="$OPTARG"
       echo "Using $git_hash as git hash"
       ;;
-    n)  username="$OPTARG"
+    n)
+      username="$OPTARG"
       echo "Changing username to $username"
       ;;
-    :)  echo "Option -$OPTARG requires an argument"
+    :)
+      echo "Option -$OPTARG requires an argument"
       exit 1
       ;;
-    --) ;;
+    *)
+      echo "$HELP" >&2
+      exit 1
+      ;;
   esac
 done
 
-# pass arguments after "--" to clrbrain
-shift "$((OPTIND-1))"
-EXTRA_ARGS="$@"
-
 if [[ "$git_hash" == "" ]]; then
-  git_hash=`git rev-parse --short HEAD`
+  git_hash=$(git rev-parse --short HEAD)
 fi
 archive="clrbrain_${git_hash}.zip"
 git archive -o "$archive" "$git_hash"
@@ -93,9 +101,13 @@ if [[ $update -eq 0 ]]; then
   #deploy_files+=" ../multiview-reconstruction/target/$mv_recon"
   server_cmd+=" && wget $FIJI"
   server_cmd+=" && unzip fiji-nojre.zip"
-  server_cmd+=" && Fiji.app/ImageJ-linux64 --update add-update-site BigStitcher http://sites.imagej.net/BigStitcher/"
+  server_cmd+=" && Fiji.app/ImageJ-linux64"
+  for site in "${FIJI_SITES[@]}"; do
+    server_cmd+=" --update add-update-site $site"
+  done
   server_cmd+=" && $cmd_fiji_update"
-  #server_cmd+=" ; rm Fiji.app/plugins/multiview?reconstruction-*.jar ; mv $mv_recon Fiji.app/plugins"
+  #server_cmd+=" ; rm Fiji.app/plugins/multiview?reconstruction-*.jar ;"
+  #server_cmd+=" mv $mv_recon Fiji.app/plugins"
 fi
 
 # add on Fiji update
@@ -111,7 +123,7 @@ echo "$server_cmd"
 echo ""
 
 # execute upload and server commands
-scp -i "$pem" -r ${deploy_files[@]} "${username}@${ip}":~
+scp -i "$pem" -r "${deploy_files[@]}" "${username}@${ip}":~
 ssh -i "$pem" "${username}@${ip}" "$server_cmd"
 
 if [[ "$run_script" != "" ]]; then

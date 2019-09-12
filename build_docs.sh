b73f38b97a2bf27fe675111af6f8363c5b76575e
David Young <david@textflex.com>
2019-09-12 11:55:20 +0800

    Format docs build script
    - Change indentation to 2-space format
    - Exit if unable to enter docs directory
    - Add help docs
    - Remove unused getopts extra args
    - Add case for unrecognized argument

--- build_docs.sh
+++ build_docs.sh
@@ -1,58 +1,71 @@
 #!/bin/bash
 # Build Clrbrain documentation
+# Author: David Young 2018, 2019
+
+HELP="
+Build documentation files for Clrbrain through the Sphinx package.
+
+Arguments:
+  -a: Rebuild API .rst files.
+  -c: Clean docs before rebuilding.
+  -h: Show help and exit.
+"
 
 DOCS_DIR="docs"
 build_api=0
 clean=0
 
 # run from project root directory
-BASE_DIR="`dirname $0`"
+BASE_DIR="$(dirname "$0")"
 cd "$BASE_DIR"
-echo $PWD
+echo "$PWD"
 
 OPTIND=1
 while getopts hac opt; do
-    case $opt in
-        h)  echo $HELP
-            exit 0
-            ;;
-        a)  build_api=1
-            echo "Set to rebuild API .rst files"
-            ;;
-        c)  clean=1
-            echo "Set to clean docs before building"
-            ;;
-        :)  echo "Option -$OPTARG requires an argument"
-            exit 1
-            ;;
-        --) ;;
-    esac
+  case $opt in
+    h)  
+      echo "$HELP"
+      exit 0
+      ;;
+    a)  
+      build_api=1
+      echo "Set to rebuild API .rst files"
+      ;;
+    c)  
+      clean=1
+      echo "Set to clean docs before building"
+      ;;
+    :)  
+      echo "Option -$OPTARG requires an argument"
+      exit 1
+      ;;
+    *)
+      echo "$HELP" >&2
+      exit 1
+      ;;
+  esac
 done
-
-# pass arguments after "--" to clrbrain
-shift "$((OPTIND-1))"
-EXTRA_ARGS="$@"
 
 # setup Sphinx configuration
 if [ ! -e "$DOCS_DIR" ]
 then
-    mkdir "$DOCS_DIR"
-    cd "$DOCS_DIR"
-    sphinx-quickstart
+  mkdir "$DOCS_DIR"
+  cd "$DOCS_DIR" || { echo "unable to make/enter $DOCS_DIR"; exit 1; }
+  sphinx-quickstart
 else
-    cd "$DOCS_DIR"
+  cd "$DOCS_DIR" || { echo "unable to enter $DOCS_DIR"; exit 1; }
 fi
 
 # (re)build .rst files
 if [ $build_api -eq 1 ]
 then
-    sphinx-apidoc -f -o . ..
+  sphinx-apidoc -f -o . ..
 fi
 
 # clean docs
 if [ $clean -eq 1 ]
 then
-    make clean
+  make clean
 fi
 
 # build docs

d15d2d518a9269c56c35253a65fd9163a98d5670
David Young <david@textflex.com>
2020-02-07 11:00:32 +0800

    Use Conda environment path in SimpleElastix using batch script
    Prevent the Windows batch script from picking up the system Python path before that of the current environment by checking for the `CONDA_PREFIX` environment variable.
    
    - Use the Conda environment variable when assigning the Python executable path during CMake configuration, which also uses this path for remaining Python settings
    - Fix returning to the original folder after running the script

--- bin/build_se.bat
+++ bin/build_se.bat
@@ -1,26 +1,28 @@
 :: Build SimpleElastix with MSVC
-:: Author: David Young, 2019
+:: Author: David Young, 2019, 2020
 
-: Build SimpleElastix for Windows using MSVC build tools.
-: Assumes that the SimpleElastix git repo has been cloned to the parent 
-: folder of this script's folder. Tested with CMake 3.15; has *not* 
-: worked with MSVS CMake 3.14 on our testing.
+:: Build SimpleElastix for Windows using MSVC build tools.
+:: Assumes:
+:: - The SimpleElastix git repo has been cloned to the parent
+::   folder of this script's folder
+:: - The CONDA_PREFIX environment variable will be used for the Python path
+:: Tested with CMake 3.15; has *not* worked with MSVS CMake 3.14 on our testing.
 
 SET "cmake=cmake.exe"
 SET "build_dir=build_se"
 SET "generator=Visual Studio 16 2019"
 SET "arch=x64"
 
-: make build folder
-cd ..
+:: make build folder
 pushd "%~dp0"
-cd ..
+cd ../..
 mkdir "%build_dir%"
 cd "%build_dir%"
 
-: configure with CMake to generate only a Python wrapper, rerunning 
-: since some paths may not be exposed until the 2nd run
+:: configure with CMake to generate only a Python wrapper, rerunning
+:: since some paths may not be exposed until the 2nd run
 "%cmake%" -G "%generator%" -A "%arch%" -T host="%arch%" -DWRAP_JAVA:BOOL=OFF^
+ -DPYTHON_EXECUTABLE="%CONDA_PREFIX%\python.exe"^
  -DWRAP_LUA:BOOL=OFF -DWRAP_R:BOOL=OFF -DWRAP_RUBY:BOOL=OFF -DWRAP_TCL:BOOL=OFF^
  -DWRAP_CSHARP:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF^
  -DSimpleITK_PYTHON_USE_VIRTUALENV:BOOL=OFF ..\SimpleElastix\SuperBuild

2875927e6f6934648c512d92e8a33fa4fcc60ab9
David Young <david@textflex.com>
2020-02-19 11:45:08 +0800

    Option to avoid unsharp filter and erosion when denoising
    If either setting is 0 or None, avoid applying the processing.

--- magmap/plot/plot_3d.py
+++ magmap/plot/plot_3d.py
@@ -116,16 +116,18 @@ def denoise_roi(roi, channel=None):
         
         # sharpening
         unsharp_strength = settings["unsharp_strength"]
-        blur_size = 8
-        # turn off multichannel since assume operation on single channel at 
-        # a time and to avoid treating as multichannel if 3D ROI happens to 
-        # have x size of 3
-        blurred = filters.gaussian(denoised, blur_size, multichannel=False)
-        high_pass = denoised - unsharp_strength * blurred
-        denoised = denoised + high_pass
+        if unsharp_strength:
+            blur_size = 8
+            # turn off multichannel since assume operation on single channel at
+            # a time and to avoid treating as multichannel if 3D ROI happens to
+            # have x size of 3
+            blurred = filters.gaussian(denoised, blur_size, multichannel=False)
+            high_pass = denoised - unsharp_strength * blurred
+            denoised = denoised + high_pass
         
         # further erode denser regions to decrease overlap among blobs
-        if saturated_mean > settings["erosion_threshold"]:
+        thresh_eros = settings["erosion_threshold"]
+        if thresh_eros and saturated_mean > thresh_eros:
             #print("denoising for saturated mean of {}".format(saturated_mean))
             denoised = morphology.erosion(denoised, morphology.octahedron(1))
         if multichannel:

8e09191c8ba9aab9d1d95f840b4ad196c81b7544
David Young <david@textflex.com>
2020-03-04 10:46:50 +0800

    Function to make a hyperparameter 2D array that varies for one index
    Hyperparameter searches frequently require taking an array as input. Often these input arrays will need to vary only at one index, such as the z-value for isotropic scaling. 2D arrays of these input arrays have been constructed manually for these isotropic factors and pruning tolerances. Generalize this construction through a function that uses `np.linspace` to create the varying factor, with a configurable base value for non-varying elements. Add a "sizeiso" grid search profile to demonstrate use of this function and replace the manually constructed arrays in commented out hyperparameters.

--- magmap/settings/profiles.py
+++ magmap/settings/profiles.py
@@ -369,13 +369,35 @@ def update_process_settings(settings, settings_type):
               .format(settings["settings_name"], settings))
 
 
-# isotropic factors
-_isotropic_zs = np.arange(0.5, 1.5, 0.1)
-_isotropic_factors = np.ones((len(_isotropic_zs), 3))
-_isotropic_factors[:, 0] = _isotropic_zs
-_prune_tol_zs = np.arange(0.5, 1.1, 0.5)
-_prune_tol_factors = np.ones((len(_prune_tol_zs), 3)) * 0.9
-_prune_tol_factors[:, 0] = _prune_tol_zs
+def make_hyperparm_arr(start, stop, num_steps, num_col, coli, base=1):
+    """Make a hyperparameter 2D array that varies across the first axis
+    for the given index.
+
+    The 2D array is used for grid searches, where each row is given as a
+    parameter. Each parameter is a 1D array with the same values except
+    at a given index, which varies across these 1D arrays. The varying
+    values are constructed by :meth:`np.linspace`.
+
+    Args:
+        start (int, float): Starting value for varying parameter.
+        stop (int, float): Ending value for varying parameter, inclusive.
+        num_steps (int): Number of steps from ``start`` to ``stop``, which
+            determines the number of rows in the output array.
+        num_col (int): Number of columns in the output array.
+        coli (int): Index of column to vary.
+        base (int, float): All values are set to this number except for the
+            varying values.
+
+    Returns:
+        :obj:`np.ndarray`: 2D array in the format ``[[start, base, base, ...],
+        [start0, base, base, ...], ...]``.
+
+    """
+    steps = np.linspace(start, stop, num_steps)
+    arr = np.ones((len(steps), num_col)) * base
+    arr[:, coli] = steps
+    return arr
+
 
 #: OrderedDict[List[int]]: Nested dictionary where each sub-dictionary
 # contains a sequence of values over which to perform a grid search to
@@ -393,11 +415,11 @@ roc_dict = OrderedDict([
         #("clip_max", np.arange(0.5, 0.6, 0.1)),
 
         # test parameters
-        #("isotropic", _isotropic_factors),
+        #("isotropic", make_hyperparm_arr(0.2, 1, 9, 3, 0),
         #("isotropic", np.array([(0.96, 1, 1)])),
         #("overlap", np.arange(0.1, 1.0, 0.1)),
         #("prune_tol_factor", np.array([(4, 1.3, 1.3)])),
-        #("prune_tol_factor", _prune_tol_factors),
+        #("prune_tol_factor", make_hyperparm_arr(0.5, 1, 2, 0.9, 0)),
         #("clip_min", np.arange(0.0, 0.2, 0.1)),
         #("clip_vmax", np.arange(97, 100.5, 0.5)),
         #("clip_max", np.arange(0.3, 0.7, 0.1)),
@@ -416,6 +438,11 @@ roc_dict = OrderedDict([
     ("size4x", OrderedDict([
         ("min_sigma_factor", np.arange(2.5, 3.51, 0.3)),
         ("max_sigma_factor", np.arange(3.5, 4.51, 0.3)),
+    ])),
+    ("sizeiso", OrderedDict([
+        ("min_sigma_factor", np.arange(2, 3.1, 1)),
+        ("max_sigma_factor", np.arange(3, 4.1, 1)),
+        ("isotropic", make_hyperparm_arr(0.2, 1, 9, 3, 0)),
     ])),
 ])
 

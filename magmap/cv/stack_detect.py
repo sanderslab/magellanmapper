sanderslab/magellanmapper

commit 143e58e95e90046c65b618f95f3b06a0ae51df63

Remove series from filenames
The series string added to filenames was introduced to avoid naming conflicts when importing a multi-tile image. The series conflict is typically not encountered however since these multi-tile images are usually imported to a single stitched image. Since the series string is not used and requires additional path manipulation for each file, simply remove this string. The filename builder functions will continue to accept the `series` parameter since it may have alternative uses such as the multiple timepoints supported in the image5d format. Change the default series to None to ignore it for now.

While adjusting filenames, the suffixes for images and blobs have also been changed. Underscores are removed since they will be prepended in `libmag.combine_paths` and for consistency with registration suffix constants. The `.npz` in the image5d file is corrected to its `.npy` format. "Info" is changed to "meta" for clarity that it refers to a metadata file. "info_proc" is changed specifically to "blobs".

magmap/cv/stack_detect.py
@@ -146,8 +146,7 @@ def make_subimage_name(base, offset, shape):
         Name (or path) to subimage.
     """
     roi_site = "{}x{}".format(offset, shape).replace(" ", "")
-    series_fill = libmag.series_as_str(config.series)
-    name = libmag.splice_before(base, series_fill, roi_site)
+    name = libmag.combine_paths(base, roi_site)
     print("subimage name: {}".format(name))
     return name
 
@@ -169,8 +168,9 @@ def detect_blobs_large_image(filename_base, image5d, offset, roi_size,
             to False.
     """
     time_start = time()
-    filename_image5d_proc = filename_base + config.SUFFIX_IMG_PROC
-    filename_info_proc = filename_base + config.SUFFIX_INFO_PROC
+    filename_image5d_proc = libmag.combine_paths(
+        filename_base, config.SUFFIX_IMG_PROC)
+    filename_blobs = libmag.combine_paths(filename_base, config.SUFFIX_BLOBS)
     roi_offset = offset
     shape = roi_size
     if roi_size is None or offset is None:
@@ -181,8 +181,7 @@ def detect_blobs_large_image(filename_base, image5d, offset, roi_size,
         # sets up processing for partial stack
         filename_image5d_proc = make_subimage_name(
             filename_image5d_proc, offset, roi_size)
-        filename_info_proc = make_subimage_name(
-            filename_info_proc, offset, roi_size)
+        filename_blobs = make_subimage_name(filename_blobs, offset, roi_size)
     
     # get ROI for given region, including all channels
     if full_roi:
@@ -338,7 +337,7 @@ def detect_blobs_large_image(filename_base, image5d, offset, roi_size,
         np.save(outfile_image5d_proc, roi)
         outfile_image5d_proc.close()
     
-    outfile_info_proc = open(filename_info_proc, "wb")
+    outfile_info_proc = open(filename_blobs, "wb")
     np.savez(outfile_info_proc, ver=BLOBS_NP_VER, segments=segments_all,
              resolutions=config.resolutions,
              basename=os.path.basename(config.filename),  # only save name
@@ -365,6 +364,7 @@ def detect_blobs_large_image(filename_base, image5d, offset, roi_size,
     df_io.dict_to_data_frame(times_dict, path_times, show=" ")
     
     return stats_detection, fdbk, segments_all
+
 
 def detect_blobs_sub_rois(sub_rois, sub_rois_offsets, denoise_max_shape, 
                           exclude_border):

magmap/io/importer.py
@@ -70,8 +70,6 @@ PIXEL_DTYPE = {
 # 14: removed pixel_type since redundant with image5d.dtype; 
 #     avoids storing object array, which requires loading by pickling
 IMAGE5D_NP_VER = 14 # image5d Numpy saved array version number
-SUFFIX_IMAGE5D = "_image5d.npz" # should actually be .npy
-SUFFIX_INFO = "_info.npz"
 
 CHANNEL_SEPARATOR = "_ch_"
 _EXT_TIFFS = (".tif", ".tiff")
@@ -206,14 +204,14 @@ def find_sizes(filename):
     print('time for finding sizes: %f' %(time() - time_start))
     return sizes, dtype
 
-def make_filenames(filename, series, modifier=""):
+def make_filenames(filename, series=None, modifier=""):
     """Make MagellanMapper-oriented image and image metadata filenames.
     
     Args:
-        filename: Original path from which MagellanMapper-oriented filenames 
-            will be derived.
-        series: Image series.
-        modifier: Separator for image series; defaults to "_"
+        filename (str): Original path from which MagellanMapper-oriented
+            filenames  will be derived.
+        series (int): Image series; defaults to None.
+        modifier (str): Separator for image series; defaults to an empty string.
     
     Returns:
         Tuple of path to the main image and path to metadata.
@@ -221,22 +219,26 @@ def make_filenames(filename, series, modifier=""):
     print("filename: {}".format(filename))
     filename_base = filename_to_base(filename, series, modifier)
     print("filename_base: {}".format(filename_base))
-    filename_image5d_npz = filename_base + SUFFIX_IMAGE5D
-    filename_info_npz = filename_base + SUFFIX_INFO
-    return filename_image5d_npz, filename_info_npz
+    filename_image5d = libmag.combine_paths(
+        filename_base, config.SUFFIX_IMAGE5D)
+    filename_meta = libmag.combine_paths(filename_base, config.SUFFIX_META)
+    return filename_image5d, filename_meta
 
-def filename_to_base(filename, series, modifier=""):
-    """Convert an image path to a base path including series information 
-    for use within MagellanMapper.
+def filename_to_base(filename, series=None, modifier=""):
+    """Convert an image path to a base path with an optional modifier.
     
     Args:
-        filename: Path to original image.
-        series: Series (eg tile) within image.
-        modifier: Modifier string prior to series; default to empty string.
+        filename (str): Path to original image.
+        series (int): Series (eg tile) within image; defaults to None.
+            Currently ignored, but may be implemented in the future to
+            track different tiles or timepoints.
+        modifier (str): Modifier string prior to series; defaults to an
+            empty string.
     """
-    return "{}_{}{}".format(
-        libmag.splitext(filename)[0], modifier, 
-        libmag.series_as_str(series))
+    path = libmag.splitext(filename)[0]
+    if modifier:
+        path = libmag.combine_paths(path, modifier)
+    return path
 
 def deconstruct_np_filename(np_filename, ext="czi"):
     """Deconstruct Numpy image filename to the appropriate image components.
@@ -513,18 +515,18 @@ def read_file(filename, series, load=True, z_max=-1,
     """
     path_split = libmag.splitext(filename)
     ext = path_split[1].lower()
-    filename_image5d_npz, filename_info_npz = make_filenames(
+    filename_image5d, filename_meta = make_filenames(
         filename, series)
     if load:
         image5d_ver_num = -1
         try:
             time_start = time()
             # load image5d metadata; if updating, only fully load if curr ver
-            output, image5d_ver_num = load_metadata(filename_info_npz, update_info)
+            output, image5d_ver_num = load_metadata(filename_meta, update_info)
             
             # load original image, using mem-mapped accessed for the image
             # file to minimize memory requirement, only loading on-the-fly
-            image5d = np.load(filename_image5d_npz, mmap_mode="r")
+            image5d = np.load(filename_image5d, mmap_mode="r")
             print("image5d shape: {}".format(image5d.shape))
             if offset is not None and size is not None:
                 # simplifies to reducing the image to a subset as an ROI if 
@@ -535,10 +537,10 @@ def read_file(filename, series, load=True, z_max=-1,
             if update_info:
                 # if metadata < latest ver, update and load info
                 load_info = _update_image5d_np_ver(
-                    image5d_ver_num, image5d, output, filename_info_npz)
+                    image5d_ver_num, image5d, output, filename_meta)
                 if load_info:
                     # load updated archive
-                    output, image5d_ver_num = load_metadata(filename_info_npz)
+                    output, image5d_ver_num = load_metadata(filename_meta)
             if return_info:
                 return image5d, output
             return image5d
@@ -657,7 +659,7 @@ def read_file(filename, series, load=True, z_max=-1,
                     # open file as memmap to directly output to disk, which is much 
                     # faster than outputting to RAM and saving to disk
                     image5d = np.lib.format.open_memmap(
-                        filename_image5d_npz, mode="w+", dtype=img.dtype, 
+                        filename_image5d, mode="w+", dtype=img.dtype,
                         shape=shape)
                     print("setting image5d array for series {} with shape: {}"
                           .format(series, image5d.shape))
@@ -680,7 +682,7 @@ def read_file(filename, series, load=True, z_max=-1,
     # TODO: consider saving resolutions as 1D rather than 2D array
     # with single resolution tuple
     md = save_image_info(
-        filename_info_npz, [name], [shape], [config.resolutions[series]],
+        filename_meta, [name], [shape], [config.resolutions[series]],
         config.magnification, config.zoom, near_mins, near_maxs)
     assign_metadata(md)
     return image5d

magmap/io/np_io.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Numpy archive import/export.
-# Author: David Young, 2019
+# Author: David Young, 2019, 2020
 """Import/export for Numpy-based archives such as ``.npy`` and ``.npz`` formats.
 """
 import os
@@ -37,9 +37,7 @@ def load_blobs(img_path, scaled_shape=None, scale=None):
         were detected. 
 
     """
-    filename_base = importer.filename_to_base(
-        img_path, config.series)
-    info = np.load(filename_base + config.SUFFIX_INFO_PROC)
+    info = np.load(libmag.combine_paths(img_path, config.SUFFIX_BLOBS))
     blobs = info["segments"]
     print("loaded {} blobs".format(len(blobs)))
     # get scaling from source image, which can be rescaled/resized image 
@@ -102,14 +100,14 @@ def _check_np_none(val):
     return None if val is None or np.all(np.equal(val, None)) else val
 
 
-def setup_images(path=None, series=0, offset=None, roi_size=None, 
+def setup_images(path=None, series=None, offset=None, roi_size=None,
                  proc_mode=None):
     """Sets up an image and all associated images and metadata.
     
     Args:
         path (str): Path to image from which MagellanMapper-style paths will 
             be generated.
-        series (int): Image series number.
+        series (int): Image series number; defaults to None.
         offset (List[int]): ROI offset given in x,y,z; defaults to None.
         roi_size (List[int]): ROI shape given in x,y,z; defaults to None.
         proc_mode (str): Processing mode, which should be a key in 
@@ -130,8 +128,10 @@ def setup_images(path=None, series=0, offset=None, roi_size=None,
         # load a blobs archive, +/- a processed ROI image
         print("Loading processed image files")
         filename_base = importer.filename_to_base(path, series)
-        filename_image5d_proc = filename_base + config.SUFFIX_IMG_PROC
-        filename_info_proc = filename_base + config.SUFFIX_INFO_PROC
+        filename_image5d_proc = libmag.combine_paths(
+            filename_base, config.SUFFIX_IMG_PROC)
+        filename_blobs = libmag.combine_paths(
+            filename_base, config.SUFFIX_BLOBS)
         
         if (not os.path.exists(filename_image5d_proc) and offset is not None
                 and roi_size is not None):
@@ -141,8 +141,8 @@ def setup_images(path=None, series=0, offset=None, roi_size=None,
             if os.path.exists(filename_image5d_proc):
                 # if ROI-based image exists, assume info file is also
                 # in ROI format
-                filename_info_proc = stack_detect.make_subimage_name(
-                    filename_info_proc, offset, roi_size)
+                filename_blobs = stack_detect.make_subimage_name(
+                    filename_blobs, offset, roi_size)
         
         try:
             # load image as an ROI chunk of the orig image if available
@@ -160,7 +160,7 @@ def setup_images(path=None, series=0, offset=None, roi_size=None,
         try:
             # load processed blobs and ROI metadata
             output_info = read_np_archive(
-                np.load(filename_info_proc))
+                np.load(filename_blobs))
             config.blobs = output_info["segments"]
             print("{} segments loaded".format(len(config.blobs)))
             if config.verbose:
@@ -179,7 +179,7 @@ def setup_images(path=None, series=0, offset=None, roi_size=None,
                 libmag.printv("could not find key:", e)
         except (FileNotFoundError, KeyError) as e:
             print("Unable to load processed info file at {}"
-                  .format(filename_info_proc))
+                  .format(filename_blobs))
             if proc_type in (
                     config.ProcessTypes.LOAD, config.ProcessTypes.EXPORT_BLOBS):
                 # blobs expected but not found

magmap/settings/config.py
@@ -35,15 +35,23 @@ POS_THRESH = 0.001 # threshold for positive values for float comparison
 
 # IMAGE FILES
 
-SUFFIX_IMG_PROC = "_image5d_proc.npz"
-SUFFIX_INFO_PROC = "_info_proc.npz"
+#: str: Suffix for main image.
+SUFFIX_IMAGE5D = "image5d.npy"
+#: str: Suffix for metadata of main image.
+SUFFIX_META = "meta.npz"
+#: str: Suffix for ROI image.
+SUFFIX_IMG_PROC = "image5d_proc.npz"
+#: str: Suffix for blobs archive.
+SUFFIX_BLOBS = "blobs.npz"
+#: str: Suffix for blob clusters archive.
 SUFFIX_BLOB_CLUSTERS = "blobclusters.npy"
 
 filename = None # current image file path
 filenames = None # list of multiple image paths
 metadata_paths = None  # metadata file paths
 metadatas = None  # metadata dicts
-series = 0 # series for multi-stack files
+#: int: Selected series index for multi-stack files; None for no series.
+series = None
 channel = None # channel of interest, where None specifies all channels
 roi_sizes = None # list of regions of interest
 roi_size = None  # current region of interest

commit 470984eb4c0390adff61d136e36080d05de8a16a

Fix loss of extension when creating ROI image filenames
Commit 143e58e9 caused the extension to be lost when creating an ROI image filename on the assumption that an extension-less filename was typically given. Truth databases require the full path, however. Restore this extension by using `insert_before_ext`, which now properly supports both paths with and without extensions. Also change the ROI image filename building to use the updated ROI offset and shape, although these variables and unlikely to have been updated for truth sets.

magmap/cv/stack_detect.py
@@ -146,7 +146,7 @@ def make_subimage_name(base, offset, shape):
         Name (or path) to subimage.
     """
     roi_site = "{}x{}".format(offset, shape).replace(" ", "")
-    name = libmag.combine_paths(base, roi_site)
+    name = libmag.insert_before_ext(base, roi_site, "_")
     print("subimage name: {}".format(name))
     return name
 
@@ -297,7 +297,7 @@ def detect_blobs_large_image(filename_base, image5d, offset, roi_size,
                 if config.truth_db is None:
                     # find and load truth DB based on filename and subimage
                     db_path_base = os.path.basename(
-                        make_subimage_name(filename_base, offset, roi_size))
+                        make_subimage_name(filename_base, roi_offset, shape))
                     print("about to verify with truth db from {}"
                           .format(db_path_base))
                     sqlite.load_truth_db(db_path_base)
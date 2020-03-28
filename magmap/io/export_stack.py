13801b90c8dbcbe5ee14527d54a1d97722034f46
David Young <david@textflex.com>
2020-03-28 17:37:00 +0800

    Fix stack export for labels that start with a plane with only one label
    Apply the `check_single` parameter to avoid the issue where first displayed planes containing only a single label fail to update.

--- magmap/io/export_stack.py
+++ magmap/io/export_stack.py
@@ -169,7 +169,8 @@ def _build_stack(ax, images, process_fnc, rescale=1, aspect=None,
         # a nested list containing a list for each image, which in turn 
         # contains a list of artists for each channel
         ax_imgs = plot_support.overlay_images(
-            ax, aspect, origin, imgs, None, cmaps_all, ignore_invis=True)
+            ax, aspect, origin, imgs, None, cmaps_all, ignore_invis=True,
+            check_single=True)
         if colorbar and len(ax_imgs) > 0 and len(ax_imgs[0]) > 0:
             # add colorbar with scientific notation if outside limits
             cbar = ax.figure.colorbar(ax_imgs[0][0], ax=ax, shrink=0.7)

sanderslab/magellanmapper

commit 5c395d6ac0d9ddcb191a669de4ec019743dc136c

Use command-line `alpha` arguments in the Atlas Editor
The alpha values in `PlotEditor`s shown in the Atlas Editor have ignored the command-line arguments. Similarly to the ROI Editor, incorporate these command-line arguments if set, falling back to the original values.

magmap/gui/plot_editor.py
@@ -12,6 +12,7 @@ from matplotlib import patches
 import numpy as np
 from skimage import draw
 
+from magmap.io import libmag
 from magmap.settings import config
 from magmap.atlas import ontology
 from magmap.plot import plot_support
@@ -168,7 +169,7 @@ class PlotEditor:
         # prep main image in grayscale and labels with discrete colormap
         imgs2d = [self.img3d[self.coord[0]]]
         cmaps = [config.cmaps]
-        alphas = [1]
+        alphas = [1]  # will be replaced by default config alpha
         
         if self.img3d_labels is not None:
             imgs2d.append(self.img3d_labels[self.coord[0]])
@@ -187,6 +188,8 @@ class PlotEditor:
                 imgs2d.append(img_add)
                 cmaps.append(self.cmap_borders[channel])
                 alphas.append(1)
+        # replace alphas with any config vals
+        alphas = libmag.pad_seq(config.alphas, len(alphas), alphas)
         
         # overlay all images and set labels for footer value on mouseover;
         # if first time showing image, need to check for images with single

commit e6cd98f5ccdef604910bb9390acdcaec779ae10e

Fix resetting labels alpha value during scrolling
Commit 5c395d6a introduced a regression where the alpha value for labels is overwritten by the config setting during scrolling. Fix by using the config settings only for the intensity and border images, skipping the labels image.

magmap/gui/plot_editor.py
@@ -169,7 +169,7 @@ class PlotEditor:
         # prep main image in grayscale and labels with discrete colormap
         imgs2d = [self.img3d[self.coord[0]]]
         cmaps = [config.cmaps]
-        alphas = [1]  # will be replaced by default config alpha
+        alphas = [config.alphas[0]]
         
         if self.img3d_labels is not None:
             imgs2d.append(self.img3d_labels[self.coord[0]])
@@ -181,15 +181,13 @@ class PlotEditor:
             # dimension for multiple sets of borders
             img2d = self.img3d_borders[self.coord[0]]
             channels = img2d.ndim if img2d.ndim >= 3 else 1
-            for channel in range(channels - 1, -1, -1):
+            for i, channel in enumerate(range(channels - 1, -1, -1)):
                 # show first (original) borders image last so that its 
                 # colormap values take precedence to highlight original bounds
                 img_add = img2d[..., channel] if channels > 1 else img2d
                 imgs2d.append(img_add)
                 cmaps.append(self.cmap_borders[channel])
-                alphas.append(1)
-        # replace alphas with any config vals
-        alphas = libmag.pad_seq(config.alphas, len(alphas), alphas)
+                alphas.append(libmag.get_if_within(config.alphas, 2 + i, 1))
         
         # overlay all images and set labels for footer value on mouseover;
         # if first time showing image, need to check for images with single
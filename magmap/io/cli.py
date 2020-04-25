sanderslab/magellanmapper

commit d1d56a3a4168c1dc698762e1c4df7906b8ec459a

Refactor truth database command-line parsing to use `args_to_dict`
Use the `args_to_dict` function to parse based on enums and specified parameters rather than relying on argument position. Add an Enum class to specify these parameters. Remove the `config.truth_db_name` variable since the parameters variable now holds this setting, but keep the mode variable since its value is parsed from another enum.

magmap/io/cli.py
@@ -531,7 +531,6 @@ def main(process_args_only=False):
                 theme_names.append(theme_enum.name)
         print("Set to use themes to {}".format(theme_names))
 
-
     # prep filename
     if not config.filename:
         # unable to parse anymore args without filename
@@ -539,8 +538,7 @@ def main(process_args_only=False):
         return
     filename_base = importer.filename_to_base(
         config.filename, config.series)
-    
-    
+
     # Database prep
     
     if args.db:
@@ -550,19 +548,18 @@ def main(process_args_only=False):
     # load "truth blobs" from separate database for viewing
     if args.truth_db is not None:
         # set the truth database mode
-        # TODO: refactor into args_to_dict format
-        config.truth_db_mode = libmag.get_enum(
-            args.truth_db[0], config.TruthDBModes)
-        print("Mapped \"{}\" truth_db setting to {}"
-              .format(args.truth_db[0], config.truth_db_mode))
-        if len(args.truth_db) > 1:
-            config.truth_db_name = args.truth_db[1]
-            print("Set truth_db name to {}".format(config.truth_db_name))
+        config.truth_db_params = args_to_dict(
+            args.truth_db, config.TruthDB, config.truth_db_params)
+        mode = config.truth_db_params[config.TruthDB.MODE]
+        config.truth_db_mode = libmag.get_enum(mode, config.TruthDBModes)
+        print("Mapped \"{}\" truth_db mode to {}"
+              .format(mode, config.truth_db_mode))
+    truth_db_path = config.truth_db_params[config.TruthDB.PATH]
     if config.truth_db_mode is config.TruthDBModes.VIEW:
         # loads truth DB as a separate database in parallel with the given 
         # editable database, with name based on filename by default unless 
         # truth DB name explicitly given
-        path = config.truth_db_name if config.truth_db_name else filename_base
+        path = truth_db_path if truth_db_path else filename_base
         try:
             sqlite.load_truth_db(path)
         except FileNotFoundError as e:
@@ -572,19 +569,19 @@ def main(process_args_only=False):
         # creates a new verified DB to store all ROC results
         config.verified_db = sqlite.ClrDB()
         config.verified_db.load_db(sqlite.DB_NAME_VERIFIED, True)
-        if config.truth_db_name:
+        if truth_db_path:
             # load truth DB path to verify against if explicitly given
             try:
-                sqlite.load_truth_db(config.truth_db_name)
+                sqlite.load_truth_db(truth_db_path)
             except FileNotFoundError as e:
                 print(e)
                 print("Could not load truth DB from {}"
-                      .format(config.truth_db_name))
+                      .format(truth_db_path))
     elif config.truth_db_mode is config.TruthDBModes.VERIFIED:
         # loads verified DB as the main DB, which includes copies of truth 
         # values with flags for whether they were detected
         path = sqlite.DB_NAME_VERIFIED
-        if config.truth_db_name: path = config.truth_db_name
+        if truth_db_path: path = truth_db_path
         try:
             config.db = sqlite.load_db(path)
             config.verified_db = config.db
@@ -595,7 +592,7 @@ def main(process_args_only=False):
     elif config.truth_db_mode is config.TruthDBModes.EDIT:
         # loads truth DB as the main database for editing rather than 
         # loading as a truth database
-        config.db_name = config.truth_db_name
+        config.db_name = truth_db_path
         if not config.db_name: 
             config.db_name = "{}{}".format(
                 os.path.basename(filename_base), sqlite.DB_SUFFIX_TRUTH)

magmap/cv/stack_detect.py
@@ -292,13 +292,13 @@ def detect_blobs_large_image(filename_base, image5d, offset, roi_size,
         if verify:
             db_path_base = None
             try:
-                if config.truth_db_name and config.truth_db:
+                truth_db_name = config.truth_db_params[config.TruthDB.PATH]
+                if truth_db_name and config.truth_db:
                     # use explicitly given truth DB if given, which can 
                     # contain multiple experiments for different subimages
-                    print("using truth DB from {}"
-                          .format(config.truth_db_name))
+                    print("using truth DB from {}".format(truth_db_name))
                     exp_name = importer.deconstruct_np_filename(
-                        config.truth_db_name)[0]
+                        truth_db_name)[0]
                 else:
                     # find truth DB based on filename and subimage
                     db_path_base = os.path.basename(

magmap/settings/config.py
@@ -248,6 +248,15 @@ scale_bar = True
 
 # DATABASE
 
+#: :class:`Enum`: Enum class for truth database settings.
+TruthDB = Enum(
+    "TruthDB", (
+        "MODE",  # mode from TruthDBModes
+        "PATH",  # path to DB
+    )
+)
+truth_db_params = dict.fromkeys(TruthDB, None)
+
 #: :class:`Enum`: Enum class for truth database modes. ``view`` loads the truth 
 # database corresponding to the filename and any offset/size to show 
 # alongside the current database. ``verify`` creates a new database 
@@ -263,11 +272,10 @@ TruthDBModes = Enum(
 truth_db_mode = None
 
 DB_NAME = "magmap.db"
-db_name = DB_NAME # path to main DB
-db = None # main DB
-truth_db_name = None # path to truth DB
-truth_db = None # truth blobs DB
-verified_db = None # automated verifications DB
+db_name = DB_NAME  # path to main DB
+db = None  # main DB
+truth_db = None  # truth blobs DB
+verified_db = None  # automated verifications DB
 
 
 # IMAGE REGISTRATION

commit e74a6161f3e8c198ca6423972da57449fdfca180

Fix parsing truth database paths with commas
Commit d1d56a3a introduced a regression where custom truth database paths with commas were being split. Use a custom separator unlikely to be found in most paths. Also print the truth database parameters when in verbose mode for debugging.

magmap/io/cli.py
@@ -640,9 +640,10 @@ def main(process_args_only=False):
     if args.truth_db is not None:
         # set the truth database mode
         config.truth_db_params = args_to_dict(
-            args.truth_db, config.TruthDB, config.truth_db_params)
+            args.truth_db, config.TruthDB, config.truth_db_params, sep_vals="|")
         mode = config.truth_db_params[config.TruthDB.MODE]
         config.truth_db_mode = libmag.get_enum(mode, config.TruthDBModes)
+        libmag.printv(config.truth_db_params)
         print("Mapped \"{}\" truth_db mode to {}"
               .format(mode, config.truth_db_mode))
     truth_db_path = config.truth_db_params[config.TruthDB.PATH]
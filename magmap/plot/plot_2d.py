sanderslab/magellanmapper

commit 6f9bcd0a0f409c0af2ccfb286260cd0d8d082131

Create new `plot` subpackage
Move plot/graph related modules into separate folder.

magmap/plot/plot_2d.py
@@ -13,13 +13,13 @@ import matplotlib.transforms as transforms
 import pandas as pd
 from skimage import exposure
 
-from magmap import colormaps
+from magmap.plot import colormaps
 from magmap import config
 from magmap.io import libmag
 from magmap.stats import mlearn
-from magmap import plot_support
+from magmap.plot import plot_support
 from magmap.stats import vols
-from magmap.plot_support import save_fig
+from magmap.plot.plot_support import save_fig
 
 
 def _show_overlay(ax, img, plane_i, cmap, out_plane, aspect=1.0, alpha=1.0,
@@ -321,7 +321,7 @@ def _bar_plots(ax, lists, errs, legend_names, x_labels, colors, y_label,
     ax.set_title(title)
     
     # show y-label with any unit in scientific notation
-    plot_support.set_scinot(ax, lbls=(y_label, ), units=(y_unit, ))
+    plot_support.set_scinot(ax, lbls=(y_label,), units=(y_unit,))
     # draw x-tick labels with smaller font for increasing number of labels
     font_size = plt.rcParams["axes.titlesize"]
     if libmag.is_number(font_size):

magmap/atlas/atlas_refiner.py
@@ -21,7 +21,7 @@ from magmap.io import importer
 from magmap.io import libmag
 from magmap.io import np_io
 from magmap.cv import cv_nd
-from magmap import plot_support
+from magmap.plot import plot_support
 from magmap import profiles
 from magmap.cv import segmenter
 from magmap.io import sitk_io

magmap/cv/detector.py
@@ -15,7 +15,7 @@ from skimage.feature import blob_log
 from magmap import config
 from magmap.cv import cv_nd
 from magmap.io import libmag
-from magmap import plot_3d
+from magmap.plot import plot_3d
 from magmap.io import sqlite
 from magmap.io import df_io
 

magmap/cv/segmenter.py
@@ -18,7 +18,7 @@ from magmap import config
 from magmap.cv import cv_nd
 from magmap.cv import detector
 from magmap.io import libmag
-from magmap import plot_3d
+from magmap.plot import plot_3d
 from magmap.io import df_io
 
 

magmap/cv/stack_detect.py
@@ -20,7 +20,7 @@ from magmap import config
 from magmap.cv import detector
 from magmap.io import importer
 from magmap.io import libmag
-from magmap import plot_3d
+from magmap.plot import plot_3d
 from magmap.io import sqlite
 from magmap.io import df_io
 

magmap/gui/atlas_editor.py
@@ -10,11 +10,11 @@ from matplotlib import pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.widgets import Slider, Button, TextBox
 
-from magmap import colormaps
+from magmap.plot import colormaps
 from magmap import config
 from magmap.io import libmag
 from magmap.gui import plot_editor
-from magmap import plot_support
+from magmap.plot import plot_support
 from magmap.cv import cv_nd
 from magmap.io import sitk_io
 
@@ -436,7 +436,7 @@ class InterpolatePlanes:
             # show current values if any exist
             self.btn.label.set_text(
                 "Fill {} {}\nID {}"
-                .format(plot_support.get_plane_axis(self.plane), self.bounds, 
+                .format(plot_support.get_plane_axis(self.plane), self.bounds,
                         self.label_id))
             self.btn.label.set_fontsize("xx-small")
         enable_btn(self.btn, all(self.bounds))

magmap/gui/plot_editor.py
@@ -14,7 +14,7 @@ from skimage import draw
 
 from magmap import config
 from magmap.atlas import ontology
-from magmap import plot_support
+from magmap.plot import plot_support
 
 
 class PlotEditor:

magmap/gui/roi_editor.py
@@ -23,12 +23,12 @@ from matplotlib import pyplot as plt
 from matplotlib.collections import PatchCollection
 from skimage import transform
 
-from magmap import colormaps
+from magmap.plot import colormaps
 from magmap import config
 from magmap.cv import detector
 from magmap.io import importer
 from magmap.io import libmag
-from magmap import plot_support
+from magmap.plot import plot_support
 
 # TODO: may want to base on scaling factor instead
 padding = (5, 5, 3) # human (x, y, z) order

commit 35b8e95986ab331fc80e21de495c5c5088057bb2

Fix import for saving figures
Commit 6f9bcd0 introduced a regression where the import statement for `save_fig` in `plot_support` sometimes failed, fixed here.

magmap/plot/plot_2d.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # 2D plot image and graph plotter
-# Author: David Young, 2017, 2019
+# Author: David Young, 2017, 2020
 """Plot 2D views of imaging data and graphs."""
 
 import os
@@ -20,7 +20,6 @@ from magmap.io import libmag
 from magmap.stats import mlearn
 from magmap.plot import plot_support
 from magmap.stats import vols
-from magmap.plot.plot_support import save_fig
 
 
 def _show_overlay(ax, img, plane_i, cmap, out_plane, aspect=1.0, alpha=1.0,
@@ -197,7 +196,7 @@ def plot_overlays_reg(exp, atlas, atlas_reg, labels_reg, cmap_exp,
         title = "Image Overlays"
     fig.suptitle(title)
     gs.tight_layout(fig)
-    save_fig(title, config.savefig)
+    plot_support.save_fig(title, config.savefig)
     if show:
         plt.show()
 
@@ -523,7 +522,7 @@ def plot_bars(path_to_df, data_cols=None, err_cols=None, legend_names=None,
     # save and display
     out_path = path_to_df if prefix is None else prefix
     out_path = libmag.combine_paths(out_path, "barplot")
-    if save: save_fig(out_path, config.savefig)
+    if save: plot_support.save_fig(out_path, config.savefig)
     if show: plt.show()
     return ax, out_path
 
@@ -679,7 +678,7 @@ def plot_lines(path_to_df, x_col, data_cols, linestyles=None, labels=None,
     # save and display
     out_path = path_to_df if prefix is None else prefix
     if suffix: out_path = libmag.insert_before_ext(out_path, suffix)
-    if save: save_fig(out_path, config.savefig)
+    if save: plot_support.save_fig(out_path, config.savefig)
     if show: plt.show()
     return ax
 
@@ -847,7 +846,7 @@ def plot_scatter(path, col_x, col_y, col_annot=None, cols_group=None,
     # save and display
     out_path = path
     if suffix: out_path = libmag.insert_before_ext(out_path, suffix)
-    save_fig(out_path, config.savefig)
+    plot_support.save_fig(out_path, config.savefig)
     if show: plt.show()
 
 
@@ -935,7 +934,7 @@ def plot_image(img, path=None, show=False):
             path_split = os.path.splitext(path)
             ext = path_split[1][1:] if path_split[1] else "png"
             print(path_split, ext)
-        save_fig(path, ext)
+        plot_support.save_fig(path, ext)
     if show: plt.show()
     plt.close()  # prevent display during next show call
 
@@ -988,7 +987,7 @@ def post_plot(ax, out_path=None, save_ext=None, show=False):
     if y_lim is not None:
         ax.set_ylim(*y_lim)
     if out_path and save_ext:
-        save_fig(out_path, save_ext)
+        plot_support.save_fig(out_path, save_ext)
     if show:
         plt.show()
 
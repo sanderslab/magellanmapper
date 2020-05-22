ae6ba7ac45209a95c32f2f1dcb15d3911b512604
David Young <david@textflex.com>
2020-05-22 17:24:30 +0800

    Fix Windows installer response to Miniconda download prompt
    The prompt for downloading and installing Miniconda in the installation Batch script did not take the user input value because the variable expanded before the block. Restructure the script into `GOTO` blocks based on this recommendation[^1]. Change `set /p` to `choice` instead as recommended here[^2] for immediate and tighter control over responses.
    
    Also, check `CONDA_EXE` instead of `CONDA_PREFIX` for a Conda installation as the latter appears to indicate an active environment, whereas the former is set so long as Conda has been initialized previously[^3].
    
    [^1] https://stackoverflow.com/a/55775540/1911852
    [^2] https://stackoverflow.com/a/51117474/1911852
    [^3] https://github.com/conda/conda/issues/7126

--- bin/setup_conda.bat
+++ bin/setup_conda.bat
@@ -8,32 +8,34 @@ SET "conda_bit=x86"
 pushd "%~dp0"
 cd ..
 
-IF "%CONDA_PREFIX%" == "" (
-  ECHO Anaconda/Miniconda not found. If you have already installed it, please open the Anaconda Prompt from the Start Menu and rerun this script.
-  ECHO Otherwise, we will attempt to install Miniconda.
-  
-  SET "install_conda=y"
-  SET /p "install_conda=Download and install Miniconda? [y|n]: "
-  IF NOT "%install_conda%" == "y" (
-    ECHO Installation stopped, exiting.
-    EXIT /b 1
-  )
-  
-  reg query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v "BuildLabEx" | >nul find /i ".x86fre." && set "os_bit=32" || set "os_bit=64"
-  ECHO "%os_bit%"
-  IF "%os_bit%" == "64" (
-    SET "conda_bit=x86_64"
-  )
-  SET "conda_out=Miniconda3-latest-Windows-%conda_bit%.exe"
-  bitsadmin /transfer downloadconda /download /priority FOREGROUND https://repo.anaconda.com/miniconda/%conda_out% "%cd%/%conda_out%"
-  
-  ECHO Downloaded Miniconda, about to install. Please allow the installer to initialize Miniconda for 'conda' to run.
-  ".\%conda_out%"
-  
-  ECHO Please open the Anaconda Prompt from the Start Menu and rerun this script to continue installation of MagellanMapper.
-  EXIT /b 0
+IF NOT "%CONDA_EXE%" == "" GOTO InstallMagMap
+
+:InstallMiniconda
+ECHO Anaconda/Miniconda not found. If you have already installed it, please open the Anaconda Prompt from the Start Menu and rerun this script.
+ECHO Otherwise, we will attempt to install Miniconda.
+
+CHOICE /c YN /M "Download and install Miniconda?"
+IF ERRORLEVEL == 2 (
+  ECHO Installation stopped, exiting.
+  EXIT /b 1
 )
 
+reg query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v "BuildLabEx" | >nul find /i ".x86fre." && set "os_bit=32" || set "os_bit=64"
+ECHO "%os_bit%"
+IF "%os_bit%" == "64" (
+  SET "conda_bit=x86_64"
+)
+SET "conda_out=Miniconda3-latest-Windows-%conda_bit%.exe"
+bitsadmin /transfer downloadconda /download /priority FOREGROUND https://repo.anaconda.com/miniconda/%conda_out% "%cd%/%conda_out%"
+
+ECHO Downloaded Miniconda, about to install. Please allow the installer to initialize Miniconda for 'conda' to run.
+".\%conda_out%"
+
+
+ECHO Please open the Anaconda Prompt from the Start Menu and rerun this script to continue installation of MagellanMapper.
+EXIT /b 0
+
+:InstallMagMap
 ECHO Creating a Conda environment for MagellanMapper with all supporting packages
 ECHO This may take awhile, especially during the 'Solving environment' and after the 'Executing transaction' steps
 CALL conda env create -n mag -f environment.yml

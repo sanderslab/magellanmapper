sanderslab/magellanmapper

commit 47c19b94551e92ad7b48ec8452e3177cb879924d

Auto-populate image import metadata fields when available
Metadata extraction has taken place during file import, meaning that the user cannot always verify the metadata beforehand and may not know if the metadata will be automatically extracted or not. To show this metadata, move up extraction from the import step to a separate step after file setup and before import. This extraction requires Bioformats, which causes a delay for Java initialization, but subsequent Java calls no longer need this initialization, so overall performance impact should be minimal. To remain responsive during this initialization, add a thread for collecting this metadata, using a `QThread` to avoid a Java exception when emitting a signal and allowing the signal to be defined within the class.

Additional changes:
- Disable the import button until setup is fully complete to avoid attempting while the setup thread is extracting metadata
- Add a "default" byte order for cases where the byte order is not applicable or default
- Pass metadata as a dictionary to the multipage file import function rather than storing as `config` attributes to avoid overwriting these settings for the current image if the import fails and does not load a new image
- Add shape and data type metadata keys to store this information during import setup

magmap/io/importer.py
@@ -664,9 +664,63 @@ def setup_import_multipage(filename):
     return chl_paths, base_path
 
 
-def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
-                             channel=None, fn_feedback=None, shape=None,
-                             data_type=None):
+def setup_import_metadata(chl_paths, channel=None, series=None, z_max=-1):
+    """Extract metadata and determine output image shape for importing
+    multipage file(s).
+    
+    Args:
+        chl_paths (dict[int, List[str]]): Dictionary of channel numbers to
+            sequences of image file paths to import.
+        channel (List[int]): Sequence of channel indices to import; defaults
+            to None to import all channels.
+        series (int): Series index to load. Defaults to None, which will use 0.
+        z_max (int): Number of z-planes to load; defaults to -1 to load all.
+
+    Returns:
+        dict[:obj:`config.MetaKeys`]: Dictionary of metadata.
+
+    """
+    print("Extracting metadata for image import, may take awhile...")
+    start_jvm()
+    jb.attach()
+    if series is None:
+        series = 0
+    num_chl_keys = len(chl_paths.keys())
+    path = tuple(chl_paths.values())[0][0]
+    md = dict.fromkeys(config.MetaKeys)
+    try:
+        # get available embedded metadata via Bioformats
+        names, sizes, md[config.MetaKeys.RESOLUTIONS], \
+            md[config.MetaKeys.MAGNIFICATION], \
+            md[config.MetaKeys.ZOOM], md[config.MetaKeys.DTYPE] = \
+            parse_ome_raw(path)
+        shape = list(sizes[series])
+    except jb.JavaException as err:
+        print(err)
+        # TODO: see if necessary or improved performance
+        sizes, dtype = find_sizes(path)
+        if dtype:
+            md[config.MetaKeys.DTYPE] = dtype.name
+        shape = list(sizes[0])
+
+    if num_chl_keys == 1:
+        if channel:
+            # when channels dict specifies only one channel, set channels
+            # based on channel parameter if set
+            shape[-1] = len(channel)
+    else:
+        # for multi-file, assume only one channel per file
+        shape[-1] = len(channel) if channel else num_chl_keys
+    if z_max != -1:
+        shape[1] = z_max
+    md[config.MetaKeys.SHAPE] = shape
+    jb.detach()
+    
+    return md
+
+
+def import_multiplane_images(chl_paths, prefix, import_md, series=None, offset=0,
+                             channel=None, fn_feedback=None):
     """Imports single or multiplane file(s) into Numpy format.
     
     For multichannel images, this import currently supports either a single
@@ -679,18 +733,16 @@ def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
         chl_paths (dict[int, List[str]]): Dictionary of channel numbers to
             sequences of image file paths to import.
         prefix (str): Ouput base path.
+        import_md (dict[:obj:`config.MetaKeys`]): Import metadata dictionary,
+            used to set up the shape, data type (for RAW file import), and
+            output image metadata (resolutions, zoom, magnification).
         series (int): Series index to load. Defaults to None, which will use 0.
-        z_max (int): Number of z-planes to load; defaults to -1 to load all.
         offset (int): z-plane offset from which to start importing.
             Defaults to 0.
         channel (List[int]): Sequence of channel indices to import; defaults
             to None to import all channels.
         fn_feedback (func): Callback function to give feedback strings
             during import; defaults to None.
-        shape (List[int]): Output image shape; defaults to None to determine
-            by ``chl_paths`` and the first image.
-        data_type (str, :obj:`np.dtype`): Data type as a string or data
-            type object; defaults to None.
 
     Returns:
         :obj:`np.ndarray`: The image array as a 5D array in the format,
@@ -705,6 +757,7 @@ def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
     time_start = time()
     if series is None:
         series = 0
+    shape = import_md[config.MetaKeys.SHAPE]
     filename_image5d, filename_meta = make_filenames(prefix, series)
     libmag.printcb("Initializing multiplane image import planes to \"{}\", "
                    "may take awhile..."
@@ -718,6 +771,14 @@ def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
     near_maxs = []
     chls_load = [0]  # default to assuming each file is single channel
     num_chl_keys = len(chl_paths.keys())
+    if num_chl_keys == 1:
+        if channel:
+            # when channels dict specifies only one channel, load channels
+            # based on channel parameter if set
+            chls_load = channel
+        else:
+            # allow multiple channels to be loaded from single file
+            chls_load = range(shape[-1])
     name = os.path.basename(prefix)
     chli = 0
     for chl, paths in chl_paths.items():
@@ -727,50 +788,6 @@ def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
             # skip if separate files for each channel current channel is not
             # in set channel parameter
             continue
-        
-        if shape is None:
-            # set up output image shape
-            try:
-                # get available embedded metadata via Bioformats
-                names, sizes, res, mag, zoom, pixel_type = parse_ome_raw(
-                    img_path)
-                if res:
-                    config.resolutions = res
-                if mag is not None:
-                    config.magnification = mag
-                if zoom is not None:
-                    config.zoom = zoom
-                shape = list(sizes[series])
-                name = names[series]
-            except jb.JavaException as err:
-                print(err)
-                if config.resolutions is None:
-                    # resolutions required for scaling, detections, etc
-                    raise IOError(
-                        "Could not import {}. Please specify resolutions."
-                        .format(img_path))
-                # get a more limited subset of metadata
-                # TODO: see if necessary or improved performance
-                sizes, dtype = find_sizes(img_path)
-                shape = list(sizes[0])
-            
-            if num_chl_keys == 1:
-                # if channels dict specifies only one channel, check if first
-                # file itself specifies multiple channels
-                if channel:
-                    # set channels based on channel parameter
-                    chls_load = channel
-                    shape[-1] = len(channel)
-                else:
-                    # if multichannel, its channels overrides channel dictionary
-                    # channels, though any subsequent files will simply
-                    # overwrite the array
-                    chls_load = range(shape[-1])
-            else:
-                # for multi-file, assume only one channel per file
-                shape[-1] = len(channel) if channel else num_chl_keys
-            if z_max != -1:
-                shape[1] = z_max
         
         rdr = None
         img_raw = None
@@ -786,7 +803,8 @@ def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
             # fall back to opening as a RAW 3D image file
             # TODO: generalize data type
             img_raw = np.memmap(
-                img_path, dtype=data_type, shape=shape[1:], mode="r")
+                img_path, dtype=import_md[config.MetaKeys.DTYPE],
+                shape=shape[1:], mode="r")
         len_shape = len(shape)
         for chl_load in chls_load:
             lows = []
@@ -839,8 +857,10 @@ def import_multiplane_images(chl_paths, prefix, series=None, z_max=-1, offset=0,
     # TODO: consider saving resolutions as 1D rather than 2D array
     # with single resolution tuple
     md = save_image_info(
-        filename_meta, [name], [shape], [config.resolutions[series]],
-        config.magnification, config.zoom, near_mins, near_maxs)
+        filename_meta, [name], [shape],
+        [import_md[config.MetaKeys.RESOLUTIONS]],
+        import_md[config.MetaKeys.MAGNIFICATION],
+        import_md[config.MetaKeys.ZOOM], near_mins, near_maxs)
     assign_metadata(md)
     libmag.printcb("Completed multiplane image import planes to \"{}\" "
                    "with metadata:\n{}"

magmap/gui/visualizer.py
@@ -379,6 +379,7 @@ class Visualization(HasTraits):
         stretch_last_section=False)
     _import_paths = List  # import paths table list
     _import_btn = Button("Import files")
+    _import_btn_enabled = Bool
     _import_clear_btn = Button("Clear import")
     _import_res = Array(np.float, shape=(1, 3))
     _import_mag = Float(1.0)
@@ -397,7 +398,7 @@ class Visualization(HasTraits):
     _import_data_type = List
     # map byte order name to Numpy symbol
     _IMPORT_BYTE_ORDERS = OrderedDict((
-        ("Little endian", "<"), ("Big endian", ">")))
+        ("Default", "="), ("Little endian", "<"), ("Big endian", ">")))
     _import_byte_order = List
     _import_prefix = Str
     _import_feedback = Str
@@ -602,7 +603,7 @@ class Visualization(HasTraits):
             label="Output image file"
         ),
         HGroup(
-            Item("_import_btn", show_label=False),
+            Item("_import_btn", show_label=False, enabled_when="_import_btn_enabled"),
             Item("_import_clear_btn", show_label=False),
         ),
         Item("_import_feedback", style="custom", show_label=False),
@@ -2009,21 +2010,74 @@ class Visualization(HasTraits):
         """Add a file or directory to import and populate the import table
         with all related files.
         """
+
+        def setup_import(md):
+            # populate the import metadata and output fields based on
+            # extracted values
+            res = md[config.MetaKeys.RESOLUTIONS]
+            if res is not None:
+                self._import_res = [res[0][::-1]]
+            mag = md[config.MetaKeys.MAGNIFICATION]
+            if mag is not None:
+                self._import_mag = mag
+            zoom = md[config.MetaKeys.ZOOM]
+            if zoom is not None:
+                self._import_zoom = zoom
+            shape = md[config.MetaKeys.SHAPE]
+            if shape is not None:
+                self._import_shape = [shape[::-1]]
+            
+            # set data type related dropdowns based on each character of
+            # the type-string of the Numpy data type object
+            dtype_str = md[config.MetaKeys.DTYPE]
+            try:
+                dtype_str = np.dtype(dtype_str).str
+                byte_orders = libmag.get_dict_keys_from_val(
+                    self._IMPORT_BYTE_ORDERS, dtype_str[0])
+                if byte_orders:
+                    self._import_byte_order = [byte_orders[0]]
+                data_types = libmag.get_dict_keys_from_val(
+                    self._IMPORT_DATA_TYPES, dtype_str[1])
+                if data_types:
+                    self._import_data_type = [data_types[0]]
+                bits = libmag.get_dict_keys_from_val(
+                    self._IMPORT_BITS, dtype_str[2])
+                if bits:
+                    self._import_bit = [bits[0]]
+            except TypeError:
+                print("Could not find data type for {}".format(dtype_str))
+            
+            # signal ready import
+            self._update_import_feedback("Ready to import")
+            self._import_btn_enabled = True
+        
+        # reset import fields
         self._clear_import_files(False)
         chl_paths = None
         base_path = None
+        
         if os.path.isdir(self._import_browser):
             # gather files within the directory to import
             self._import_mode = ImportModes.DIR
             chl_paths = importer.setup_import_dir(self._import_browser)
             base_path = os.path.join(
                 os.path.dirname(self._import_browser), "myvolume")
+            self._import_btn_enabled = True
+        
         elif self._import_browser:
             # gather files matching the pattern of the selected file to import
             self._import_mode = ImportModes.MULTIPAGE
             chl_paths, base_path = importer.setup_import_multipage(
                 self._import_browser)
-        
+            
+            # extract metadata in separate thread given delay from Java
+            # initialization for Bioformats
+            self._update_import_feedback(
+                "Gathering metadata related to {}, please wait"
+                "...".format(self._import_browser))
+            import_setup_thread = SetupImportThread(chl_paths, setup_import)
+            import_setup_thread.start()
+
         if chl_paths:
             # populate the import table
             data = []
@@ -2032,7 +2086,7 @@ class Visualization(HasTraits):
                     data.append([path, chl])
             self._import_paths = data
             self._import_prefix = base_path
-
+    
     @on_trait_change("_import_btn")
     def _import_files(self):
         """Import files based on paths in the import table.
@@ -2048,27 +2102,26 @@ class Visualization(HasTraits):
         
         if chl_paths:
             # set metadata
-            config.resolutions = [self._import_res[0].astype(float)[::-1]]
-            config.magnification = self._import_mag
-            config.zoom = self._import_zoom
-            data_type = "".join(
-                (self._IMPORT_BYTE_ORDERS[self._import_byte_order[0]],
-                 self._IMPORT_DATA_TYPES[self._import_data_type[0]],
-                 self._IMPORT_BITS[self._import_bit[0]]))
-            
-            # get shape if not default
-            shape = self._import_shape[0].astype(int)[::-1]
-            if all(np.equal(shape, 0)):
-                shape = None
+            series = 0 if config.series is None else config.series
+            md = {
+                config.MetaKeys.RESOLUTIONS: self._import_res[series].astype(
+                    float)[::-1],
+                config.MetaKeys.MAGNIFICATION: self._import_mag,
+                config.MetaKeys.ZOOM: self._import_zoom,
+                config.MetaKeys.DTYPE:  "".join(
+                    (self._IMPORT_BYTE_ORDERS[self._import_byte_order[0]],
+                     self._IMPORT_DATA_TYPES[self._import_data_type[0]],
+                     self._IMPORT_BITS[self._import_bit[0]])),
+                config.MetaKeys.SHAPE: self._import_shape[0].astype(int)[::-1],
+            }
             
             # initialize import in separate thread with a signal for
             # loading thew newly imported image
             self.load_image_signal = QtSignal()
             self.load_image_signal.signal.connect(update_filename)
             import_thread = ImportThread(
-                self._import_mode, self._import_prefix, chl_paths,
-                self._update_import_feedback, self.load_image_signal.signal,
-                shape, data_type)
+                self._import_mode, self._import_prefix, chl_paths, md,
+                self._update_import_feedback, self.load_image_signal.signal)
             import_thread.start()
     
     @on_trait_change("_import_clear_btn")
@@ -2090,6 +2143,7 @@ class Visualization(HasTraits):
         self._import_bit = [tuple(self._IMPORT_BITS.keys())[0]]
         self._import_data_type = [tuple(self._IMPORT_DATA_TYPES.keys())[0]]
         self._import_byte_order = [tuple(self._IMPORT_BYTE_ORDERS.keys())[0]]
+        self._import_btn_enabled = False
 
     def _update_import_feedback(self, val):
         """Update the import feedback text box.
@@ -2106,6 +2160,32 @@ class QtSignal(PyQt5.QtCore.QObject):
     signal = PyQt5.QtCore.pyqtSignal()
 
 
+class SetupImportThread(PyQt5.QtCore.QThread):
+    """Thread for setting up file import by extracting image metadata.
+
+    Attributes:
+        chl_paths (dict[int, List[str]]): Dictionary of channel numbers
+            to sequences of paths within the given channel.
+        fn_success (func): Signal taking
+            no arguments, to be emitted upon successfull import; defaults
+            to None.
+
+    """
+    
+    signal = PyQt5.QtCore.pyqtSignal(object)
+    
+    def __init__(self, chl_paths, fn_success):
+        """Initialize the import thread."""
+        super().__init__()
+        self.chl_paths = chl_paths
+        self.signal.connect(fn_success)
+    
+    def run(self):
+        """Set up image import metadata."""
+        md = importer.setup_import_metadata(self.chl_paths)
+        self.signal.emit(md)
+
+
 class ImportThread(Thread):
     """Thread for importing files into a Numpy array.
     
@@ -2115,27 +2195,25 @@ class ImportThread(Thread):
             will be constructed.
         chl_paths (dict[int, List[str]]): Dictionary of channel numbers
             to sequences of paths within the given channel.
+        import_md (dict[:obj:`config.MetaKeys`]): Import metadata dictionary.
         fn_feedback (func): Function taking a string to display feedback;
             defaults to None.
         signal_success (:obj:`PyQt5.QtCore.pyqtBoundSignal`): Signal taking
             no arguments, to be emitted upon successfull import; defaults
             to None.
-        data_type (str, :obj:`np.dtype`): Output image data type; defaults
-            to None.
     
     """
     
-    def __init__(self, mode, prefix, chl_paths, fn_feedback=None,
-                 signal_success=None, shape=None, data_type=None):
+    def __init__(self, mode, prefix, chl_paths, import_md, fn_feedback=None,
+                 signal_success=None):
         """Initialize the import thread."""
         super().__init__()
         self.mode = mode
         self.prefix = prefix
         self.chl_paths = chl_paths
+        self.import_md = import_md
         self.fn_feedback = fn_feedback
         self.signal_success = signal_success
-        self.shape = shape
-        self.data_type = data_type
 
     def run(self):
         """Import files based on the import mode and set up the image."""
@@ -2149,8 +2227,8 @@ class ImportThread(Thread):
             elif self.mode is ImportModes.MULTIPAGE:
                 # import multi-plane files
                 image5d = importer.import_multiplane_images(
-                    self.chl_paths, self.prefix, fn_feedback=self.fn_feedback,
-                    shape=self.shape, data_type=self.data_type)
+                    self.chl_paths, self.prefix, self.import_md,
+                    fn_feedback=self.fn_feedback)
         finally:
             if image5d is not None:
                 # set up the image for immediate use within MagellanMapper

magmap/io/np_io.py
@@ -225,8 +225,10 @@ def setup_images(path=None, series=None, offset=None, size=None,
                     chls, import_path = importer.setup_import_multipage(path)
                     prefix = (import_path if config.prefix is None
                               else config.prefix)
+                    import_md = importer.setup_import_metadata(
+                        chls, config.channel, series)
                     config.image5d = importer.import_multiplane_images(
-                        chls, prefix, series, channel=config.channel)
+                        chls, prefix, import_md, series, channel=config.channel)
                 config.image5d_io = config.LoadIO.NP
             except FileNotFoundError as e:
                 print(e)

magmap/settings/config.py
@@ -112,6 +112,8 @@ MetaKeys = Enum(
         "RESOLUTIONS",  # image resolutions in x,y,z
         "MAGNIFICATION",  # objective magnification
         "ZOOM",  # objective zoom
+        "SHAPE",  # output image shape
+        "DTYPE",  # data type as a string
     )
 )
 meta_dict = dict.fromkeys(MetaKeys, None)

commit b83dfcb8da68c7b5fe1ab55e88e5ea1d33887fc7

Fix updating imported image shape for user-updated paths
Separating out shape determination from the import step in commit 47c19b94 caused a regression where user updates to the table of paths to import caused the incorrect channels to be read. Determining the output image shape for channels has been complicated by the possibility of either multichannel single files or multiple single-channel files. To distinguish these scenarios, use a special key to designate the former case. This key will be applied only when no filenames with channel designators are found, in which case only the given file will be taken, no longer falling back to any matching file except extension.

This change avoids the ambiguity from relying on the number of channels in the dictionary since the user may reduce the number of channels to a single channel, and it would be ambiguous whether this file has multiple channels or not. To further reduce potential conflicts between these two scenarios, reduce the dictionary to only this item if the key exists. Similarly, prune the dictionary to any channels parameter if the dictionary is assumed to only contain single channel files.

magmap/io/importer.py
@@ -73,6 +73,7 @@ IMAGE5D_NP_VER = 14  # image5d Numpy saved array version number
 
 CHANNEL_SEPARATOR = "_ch_"
 _EXT_TIFFS = (".tif", ".tiff")
+_KEY_ANY_CHANNEL = "1+"  # 1+ channel files
 
 
 def start_jvm(heap_size="8G"):
@@ -606,7 +607,9 @@ def setup_import_multipage(filename):
     Multiple channels are assumed to be stored in separate files with
     :const:``CHANNEL_SEPARATOR`` followed by an integer corresponding to
     the channel number (0-based indexing), eg ``/path/to/image_ch_0.tif``.
-    If no matches are found, ``filename`` will be taken directly.
+    If any such file is found, only files with these channel designators
+    will be taken. If none are found, ``filename`` will be taken directly.
+
 
     Args:
         filename (str): Path to image file to import. Files for separate
@@ -638,14 +641,6 @@ def setup_import_multipage(filename):
           "the format:", path_base)
     matches = glob.glob(path_base)
     
-    if not matches:
-        # fall back to matching any file with the same name regardless
-        # of extension, typically for single-channel images
-        path_base = "{}.*".format(base_path)
-        tif_searches.append(path_base)
-        print("Looking for TIFF files matching the format:", path_base)
-        matches = glob.glob(path_base)
-    
     for match in matches:
         # prune files to matching extensions
         match_split = os.path.splitext(match)
@@ -654,15 +649,16 @@ def setup_import_multipage(filename):
     filenames = sorted(filenames)
     
     if filenames:
+        # get dict of channels by files
         print("Found matching file(s), where each file will be imported "
               "as a separate channel:", filenames)
+        chl_paths = _parse_import_chls(filenames)
     else:
-        # default to taking the file directly
+        # take file directly with key specifying it could have multiple channels
         print("Found single file {}".format(filename))
-        filenames = [filename]
+        chl_paths = {_KEY_ANY_CHANNEL: [filename]}
     
     # parse to dict by channel
-    chl_paths = _parse_import_chls(filenames)
     return chl_paths, base_path
 
 
@@ -687,7 +683,6 @@ def setup_import_metadata(chl_paths, channel=None, series=None, z_max=-1):
     jb.attach()
     if series is None:
         series = 0
-    num_chl_keys = len(chl_paths.keys())
     path = tuple(chl_paths.values())[0][0]
     md = dict.fromkeys(config.MetaKeys)
     try:
@@ -704,21 +699,39 @@ def setup_import_metadata(chl_paths, channel=None, series=None, z_max=-1):
         if dtype:
             md[config.MetaKeys.DTYPE] = dtype.name
         shape = list(sizes[0])
-
-    if num_chl_keys == 1:
-        if channel:
-            # when channels dict specifies only one channel, set channels
-            # based on channel parameter if set
-            shape[-1] = len(channel)
-    else:
-        # for multi-file, assume only one channel per file
-        shape[-1] = len(channel) if channel else num_chl_keys
+    
+    shape = _update_shape_for_channels(shape, chl_paths, channel)
     if z_max != -1:
         shape[1] = z_max
     md[config.MetaKeys.SHAPE] = shape
     jb.detach()
     
     return md
+
+
+def _update_shape_for_channels(shape, chl_paths, channel):
+    """Change image shape to match specified number of channels.
+    
+    Args:
+        shape (List[int]): Image shape, with last dimenion for channels.
+        chl_paths (dict): Dictionary of channels by files.
+        channel (List[int]): Sequence of channels to keep.
+
+    Returns:
+        List[int]: Copy of ``shape`` with channel size adjusted.
+
+    """
+    shape_up = list(shape)
+    if _KEY_ANY_CHANNEL in chl_paths:
+        # file present with unspecified channel, potentially multichannel,
+        # with shape assumed to be based on this file
+        if channel:
+            # limit channels to set parameter
+            shape_up[-1] = len(channel)
+    else:
+        # assume only one channel per file
+        shape_up[-1] = len(channel) if channel else len(chl_paths.keys())
+    return shape_up
 
 
 def import_multiplane_images(chl_paths, prefix, import_md, series=None, offset=0,
@@ -759,7 +772,6 @@ def import_multiplane_images(chl_paths, prefix, import_md, series=None, offset=0
     time_start = time()
     if series is None:
         series = 0
-    shape = import_md[config.MetaKeys.SHAPE]
     filename_image5d, filename_meta = make_filenames(prefix, series)
     libmag.printcb("Initializing multiplane image import planes to \"{}\", "
                    "may take awhile..."
@@ -768,28 +780,31 @@ def import_multiplane_images(chl_paths, prefix, import_md, series=None, offset=0
     # initialize JVM for import via Bioformats
     start_jvm()
     jb.attach()
+    
+    # set up channels in case chl_paths was updated after shape determination
+    # and to get channels to extract from each file
+    shape = _update_shape_for_channels(
+        import_md[config.MetaKeys.SHAPE], chl_paths, channel)
+    if _KEY_ANY_CHANNEL in chl_paths:
+        # unspecified channel file, potentially multichannel, takes
+        # precedence over all other files
+        chls_load = channel if channel else range(shape[-1])
+        chl_paths = {_KEY_ANY_CHANNEL: chl_paths[_KEY_ANY_CHANNEL]}
+    else:
+        # assuming each file is single channel and skip if channel not in
+        # channel parameter
+        chls_load = [0]
+        chl_paths = OrderedDict(
+            [(k, v) for k, v in chl_paths.items()
+             if not channel or k in channel])
+
     image5d = None
     near_mins = []
     near_maxs = []
-    chls_load = [0]  # default to assuming each file is single channel
-    num_chl_keys = len(chl_paths.keys())
-    if num_chl_keys == 1:
-        if channel:
-            # when channels dict specifies only one channel, load channels
-            # based on channel parameter if set
-            chls_load = channel
-        else:
-            # allow multiple channels to be loaded from single file
-            chls_load = range(shape[-1])
-    name = os.path.basename(prefix)
     chli = 0
     for chl, paths in chl_paths.items():
         # assume only one file per channel, ignoring others in same channel
         img_path = paths[0]
-        if num_chl_keys > 1 and channel and chl not in channel:
-            # skip if separate files for each channel current channel is not
-            # in set channel parameter
-            continue
         
         rdr = None
         img_raw = None
@@ -859,7 +874,7 @@ def import_multiplane_images(chl_paths, prefix, import_md, series=None, offset=0
     # TODO: consider saving resolutions as 1D rather than 2D array
     # with single resolution tuple
     md = save_image_info(
-        filename_meta, [name], [shape],
+        filename_meta, [os.path.basename(prefix)], [shape],
         [import_md[config.MetaKeys.RESOLUTIONS]],
         import_md[config.MetaKeys.MAGNIFICATION],
         import_md[config.MetaKeys.ZOOM], near_mins, near_maxs)
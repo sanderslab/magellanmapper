sanderslab/magellanmapper

commit f09ddaf04f8eedb965c079ae32ef4f11abf0c690

Use the same set of colors for positive labels whether or not generating symmetric colors
Generating symmetric colors has applied a different ordering of colors to labels compared to non-symmetric colors. To keep colors consistent, keep the colors for positive labels the same when generating symmetric labels, simply reflecting those colors onto negative labels. Also make a small simplification by not halving the number of colors, instead generating the full set of colors and simply replacing the first half of colors. Change the default offset for duplicate colors to 0 for simplicity.

magmap/plot/colormaps.py
@@ -189,7 +189,7 @@ def discrete_colormap(num_colors, alpha=255, prioritize_default=True,
             assuming the first half of ``num_colors`` mirror those of
             the second half; defaults to False.
         dup_offset (int): Amount by which to offset duplicate color values
-            if ``dup_for_neg`` is enabled; defaults to 30.
+            if ``dup_for_neg`` is enabled; defaults to 0.
         jitter (int): In :obj:`DiscreteModes.GRID` mode, coordinates are
             randomly shifted by half this value above or below their original
             value; defaults to 0.
@@ -204,10 +204,8 @@ def discrete_colormap(num_colors, alpha=255, prioritize_default=True,
         to generate a map that can be used directly in functions such 
         as ``imshow``.
     """
-    cmap_offset = 0 if num_colors // 2 == num_colors / 2 else 1
-        # halve number of colors to duplicate for corresponding labels
-        num_colors = int(np.ceil(num_colors / 2))
     if symmetric_colors:
+        # make room for offset when duplicating colors
         max_val -= dup_offset
 
     # generate random combination of RGB values for each number of colors, 
@@ -257,10 +255,11 @@ def discrete_colormap(num_colors, alpha=255, prioritize_default=True,
             cmap[below_offset, axes] = np.multiply(
                 cmap[below_offset, axes], max_val / min_any)
     
-        # assume that corresponding labels are mirrored (eg -5, 3, 0, 3, 5)
-        cmap_neg = cmap + dup_offset
-        cmap = np.vstack((cmap_neg[::-1], cmap[cmap_offset:]))
     if symmetric_colors:
+        # invert latter half onto former half, assuming that corresponding
+        # labels are mirrored (eg -5, 3, 0, 3, 5), with background centered as 0
+        mid = len(cmap) // 2
+        cmap[:mid] = cmap[:mid:-1] + dup_offset
     cmap[:, -1] = alpha  # set transparency
     if prioritize_default is not False:
         # prioritize default colors by replacing first colors with default ones

commit 94bc8df645e6e2d711c60bd3a046a88a67c7b81e

Fix color symmetry for discrete colormaps with an even number of colors
Commit f09ddaf0 introduced a regression where rounding led to an off-by-one array length error when remapping a colormap to be symmetric. Ensure that the array half to be copied to the other half has an identical length.

magmap/plot/colormaps.py
@@ -289,8 +289,9 @@ def discrete_colormap(num_colors, alpha=255, prioritize_default=True,
     if symmetric_colors:
         # invert latter half onto former half, assuming that corresponding
         # labels are mirrored (eg -5, 3, 0, 3, 5), with background centered as 0
-        mid = len(cmap) // 2
-        cmap[:mid] = cmap[:mid:-1] + dup_offset
+        cmap_len = len(cmap)
+        mid = cmap_len // 2
+        cmap[:mid] = cmap[:cmap_len-mid-1:-1] + dup_offset
     cmap[:, -1] = alpha  # set transparency
     if prioritize_default is not False:
         # prioritize default colors by replacing first colors with default ones
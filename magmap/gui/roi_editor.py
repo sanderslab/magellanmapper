sanderslab/magellanmapper

commit 9ab75cc90ae33ec889a24ecda7aab15268b7518f

Move figure saving from ROI and Atlas Editors to mixin
- Move figure attribute to `ImageSyncMixin`, now generalized for editors with Matplotlib figures
- Refactor figure save function to this mixin to avoid redundancy
- Remove default path of None since a path is currently always given

magmap/gui/roi_editor.py
@@ -325,8 +325,8 @@ class ROIEditor(plot_support.ImageSyncMixin):
     def __init__(self, image5d=None, labels_img=None, img_region=None,
                  fn_show_label_3d=None, fn_status_bar=None):
         """Initialize the editor."""
+        super().__init__()
         print("Initiating ROI Editor")
-        self.fig = None
         self.image5d = image5d
         self.labels_img = labels_img
         if img_region is not None:
@@ -1014,17 +1014,6 @@ class ROIEditor(plot_support.ImageSyncMixin):
             plot_support.get_plane_axis(self.plane),
             self._z_overview, ext)
     
-    def save_fig(self, path=None):
-        """Save the figure to file, with path based on filename, ROI,
-        and overview plane shown.
-        """
-        if not self.fig:
-            print("ROI Editor not yet initialized, skipping save")
-            return
-        if path is None:
-            path = self.get_save_path()
-        plot_support.save_fig(path, fig=self.fig)
-
     @staticmethod
     def _fig_title(atlas_region, name, offset, roi_size):
         """Figure title parser.

magmap/gui/atlas_editor.py
@@ -67,6 +67,7 @@ class AtlasEditor(plot_support.ImageSyncMixin):
                  borders_img=None, fn_show_label_3d=None, title=None,
                  fn_refresh_atlas_eds=None, fig=None, fn_status_bar=None):
         """Plot ROI as sequence of z-planes containing only the ROI itself."""
+        super().__init__()
         self.image5d = image5d
         self.labels_img = labels_img
         self.channel = channel
@@ -378,21 +379,6 @@ class AtlasEditor(plot_support.ImageSyncMixin):
         return "{}.{}".format(plot_support.get_roi_path(
             os.path.basename(self.title), self.offset), ext)
     
-    def save_fig(self, path=None):
-        """Save the figure to file.
-        
-        Args:
-            path (str): Save path; defaults to None to use the path from
-                :meth:`get_save_path`.
-        
-        """
-        if not self.fig:
-            print("ROI Editor not yet initialized, skipping save")
-            return
-        if path is None:
-            path = self.get_save_path()
-        plot_support.save_fig(path, fig=self.fig)
-
     def toggle_edit_mode(self, event):
         """Toggle editing mode, determining the current state from the
         first :class:`magmap.plot_editor.PlotEditor` and switching to the 

magmap/plot/plot_support.py
@@ -25,7 +25,10 @@ except ImportError as e:
 
 
 class ImageSyncMixin:
-    """Mixin class for synchronizing image display settings."""
+    """Mixin class for synchronizing editors with Matplotlib figures."""
+    
+    def __init__(self):
+        self.fig = None
 
     @staticmethod
     def get_img_display_settings(plot_eds, imgi, chl=None):
@@ -76,6 +79,20 @@ class ImageSyncMixin:
             plot_ax_img = ed.update_img_display(
                 imgi, chl, minimum, maximum, brightness, contrast, alpha)
         return plot_ax_img
+    
+    def save_fig(self, path):
+        """Save the figure to file, with path based on filename, ROI,
+        and overview plane shown.
+        
+        Args:
+            path (str): Save path.
+        
+        """
+        if not self.fig:
+            print("Figure not yet initialized, skipping save")
+            return
+        # use module save fig function
+        save_fig(path, fig=self.fig)
 
 
 def imshow_multichannel(ax, img2d, channel, cmaps, aspect, alpha=None,

commit c826f9385ae0a5bc7200531f6a7c1993664c77d6

Fix regression in ROI Editor save shortcut
Commit 9ab75cc90ae33ec889a24ecda7aab15268b7518f introduced a regression where saving a figure did not pass the required path argument in the mixin class, fixed here.

magmap/gui/roi_editor.py
@@ -729,7 +729,7 @@ class ROIEditor(plot_support.ImageSyncMixin):
                 # support default save shortcuts on multiple platforms;
                 # ctrl-s will bring up save dialog from fig, but cmd/win-S
                 # will bypass
-                self.save_fig()
+                self.save_fig(self.get_save_path())
             else:
                 # default to scrolling commands for up/down/right arrows
                 scroll_overview(event)
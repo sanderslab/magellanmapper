6fed6e9dad7af68e98a09c8bbc0930b1ce905eab
David Young <david@textflex.com>
2020-10-28 17:57:32 +0800

    Use the `optparse` package to parse command-line arguments for R stats
    Add a command-line interface to R stats by importing the `optparse` package to parse command-line arguments. To support running `run.R` both within an R session (ie by sourcing) or from a shell (eg through Rscript), allow users to set the `args.parsed` list directly since arguments cannot be provided when sourcing the file. These arguments can be set in another script that in turn sources `run.R`.

--- clrstats/run.R
+++ clrstats/run.R
@@ -1,10 +1,14 @@
 #!/usr/bin/env R
-# Simple R stats run script
+# MagellanMapper R stats Command-Line Interface
 # Author: David Young, 2020
 
 # Usage:
-#   Rscript --verbose clrstats/run.R
-# Adjust profiles in clrstats.R before running
+#   1) From a shell: Rscript --verbose <path-to-clrstats>/run.R [options]
+#   2) From an R session: source("<path-to-clrstats>/run.R")
+#
+# Run with `-h` flag to see options. To set options when running from an
+# R session, create an `args.parsed` list and set these options in the list
+# (eg `args.parsed$file <- path/to/my/file.csv`).
 
 kWorkDir <- "clrstats"
 dir.start <- getwd()
@@ -16,7 +20,25 @@ if (dir.exists(kWorkDir)) {
 # load all source changes and run stats
 devtools::load_all(file.path(getwd(), "R"))
 tryCatch({
+  # set args.parsed directly if sourcing this file since arguments cannot
+  # be given to the parser
+  if (!exists("args.parsed")) {
+    # parse command-line arguments if args have not been set explicitly
+    parser <- optparse::OptionParser()
+    parser <- optparse::add_option(
+      parser, c("-f", "--file"), type="character",
+      help="Input file, generally a CSV/TSV file")
+    parser <- optparse::add_option(
+      parser, c("-p", "--profiles"), type="character",
+      help="Profile names delimited by comma")
+    args.parsed <- optparse::parse_args(parser)
+  }
+  cat("Parsed arguments:", paste(args.parsed), "\n")
+
+  # run main statistics
   runStats()
+}, error=function(e) {
+  message(e)
 }, finally={
   # return to original directory
   setwd(dir.start)

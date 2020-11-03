sanderslab/magellanmapper

commit 7320a359f50e87913217778019442882aaa402b2

Move image5d I/O source into `Image5d` class
- Refactor into class to encapsulate this data as it relates to the given image
- Set this value for Numpy-based images in the Numpy-based loaders

magmap/io/np_io.py
@@ -28,13 +28,15 @@ class Image5d:
         path_img (str): Path from which ``img`` was loaded; defaults to None.
         path_meta (str): Path from which metadata for ``img`` was loaded;
             defaults to None.
+        img_io (enum): I/O source for image5d array; defaults to None.
     
     """
-    def __init__(self, img=None, path_img=None, path_meta=None):
+    def __init__(self, img=None, path_img=None, path_meta=None, img_io=None):
         """Construct an Image5d object."""
         self.img = img
         self.path_img = path_img
         self.path_meta = path_meta
+        self.img_io = img_io
 
 
 def load_blobs(img_path, check_scaling=False, scaled_shape=None, scale=None):
@@ -209,9 +211,9 @@ def setup_images(path=None, series=None, offset=None, size=None,
             config.image5d = np.load(filename_subimg, mmap_mode="r")
             config.image5d = importer.roi_to_image5d(config.image5d)
             config.image5d_is_roi = True
-            config.image5d_io = config.LoadIO.NP
             config.img5d.img = config.image5d
             config.img5d.path_img = filename_subimg
+            config.img5d.img_io = config.LoadIO.NP
             print("Loaded sub-image from {} with shape {}"
                   .format(filename_subimg, config.image5d.shape))
 
@@ -265,9 +267,9 @@ def setup_images(path=None, series=None, offset=None, size=None,
             if path.endswith(sitk_io.EXTS_3D):
                 # attempt to format supported by SimpleITK and prepend time axis
                 config.image5d = sitk_io.read_sitk_files(path)[None]
-                config.image5d_io = config.LoadIO.SITK
                 config.img5d.img = config.image5d
                 config.img5d.path_img = path
+                config.img5d.img_io = config.LoadIO.SITK
             else:
                 # load or import from MagellanMapper Numpy format
                 import_only = proc_type is config.ProcessTypes.IMPORT_ONLY
@@ -305,7 +307,6 @@ def setup_images(path=None, series=None, offset=None, size=None,
                     # set loaded main image in config
                     config.img5d = img5d
                     config.image5d = config.img5d.img
-                config.image5d_io = config.LoadIO.NP
         except FileNotFoundError as e:
             print(e)
             print("Could not load {}, will fall back to any associated "
@@ -334,7 +335,7 @@ def setup_images(path=None, series=None, offset=None, size=None,
             config.image5d = sitk_io.read_sitk_files(
                 path, reg_names=atlas_suffix)[None]
             config.img5d.img = config.image5d
-            config.image5d_io = config.LoadIO.SITK
+            config.img5d.img_io = config.LoadIO.SITK
         except FileNotFoundError as e:
             print(e)
     

magmap/io/importer.py
@@ -633,7 +633,8 @@ def read_file(filename, series=None, offset=None, size=None, return_info=False,
             if load_info:
                 # load updated archive
                 metadata, image5d_ver_num = load_metadata(filename_meta)
-        img5d = np_io.Image5d(image5d, filename_image5d, filename_meta)
+        img5d = np_io.Image5d(
+            image5d, filename_image5d, filename_meta, config.LoadIO.NP)
         if return_info:
             return img5d, metadata
         return img5d
@@ -988,7 +989,8 @@ def import_multiplane_images(chl_paths, prefix, import_md, series=None,
                    .format(filename_image5d, md), fn_feedback)
     if jb_attached:
         jb.detach()
-    return np_io.Image5d(image5d, filename_image5d, filename_meta)
+    return np_io.Image5d(
+        image5d, filename_image5d, filename_meta, config.LoadIO.NP)
 
 
 def _parse_import_chls(paths):
@@ -1148,7 +1150,8 @@ def import_planes_to_stack(chl_paths, prefix, import_md, rgb_to_grayscale=True,
     assign_metadata(md)
     libmag.printcb("Saved image to \"{}\" with the following metadata:\n{}"
                    .format(filename_image5d_npz, md), fn_feedback)
-    return np_io.Image5d(image5d, filename_image5d_npz, filename_info_npz)
+    return np_io.Image5d(
+        image5d, filename_image5d_npz, filename_info_npz, config.LoadIO.NP)
 
 
 def calc_intensity_bounds(image5d, lower=0.5, upper=99.5, dim_channel=4):

magmap/plot/plot_support.py
@@ -846,7 +846,7 @@ def get_downsample_max_sizes():
     """
     max_sizes = None
     downsample_io = config.atlas_profile["editor_downsample_io"]
-    if downsample_io and config.image5d_io in downsample_io:
+    if downsample_io and config.img5d and config.img5d.img_io in downsample_io:
         max_sizes = config.atlas_profile["editor_max_sizes"]
         if max_sizes:
             max_sizes = max_sizes[::-1]

commit e5da520809639f282048cbc09c7885063ac52f00

Fix attempting to reimport main image after loading
Commit 7320a359f50e87913217778019442882aaa402b2 introduced a regression where a loaded main image would not be detected when checking whether to import the image, fixed here.

magmap/io/np_io.py
@@ -292,7 +292,7 @@ def setup_images(path=None, series=None, offset=None, size=None,
                                 importer.DEFAULT_IMG_STACK_NAME)
                         img5d = importer.import_planes_to_stack(
                             chls, prefix, import_md)
-                    elif import_only or config.image5d is None:
+                    elif import_only or img5d is None:
                         # import multi-plane image
                         chls, import_path = importer.setup_import_multipage(
                             path)
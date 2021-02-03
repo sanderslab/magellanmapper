sanderslab/magellanmapper

commit 59cf735e9752287f3c5cc1b6479b04269222d0bd

Refactor blob saving to use the Blobs class
Generalize blob saving from the blobs archive update function and use this function when saving blobs generated during stack detection. Also, remove the file save time output during stack detection since this time is generally so small.

magmap/cv/detector.py
@@ -53,19 +53,26 @@ class Blobs:
         self.colocalizations = colocalizations
         self.path = path
     
-    def update_archive(self, to_add):
-        """Update the blobs Numpy archive file.
+    def save_archive(self, to_add, update=False):
+        """Save the blobs Numpy archive file to :attr:`path`.
         
         Args:
             to_add (dict): Dictionary of items to add.
+            update (bool): True to load the Numpy archive at :attr:`path`
+                and update it.
 
         Returns:
-            dict: Updated dictionary saved to :attr:`path`.
+            dict: Dictionary saved to :attr:`path`.
 
         """
         with np.load(self.path) as archive:
-            blobs_arc = np_io.read_np_archive(archive)
-            blobs_arc.update(to_add)
+            if update:
+                # load archive, convert to dict, and update dict
+                blobs_arc = np_io.read_np_archive(archive)
+                blobs_arc.update(to_add)
+            else:
+                blobs_arc = to_add
+            # save as uncompressed zip Numpy archive file
             np.savez(self.path, **blobs_arc)
         if config.verbose:
             pprint.pprint(blobs_arc)

magmap/cv/stack_detect.py
@@ -305,7 +305,8 @@ def detect_blobs_large_image(filename_base, image5d, offset, size,
         offset = (0, 0, 0)
     else:
         # get base path for sub-image
-        subimg_path_base = naming.make_subimage_name(filename_base, offset, size)
+        subimg_path_base = naming.make_subimage_name(
+            filename_base, offset, size)
     filename_blobs = libmag.combine_paths(subimg_path_base, config.SUFFIX_BLOBS)
     
     # get ROI for given region, including all channels
@@ -441,7 +442,6 @@ def detect_blobs_large_image(filename_base, image5d, offset, size,
             except LookupError as e:
                 libmag.warn(str(e))
     
-    file_time_start = time()
     if config.save_subimg:
         subimg_base_path = libmag.combine_paths(
             subimg_path_base, config.SUFFIX_SUBIMG)
@@ -458,14 +458,13 @@ def detect_blobs_large_image(filename_base, image5d, offset, size,
 
     # save blobs
     # TODO: only segments used; consider removing the rest except ver
-    outfile_blobs = open(filename_blobs, "wb")
-    np.savez(outfile_blobs, ver=BLOBS_NP_VER, segments=segments_all,
-             resolutions=config.resolutions,
-             basename=os.path.basename(config.filename),  # only save name
-             offset=offset, roi_size=size,  # None unless explicitly set
-             colocs=colocs)
-    outfile_blobs.close()
-    file_save_time = time() - file_time_start
+    blobs = detector.Blobs(path=filename_blobs)
+    blobs.save_archive(dict(
+        ver=BLOBS_NP_VER, segments=segments_all,
+        resolutions=config.resolutions,
+        basename=os.path.basename(config.filename),  # only save name
+        offset=offset, roi_size=size,  # None unless explicitly set
+        colocs=colocs))
     
     # whole image benchmarking time
     times = (
@@ -480,7 +479,6 @@ def detect_blobs_large_image(filename_base, image5d, offset, size,
     else:
         print("\nTotal blobs found:", len(segments_all))
         detector.show_blobs_per_channel(segments_all)
-    print("file save time:", file_save_time)
     print("\nTotal detection processing times (s):")
     path_times = "stack_detection_times.csv" if save_dfs else None
     df_io.dict_to_data_frame(times_dict, path_times, show=" ")

commit ce03390e67cbf7087a81dcf52d1640174b6e1011

Fix saving blobs archive when it does not yet exist
Commit 59cf735e9752287f3c5cc1b6479b04269222d0bd introduced a regression where blobs archives needed to be loaded prior to saving, even if the archive was not being updates. This loading causes an error when the archives has not yet been saved for the first time. Fix by loading the archive during saving only when updating an existing archive. Also, the context manager was not being used for the save operation, fixed here by adding another context manager.

magmap/cv/detector.py
@@ -135,15 +135,15 @@ class Blobs:
             dict: Dictionary saved to :attr:`path`.
 
         """
-        with np.load(self.path) as archive:
-            if update:
+        blobs_arc = to_add
+        if update:
+            with np.load(self.path) as archive:
                 # load archive, convert to dict, and update dict
                 blobs_arc = np_io.read_np_archive(archive)
                 blobs_arc.update(to_add)
-            else:
-                blobs_arc = to_add
+        with open(self.path, "wb") as archive:
             # save as uncompressed zip Numpy archive file
-            np.savez(self.path, **blobs_arc)
+            np.savez(archive, **blobs_arc)
         if config.verbose:
             pprint.pprint(blobs_arc)
         return blobs_arc
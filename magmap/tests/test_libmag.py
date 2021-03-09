0d2941581e100f5217186cc2cf695fffdef976cd
Aparna Kumar <akumar24@berkeley.edu>
2021-03-08 19:22:07 -0800

    Extended tests on insert_before_ext and added tests for libmag
    Added two more tests for the insert_before_ext function and added tests for the functions splitext, match_ext, and get_filename_without_ext.

--- magmap/tests/test_libmag.py
+++ magmap/tests/test_libmag.py
@@ -13,8 +13,20 @@ class TestLibmag(unittest.TestCase):
     def test_insert_before_ext(self):
         self.assertEqual(libmag.insert_before_ext(
             "foo/bar/item.py", "totest", "_"), "foo/bar/item_totest.py")
+        self.assertEqual(libmag.insert_before_ext(
+            "foo/bar/item.py", "totest"), "foo/bar/itemtotest.py")
+        self.assertEqual(libmag.insert_before_ext(
+            "foo/bar/item", "totest", "_"), "foo/bar/item_totest")
+            
+    def test_splitext(self):
+        self.assertEqual(libmag.splitext("foo/bar/item.py"), ("foo/bar/item", ".py"))
         
-    
+    def test_match_ext(self):
+        self.assertEqual(libmag.match_ext(
+            "foo1/bar1/item1.ext1", "foo2/bar2/item2.ext2"), "foo2/bar2/item2.ext1")
+            
+    def test_get_filename_without_ext(self):
+        self.assertEqual(libmag.get_filename_without_ext("foo/bar/item.py"), "item")
 
 if __name__ == "__main__":
     unittest.main()

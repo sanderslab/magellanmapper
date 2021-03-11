954a43a3df4f7f8af3e7e1f6d7c7eda43d432082
David Young <david@textflex.com>
2021-03-11 18:55:51 +0800

    Initial Pyinstaller specification script
    Add a script for freezing an environment using Pyinstaller. Add a packaging function to retrieve the install path of a given package to include additional folders not auto-detected by Pyinstaller.

--- magmap/io/packaging.py
+++ magmap/io/packaging.py
@@ -3,6 +3,7 @@
 
 import os
 import pkg_resources
+import importlib_resources
 
 
 def get_pkg_egg(name, prefix=None):
@@ -22,4 +23,27 @@ def get_pkg_egg(name, prefix=None):
         egg_name = os.path.join(prefix, egg_name)
     paths = (distrib.egg_info, egg_name)
     print("Adding package egg-info path:", paths)
+    return paths
+
+
+def get_pkg_path(name, prefix=None):
+    """Get path to the installed package.
+
+    Args:
+        name (str): Package name.
+        prefix (str): Start of output path; defaults to None.
+
+    Returns:
+        str, str: The package path and output path for the given package.
+
+    """
+    pkg_dir = name
+    for entry in importlib_resources.files(name).iterdir():
+        pkg_dir = os.path.dirname(entry)
+        break
+    out_path = os.path.basename(pkg_dir)
+    if prefix:
+        out_path = os.path.join(prefix, out_path)
+    paths = (pkg_dir, out_path)
+    print("Adding package path:", paths)
     return paths

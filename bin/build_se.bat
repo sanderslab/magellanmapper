99034e9adbe0c05f32163c176a912cf07e1d044c
David Young <david@textflex.com>
2021-04-06 17:05:37 +0800

    Options to configure output directory and SimpleElastix folder in Batch script
    Parse user arguments in preparation for building for multiple Python versions. Also, add specific MSVC requirements in docs.

--- bin/build_se.bat
+++ bin/build_se.bat
@@ -3,6 +3,22 @@
 
 :: Build SimpleElastix for Windows using MSVC build tools. Compiled files
 :: will be found in ../../build_se relative to this script.
+::
+:: Usage:
+::   build_se.bat [build-dir] [path-to-SimpleElastix]
+::
+:: Args:
+::   build-dir: Path to output build directory.
+::   path-to-SimpleElastix: Path to existing SimpleElastix directory or
+::     where it will be stored.
+::
+:: MSVC requirements:
+:: - MSVC C++ x64/x86 build tools (tested on MSVC v142 VS 2019 C++ with the
+::   "Latest" version checked)
+:: - C++ CMake Tools for Windows
+:: - Git (eg "Git for Windows")
+:: - Run this script in an "x64 Native Tools Command Prompt to VS 2019"
+::
 :: Assumes:
 :: - The SimpleElastix git repo has been cloned to the parent
 ::   folder of this script's folder
@@ -10,8 +26,24 @@
 ::   the CONDA_PREFIX environment variable
 :: Tested with CMake 3.15; has *not* worked with MSVS CMake 3.14 on our testing.
 
-SET "cmake=cmake.exe"
+:: parse arg for output build directory
 SET "build_dir=build_se"
+IF NOT "%~1" == "" (
+  SET "build_dir=%~1"
+)
+ECHO Setting the build directory path to %build_dir%
+
+:: parse arg for SimpleElastix, converting to an absolute path
+SET "se_dir=..\SimpleElastix"
+IF NOT "%~2" == "" (
+  SET "se_dir=%~2"
+)
+call :get_abs_path "%se_dir%"
+SET "se_dir=%abs_path%"
+ECHO Setting the SimpleElastix path to %se_dir%
+
+:: set up Cmake
+SET "cmake=cmake.exe"
 SET "generator=Visual Studio 16 2019"
 SET "arch=x64"
 SET "python_exe="
@@ -33,9 +65,9 @@ cd "%build_dir%"
  %python_exe% -DWRAP_LUA:BOOL=OFF -DWRAP_R:BOOL=OFF -DWRAP_RUBY:BOOL=OFF^
  -DWRAP_TCL:BOOL=OFF -DWRAP_CSHARP:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF^
  -DBUILD_TESTING:BOOL=OFF -DSimpleITK_PYTHON_USE_VIRTUALENV:BOOL=OFF^
- ..\SimpleElastix\SuperBuild
+ "%se_dir%\SuperBuild"
 "%cmake%" -G "%generator%" -A "%arch%" -T host="%arch%"^
- ..\SimpleElastix\SuperBuild
+ "%se_dir%\SuperBuild"
 
 :: build SimpleElastix
 msbuild ALL_BUILD.vcxproj /p:Configuration=Release
@@ -45,3 +77,10 @@ cd SimpleITK-build/Wrapping/Python
 python Packaging/setup.py build sdist bdist_wheel
 
 popd
+
+exit /b
+
+:: Convert a path to an absolute path.
+:get_abs_path
+  set "abs_path=%~f1
+  exit /b

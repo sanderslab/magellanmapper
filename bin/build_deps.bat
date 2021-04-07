f1d2f2d3a4556289a0f4a2122aa3a6939089c10c
David Young <david@textflex.com>
2021-04-07 19:25:08 +0800

    Batch script to build dependencies for multiple Python versions
    Provide a batch script corresponding to the Bash script to wrap dependency building in separate Python environments, one for each supported Python version.

---
+++ bin/build_deps.bat
@@ -0,0 +1,43 @@
+@echo off
+rem Build custom binaries for MagellanMapper dependencies on Windows platforms
+
+rem Usage:
+rem   setup_venv.sh [envs-dir] [output-dir]
+
+rem Args:
+rem   [env-dir]: Path to environment directory; defaults to `..\venvs\vmag`.
+rem   [output-dir]: Path to output directory; defaults to `..\build_deps`.
+
+
+rem parse user arg for path containing Venvs
+set "venvs_dir=..\venvs"
+if not "%~1" == "" (
+  set "venvs_dir=%~1"
+)
+echo Setting the Venvs directory path to %venvs_dir%
+
+rem parse user env directory path
+set "output_dir=..\build_deps"
+if not "%~2" == "" (
+  set "output_dir=%~2"
+)
+echo Setting the output directory path to %output_dir%
+
+pushd "%~dp0"
+cd ..
+
+rem build binaries within each Python version Venv environment
+for %%v in (3.6.8) do (
+  echo Activating Venv for py%%v
+  call "%venvs_dir%\py%%v\Scripts\Activate.bat"
+  call pip install wheel
+  call bin\build_se.bat "%output_dir%\build_se_py%%v"
+  call copy "%output_dir%\build_se_py%%v\SimpleITK-build\Wrapping\Python\dist\"*.whl "%output_dir%"
+  call copy "%output_dir%\build_se_py%%v\SimpleITK-build\Wrapping\Python\dist\"*.tar.gz "%output_dir%"
+  call deactivate
+)
+
+popd
+
+rem keep window open if user double-clicked this script to launch it
+pause

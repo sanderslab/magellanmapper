f9427c38a882682ec62f0beba82f3b714ac7ebf6
David Young <david@textflex.com>
2021-04-08 15:37:51 +0800

    Extend dependencies build script to building Javabridge
    - Generalize Venv activation
    - Build Javabridge, installing Numpy <=1.19 to retain compatibility with Python 3.6
    - Deactivate the Venv before activating the next one

--- bin/build_deps.sh
+++ bin/build_deps.sh
@@ -2,9 +2,9 @@
 # Build wheels for MagellanMapper dependencies
 
 venv_dir="../venvs"
-output_dir="../builds_se"
+output_dir="../build_deps"
 HELP="
-Create Venv environments for multiple Python versions.
+Build dependencies in Venv environments for multiple Python versions.
 
 Assumes that environments for each Python version have been set up using
 \"setup_multi_venvs.sh\".
@@ -14,13 +14,15 @@ Arguments:
   -d [path]: Path to build directory; defaults to \"$output_dir\".
   -e [path]: Path to folder where the new venv directory will be placed. 
     Defaults to \"$venv_dir\".
+  -j [path]: Path to Javabridge directory, passed to \"build_jb.sh\".
   -s [path]: Path to SimpleElastix directory, passed to \"build_se.sh\".
 "
 
 se_dir=()
+jb_dir=()
 
 OPTIND=1
-while getopts hd:e:s: opt; do
+while getopts hd:e:j:s: opt; do
   case $opt in
     h)
       echo "$HELP"
@@ -33,6 +35,10 @@ while getopts hd:e:s: opt; do
     e)
       venv_dir="$OPTARG"
       echo "Set the venv directory to $venv_dir"
+      ;;
+    j)
+      jb_dir+=(-d "$OPTARG")
+      echo "Changed Python-Javabridge directory to $OPTARG"
       ;;
     s)
       se_dir+=(-s "$OPTARG")
@@ -55,28 +61,49 @@ echo "Initating build of SimpleElastix..."
 BASE_DIR="$(dirname "$0")/.."
 cd "$BASE_DIR" || { echo "Unable to find folder $BASE_DIR, exiting"; exit 1; }
 
-# Build SimpleElastix in the given Python version environment.
+# Activate Venv environment for the given Python version and install wheels
+# package.
 # Args:
-#   1: Python version, used to activate the corresponding Venv environment.
-build_se_ver() {
+#   1: Python version given as x.y, eg "3.6".
+# Returns:
+#   1 if the environment could not be activated, 0 otherwise.
+activate_venv() {
   # activate the environment
   local ver="$1"
   local vdir
   vdir="$(cd "${venv_dir}/py${ver}" && pwd)"
+  echo "Activating $vdir"
   . "${vdir}/bin/activate"
   if [[ "$VIRTUAL_ENV" != "$vdir" ]]; then
     echo "Could not activate $vdir Venv environment, skipping"
-    return
+    return 1
   fi
   
   # install package to build wheels
   pip install wheel
-  
-  # build SimpleElastix and copy wheel to output dir
+  return 0
+}
+
+# Build SimpleElastix in the given Python version environment.
+# Args:
+#   1: Python version, used to activate the corresponding Venv environment.
+build_se_ver() {
+  # build SimpleElastix
+  local ver="$1"
   local output="${output_dir}/build_se_py${ver}"
   bin/build_se.sh -d "$output" "${se_dir[@]}"
+  
+  # copy wheel to output dir
   local dist="${output}/SimpleITK-build/Wrapping/Python/dist"
   cp -v "${dist}/"*.whl "${dist}/"*.tar.gz "$output_dir"
+}
+
+# Build Javabridge in the given Python version environment.
+# Also installs Cython and Numpy.
+build_jb_ver() {
+  pip install cython
+  pip install numpy<=1.19 # v1.19 is last ver supporting Python 3.6
+  bin/build_jb.sh "${jb_dir[@]}" -o "$output_dir" -j "$(/usr/libexec/java_home -v 1.8)"
 }
 
 if [[ ! -d "$output_dir" ]]; then
@@ -87,5 +114,9 @@ fi
 # build dependencies for each of all supported Python versions
 py_vers=(3.6 3.7 3.8 3.9)
 for py_ver in "${py_vers[@]}"; do
-  build_se_ver "$py_ver"
+  if activate_venv "$py_ver"; then
+    build_se_ver "$py_ver"
+    build_jb_ver
+    deactivate
+  fi
 done

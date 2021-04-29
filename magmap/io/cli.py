sanderslab/magellanmapper

commit 67391e191ebecf351f8a5ee911eb9f55c1071b87

Configure log levels and path by command-line argument
Extend the `--verbose` setting to `--verbose LEVEL=<level> LOG_PATH=<path>` for level and output path sub-arguments.

- Move log functions to its own module
- Allow the log level to be configured, including support for specification by string, corresponding integer, or a simplified (0-9) integer
- Allow the output path to be configured, including no log file output if a path is not specified

magmap/io/cli.py
@@ -61,7 +61,7 @@ from magmap.cloud import notify
 from magmap.cv import chunking, colocalizer, stack_detect
 from magmap.io import df_io, export_stack, importer, libmag, np_io, sqlite
 from magmap.plot import colormaps, plot_2d
-from magmap.settings import atlas_prof, config, grid_search_prof, roi_prof
+from magmap.settings import atlas_prof, config, grid_search_prof, logs, roi_prof
 from magmap.stats import mlearn
 
 _logger = config.logger.getChild(__name__)
@@ -386,9 +386,25 @@ def process_cli_args():
     parser.add_argument(
         "--groups", nargs="*", help="Group values corresponding to each image")
     parser.add_argument(
-        "-v", "--verbose", action="store_true",
-        help="Verbose output to assist with debugging")
+        "-v", "--verbose", nargs="*",
+        help=_get_args_dict_help(
+            "Verbose output to assist with debugging; see config.Verbosity.",
+            config.Verbosity))
     args = parser.parse_args()
+
+    if args.verbose:
+        # verbose mode, including printing longer Numpy arrays for debugging
+        config.verbose = True
+        config.verbosity = args_to_dict(
+            args.verbose, config.Verbosity, config.verbosity)
+        logs.update_log_level(
+            config.logger, config.verbosity[config.Verbosity.LEVEL])
+        log_path = config.verbosity[config.Verbosity.LOG_PATH]
+        if log_path:
+            # log to file
+            logs.add_file_handler(config.logger, log_path)
+        np.set_printoptions(linewidth=200, threshold=10000)
+        _logger.info("Set verbose to {}".format(config.verbosity))
     
     if args.img is not None or args.img_paths:
         # set image file path and convert to basis for additional paths
@@ -431,12 +447,6 @@ def process_cli_args():
         # save figure with file type of this extension; remove leading period
         config.savefig = _parse_none(args.savefig.lstrip("."))
         print("Set savefig extension to {}".format(config.savefig))
-
-    if args.verbose:
-        # verbose mode, including printing longer Numpy arrays for debugging
-        config.verbose = args.verbose
-        np.set_printoptions(linewidth=200, threshold=10000)
-        print("Set verbose to {}".format(config.verbose))
 
     # parse sub-image offsets and sizes;
     # expects x,y,z input but stores as z,y,x by convention

magmap/settings/config.py
@@ -25,12 +25,11 @@ Attributes:
 """
 
 from enum import Enum, auto
-import logging
 
 import numpy as np
 
-#: bool: True for verbose debugging output.
-verbose = False
+from magmap.settings import logs
+
 #: float: Threshold for positive values for float comparison.
 POS_THRESH = 0.001
 #: int: Number of CPUs for multiprocessing tasks; defaults to None to
@@ -40,25 +39,19 @@ cpus = None
 
 # LOGGING
 
+class Verbosity(Enum):
+    LEVEL = auto()
+    LOG_PATH = auto()
+
+
+#: dict: Command-line arguments for verbosity.
+verbosity = dict.fromkeys(Verbosity, None)
+
+#: bool: True for verbose debugging output.
+verbose = False
+
 #: :class:`logging.Logger`: Root logger for the application.
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-# set up handler for console
-_log_handler_stream = logging.StreamHandler()
-_log_handler_stream.setLevel(logging.INFO)
-_log_handler_stream.setFormatter(logging.Formatter(
-    "%(name)s - %(levelname)s - %(message)s"))
-
-# set up file handler
-_log_handler_file = logging.FileHandler("out.log")
-_log_handler_file.setLevel(logging.INFO)
-_log_handler_file.setFormatter(logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-
-# add logging handlers to root logger
-logger.addHandler(_log_handler_stream)
-logger.addHandler(_log_handler_file)
+logger = logs.setup_logger()
 
 
 # IMAGE FILES

magmap/settings/logs.py
@@ -1,0 +1,78 @@
+# MagellanMapper logging
+"""Logging utilities."""
+
+import logging
+
+
+def setup_logger():
+    """Set up a basic root logger with a stream handler.
+    
+    Returns:
+        :class:`logging.Logger`: Root logger for the application.
+
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    # set up handler for console
+    handler_stream = logging.StreamHandler()
+    handler_stream.setLevel(logging.INFO)
+    handler_stream.setFormatter(logging.Formatter(
+        "%(name)s - %(levelname)s - %(message)s"))
+    logger.addHandler(handler_stream)
+    
+    return logger
+
+
+def update_log_level(logger, level):
+    """Update the logging level.
+    
+    Args:
+        logger (:class:`logging.Logger`): Logger to update.
+        level (Union[str, int]): Level given either as a string corresponding
+            to ``Logger`` levels, or their corresponding integers, ranging
+            from 0 (``NOTSET``) to 50 (``CRITICAL``). For convenience,
+            values can be given from 0-5, which will be multiplied by 10.
+
+    Returns:
+        :class:`logging.Logger`: The logger for chained calls.
+
+    """
+    if isinstance(level, str):
+        # specify level by level name
+        level = level.upper()
+    elif isinstance(level, int):
+        # specify by level integer (0-50)
+        if level < 10:
+            # for convenience, assume values under 10 are 10-fold
+            level *= 10
+    else:
+        return
+    
+    try:
+        # set level for the logger and all its handlers
+        logger.setLevel(level)
+        for handler in logger.handlers:
+            handler.setLevel(level)
+    except (TypeError, ValueError) as e:
+        logger.error(e, exc_info=True)
+    return logger
+
+
+def add_file_handler(logger, path):
+    """Add a log file handler.
+    
+    Args:
+        logger (:class:`logging.Logger`): Logger to update.
+        path (str): Path to log.
+
+    Returns:
+        :class:`logging.Logger`: The logger for chained calls.
+
+    """
+    handler_file = logging.FileHandler(path)
+    handler_file.setLevel(logger.level)
+    handler_file.setFormatter(logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+    logger.addHandler(handler_file)
+    return logger

commit 232062ea31ba1dce1ecab294be44ae6afac7570c

Fix regression in applying verbose mode without sub-arguments
- Commit 67391e191ebecf351f8a5ee911eb9f55c1071b87 introduced a regression where the verbose flag was no longer applied unless it had sub-arguments, fixed here
- Default to debug mode if the verbose flag is given without setting the log level
- Use printf-style for log argument logging for slight performance optimization

magmap/io/cli.py
@@ -51,6 +51,7 @@ Command-line arguments in addition to those from attributes listed below:
 """
 
 import argparse
+import logging
 import os
 import sys
 
@@ -394,15 +395,20 @@ def process_cli_args():
     # only parse recognized arguments to avoid error for unrecognized ones
     args, args_unknown = parser.parse_known_args()
 
-    if args.verbose:
-        # verbose mode, including printing longer Numpy arrays for debugging
+    if args.verbose is not None:
+        # verbose mode and logging setup
         config.verbose = True
         config.verbosity = args_to_dict(
             args.verbose, config.Verbosity, config.verbosity)
+        if config.verbosity[config.Verbosity.LEVEL] is None:
+            # default to debug mode if any verbose flag is set without level
+            config.verbosity[config.Verbosity.LEVEL] = logging.DEBUG
         logs.update_log_level(
             config.logger, config.verbosity[config.Verbosity.LEVEL])
+        
+        # print longer Numpy arrays for debugging
         np.set_printoptions(linewidth=200, threshold=10000)
-        _logger.info("Set verbose to {}".format(config.verbosity))
+        _logger.info("Set verbose to %s", config.verbosity)
     
     # set up logging to given file unless explicitly given an empty string
     log_path = config.verbosity[config.Verbosity.LOG_PATH]
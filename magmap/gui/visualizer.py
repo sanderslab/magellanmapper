97156d9768a720dfe8f9789d864cc11000d9e0e8
David Young <david@textflex.com>
2021-07-05 19:15:17 +0800

    Trigger saving window position and size preferences through TraitsUI
    Add an ID to the main View to trigger saving and restoring window position and size through TraitsUI.

--- magmap/gui/visualizer.py
+++ magmap/gui/visualizer.py
@@ -755,6 +755,8 @@ class Visualization(HasTraits):
         statusbar="_status_bar_msg",
         resizable=True,
         icon=icon_img,
+        # set ID to trigger saving TraitsUI preferences for window size/position
+        id=f"{__name__}.{__qualname__}",
     )
 
     def __init__(self):

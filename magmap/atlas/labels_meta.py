2acb32c5bc61fac634dd50d25d0cba407ac8d7f0
David Young <david@textflex.com>
2021-07-13 13:54:20 +0800

    Simplify making reference path absolute in labels metadata
    - Ensure that the metadata is loaded from a regular file rather than a directory
    - Use this load path to make the relative reference file path absolute rather than converting the prefix
    - Add labels metadata docs

--- magmap/atlas/labels_meta.py
+++ magmap/atlas/labels_meta.py
@@ -1,25 +1,48 @@
+"""Labels image metadata."""
+
 import pathlib
+from typing import Optional, Sequence
 
 from magmap.io import libmag, yaml_io
 from magmap.settings import config
-
 
 _logger = config.logger.getChild(__name__)
 
 
 class LabelsMeta:
+    """Labels image metadata container and I/O.
+    
+    Attributes:
+        prefix: Path prefix for saving metadata and locating the reference
+            file path.
+        save_path: Path to save this metadata.
+        path_ref: Path to labels reference file.
+        region_ids_orig: Sequence of original label IDs.
+    
+    """
     PATH_LABELS_META: str = "meta_labels.yml"
     
-    def __init__(self, prefix=None):
+    def __init__(self, prefix: Optional[str] = None):
+        """Initialize metadata."""
         super().__init__()
-        self.prefix = prefix
-        self.save_path = None
+        # output paths
+        self.prefix: Optional[str] = prefix
+        self.save_path: Optional[str] = None
         
-        self.path_ref = None
-        self.region_ids_orig = None
+        # metadata
+        self.path_ref: Optional[str] = None
+        self.region_ids_orig: Optional[Sequence[int]] = None
     
     @property
     def save_path(self):
+        """Get the save path.
+        
+        Returns:
+            The save path if set, otherwise a path constructed from
+            :attr:`prefix` if set and :const:`PATH_LABELS_META`, or the
+            constant alone.
+
+        """
         if not self._save_path:
             if self.prefix:
                 return libmag.combine_paths(
@@ -29,12 +52,18 @@ class LabelsMeta:
     
     @save_path.setter
     def save_path(self, val):
+        """Set the save path."""
         self._save_path = val
     
     def save(self):
+        """Save the metadata.
+        
+        Also copies the reference file to the metadata's directory.
+        
+        """
         labels_ref_name = None
         if self.path_ref:
-            # if provided, copy labels file to import directory
+            # if provided, copy labels reference file to output dir
             labels_ref_name = pathlib.Path(self.path_ref).name
             labels_ref_out = labels_ref_name
             if self.prefix:
@@ -42,27 +71,36 @@ class LabelsMeta:
                     self.prefix, labels_ref_name, check_dir=True)
                 labels_ref_name = pathlib.Path(labels_ref_out).name
             libmag.copy_backup(self.path_ref, labels_ref_out)
+        
+        # save metadata as YAML file
         meta = {
+            # reference filename is relative to output directory
             "path_ref": labels_ref_name,
             "region_ids_orig": self.region_ids_orig,
         }
         yaml_io.save_yaml(self.save_path, meta, True, True)
 
     def load(self):
+        """Load metadata from a YAML file."""
+        # load from save location
         meta_path = pathlib.Path(self.save_path)
         meta = None
-        if meta_path.exists():
+        if meta_path.is_file():
+            # load YAML file from save location
             loaded = yaml_io.load_yaml(str(meta_path))
-            _logger.debug("Loaded labels metadata from: %s", meta_path)
             if loaded:
+                # get first YAML document
                 meta = loaded[0]
-                print(meta)
+                _logger.debug("Loaded labels metadata from: %s", meta_path)
+                _logger.debug(meta)
+                
                 path_ref = meta["path_ref"]
-                if path_ref and self.prefix:
-                    prefix = pathlib.Path(self.prefix)
-                    if not prefix.is_dir():
-                        prefix = prefix.parent
-                    path_ref = str(prefix / path_ref)
+                if path_ref:
+                    # path ref is relative to metadata file; make absolute
+                    path_ref = str(
+                        pathlib.Path(self.save_path).parent / path_ref)
+                
+                # set attributes to loaded metadata
                 self.path_ref = path_ref
                 self.region_ids_orig = meta["region_ids_orig"]
         if not meta:

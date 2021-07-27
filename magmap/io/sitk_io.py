sanderslab/magellanmapper

commit da6efaf41254652edce0f2715370e87c1dfab7b3

Support specifying registered image suffixes as absolute paths
Registered image suffixes have been assumed to be appended to the main image path. These suffixes are also currently the only way to load images as labels or borders. Occasionally, users may wish to load a labels image that is not registered to the main image, which the registered suffixes does not currently support. As a way to specify a non-registered path while still using the registered suffixes to load certain file types such as labels, load "registered suffixes" given as absolute paths directly, without modifying the path.

magmap/io/sitk_io.py
@@ -85,7 +85,7 @@ def match_world_info(source, target):
           "direction from {} to {}"
           .format(target.GetSpacing(), spacing, target.GetOrigin(), origin,
                   target.GetDirection(), direction))
-    target.SetSpacing(spacing)
+    # target.SetSpacing(spacing)
     target.SetOrigin(origin)
     target.SetDirection(direction)
 
@@ -154,9 +154,10 @@ def _load_reg_img_to_combine(path, reg_name, img_nps):
 
 
 def read_sitk_files(filename_sitk, reg_names=None, return_sitk=False):
-    """Read an image file through SimpleITK and export to Numpy array format,
-    with support for combining multiple registered image files into a single
-    image.
+    """Read image files through SimpleITK.
+    
+    Supports identifying files based on registered suffixes and combining
+    multiple registered image files into a single image.
     
     Also sets up spacing from the first loaded image in
     :attr:`magmap.settings.config.resolutions` if not already set.
@@ -228,26 +229,30 @@ def load_registered_img(img_path, reg_name, get_sitk=False, return_path=False):
     """Load atlas-based image that has been registered to another image.
     
     Args:
-        img_path: Path as had been given to generate the registered images, 
-            with the parent path of the registered images and base name of 
-            the original image.
-        reg_name: Atlas image suffix to open.
-        get_sitk: True if the image should be returned as a SimpleITK image; 
-            defaults to False, in which case the corresponding Numpy array will 
-            be extracted instead.
+        img_path (str): Path as had been given to generate the registered
+            images, with the parent path of the registered images and base name 
+            of the original image.
+        reg_name (str): Atlas image suffix to open. Can be an absolute path,
+            which will be used directly, ignoring ``img_path``.
+        get_sitk (bool): True if the image should be returned as a SimpleITK
+            image; defaults to False, in which case the corresponding Numpy
+            array will be extracted instead.
         return_path (bool): True to return the path from which the image
             was loaded; defaults to False.
     
     Returns:
-        :obj:`np.ndarray`: The atlas-based image as a Numpy array, or a
-        :obj:`sitk.Image` object if ``get_sitk`` is True. Also returns the
+        :class:`numpy.ndarray`: The atlas-based image as a Numpy array, or a
+        :class:`sitk.Image` object if ``get_sitk`` is True. Also returns the
         loaded path if ``return_path`` is True.
     
     Raises:
         ``FileNotFoundError`` if the path cannot be found.
     """
-    # prioritize registered image extension matched to that of main image
-    reg_img_path = reg_out_path(img_path, reg_name, True)
+    reg_img_path = reg_name
+    if not os.path.isabs(reg_name):
+        # use suffix given as an absolute path directly; otherwise, get
+        # registered image extension matched to that of main image
+        reg_img_path = reg_out_path(img_path, reg_name, True)
     reg_img, reg_img_path = read_sitk(reg_img_path)
     if reg_img is None:
         # fallback to loading barren reg_name from img_path's dir

commit f47831dc88a0681a3c7b7aa3436c4bfa9b327bde

Make matching world info customizable
Commit da6efaf41254652edce0f2715370e87c1dfab7b3 inadvertently turned off spacing match when copying world info from one image to another. Extend the function to make each component of the world info customizable, including toggling whether to match components or to supply an alternative value. Default to match all components, restoring the original behavior. Also, convert to logging.

magmap/io/sitk_io.py
@@ -18,6 +18,8 @@ from magmap.io import libmag
 
 EXTS_3D = (".mhd", ".mha", ".nii.gz", ".nii", ".nhdr", ".nrrd")
 
+_logger = config.logger.getChild(__name__)
+
 
 def reg_out_path(file_path, reg_name, match_ext=False):
     """Generate a path for a file registered to another file.
@@ -65,7 +67,11 @@ def replace_sitk_with_numpy(img_sitk, img_np):
     return img_sitk_back
 
 
-def match_world_info(source, target):
+def match_world_info(
+        source: sitk.Image, target: sitk.Image,
+        spacing: Union[bool, Tuple[int], List[int]] = True,
+        origin: Union[bool, Tuple[int], List[int]] = True,
+        direction: Union[bool, Tuple[int], List[int]] = True):
     """Copy world information (eg spacing, origin, direction) from one
     image object to another.
 
@@ -77,18 +83,33 @@ def match_world_info(source, target):
             will be copied into ``target``.
         target (:obj:`sitk.Image`): Target object whose corresponding
             metadata will be overwritten by that of ``source``.
+        spacing: True to copy the spacing from ``source`` to ``target``, or
+            the spacing to set in ``target``; defaults to True.
+        origin: True to copy the origin from ``source`` to ``target``, or
+            the origin to set in ``target``; defaults to True.
+        direction: True to copy the direction from ``source`` to ``target``, or
+            the direction to set in ``target``; defaults to True.
 
     """
-    spacing = source.GetSpacing()
-    origin = source.GetOrigin()
-    direction = source.GetDirection()
-    print("Adjusting spacing from {} to {}, origin from {} to {}, "
-          "direction from {} to {}"
-          .format(target.GetSpacing(), spacing, target.GetOrigin(), origin,
-                  target.GetDirection(), direction))
-    # target.SetSpacing(spacing)
-    target.SetOrigin(origin)
-    target.SetDirection(direction)
+    # get the world info from the source if not already set
+    if spacing is True:
+        spacing = source.GetSpacing()
+    if origin is True:
+        origin = source.GetOrigin()
+    if direction is True:
+        direction = source.GetDirection()
+    
+    # set the values in the target
+    _logger.debug(
+        "Adjusting spacing from %s to %s, origin from %s to %s, "
+        "direction from %s to %s", target.GetSpacing(), spacing,
+        target.GetOrigin(), origin, target.GetDirection(), direction)
+    if spacing:
+        target.SetSpacing(spacing)
+    if origin:
+        target.SetOrigin(origin)
+    if direction:
+        target.SetDirection(direction)
 
 
 def read_sitk(path, dryrun=False):
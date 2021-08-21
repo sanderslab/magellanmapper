sanderslab/magellanmapper

commit 8b553703342d35d1d8468e512291aa9805141f14

Support repeating registrations on multiple atlases
The "single" registration task has applied one registration run between an atlas and sample image, determining the registration based on intensity images and applying the same registration using the Elastix Transformix object to apply this same transformation to the atlas' labels image. Extend usage of Transformix to additional complete atlases, applying it to both the intensity and labels images. Use multiple prefix CLI parameters to specify the output name from each atlas.

magmap/atlas/register.py
@@ -42,6 +42,7 @@ from collections import OrderedDict
 import os
 import shutil
 from time import time
+from typing import Optional
 
 import pandas as pd
 import numpy as np
@@ -50,8 +51,7 @@ from skimage import filters, measure, morphology, transform
 
 from magmap.atlas import atlas_refiner, edge_seg, labels_meta, ontology, transformer
 from magmap.cv import cv_nd
-from magmap.io import cli, df_io, export_regions, importer, libmag, np_io, \
-    sitk_io
+from magmap.io import cli, df_io, export_regions, importer, libmag, sitk_io
 from magmap.plot import plot_2d, plot_3d
 from magmap.settings import config
 from magmap.stats import atlas_stats, clustering, vols
@@ -388,8 +388,12 @@ def register_duo(fixed_img, moving_img, path=None, fixed_mask=None,
     return transformed_img, transformix_img_filter
 
 
-def register(fixed_file, moving_img_path, show_imgs=True, write_imgs=True,
-             name_prefix=None, new_atlas=False):
+def register(
+        fixed_file: str, moving_img_path: str, show_imgs: bool = True,
+        write_imgs: bool = True, name_prefix: Optional[str] = None,
+        new_atlas: bool = False,
+        transformix: Optional[sitk.TransformixImageFilter] = None
+) -> sitk.TransformixImageFilter:
     """Register an atlas to a sample image using the SimpleElastix library.
     
     Loads the images, applies any transformations to the moving image, and
@@ -421,6 +425,11 @@ def register(fixed_file, moving_img_path, show_imgs=True, write_imgs=True,
             defaults to None, in which case the fixed_file path will be used.
         new_atlas: True to generate registered images that will serve as a 
             new atlas; defaults to False.
+        transformix: SimpleElastix transformation filter from prior run;
+            defaults to None.
+    Returns:
+        The SimpleElastix transformation filter generated by the registration.
+    
     """
     def get_similarity_metric():
         # get the name of the similarity metric from the first found name
@@ -525,45 +534,55 @@ def register(fixed_file, moving_img_path, show_imgs=True, write_imgs=True,
         moving_img_spacing = np.multiply(moving_img.GetSpacing(), rescale)
         for img in moving_imgs:
             img.SetSpacing(moving_img_spacing)
-    
-    # perform registration
-    try:
-        img_moved, transformix_filter = register_duo(
-            fixed_img, moving_img, name_prefix, fixed_mask, moving_mask)
-    except RuntimeError:
-        libmag.warn("Could not perform registration. Will retry with"
-                    "world info (spacing, origin, etc) set to that of the "
-                    "fixed image.")
-        # fall back to simply matching all world info
-        # TODO: consider matching world info by default since output is same
-        imgs = list(moving_imgs)
-        if fixed_mask is not None:
-            imgs.append(fixed_mask)
-        for img in imgs:
-            sitk_io.match_world_info(fixed_img, img)
-        img_moved, transformix_filter = register_duo(
-            fixed_img, moving_img, name_prefix, fixed_mask, moving_mask)
 
-    # overlap stats comparing original and registered samples (eg histology)
-    print("DSC of original and registered sample images")
     thresh_mov = settings["atlas_threshold"]
-    dsc_sample = atlas_refiner.measure_overlap(
-        fixed_img_orig, img_moved, thresh_img2=thresh_mov)
-    fallback = settings["metric_sim_fallback"]
-    metric_sim = get_similarity_metric()
-    if fallback and dsc_sample < fallback[0]:
-        # fall back to another atlas profile; update the current profile with
-        # this new profile and re-set-up the original profile afterward
-        print("Registration DSC below threshold of {}, will re-register "
-              "using {} atlas profile".format(*fallback))
-        atlas_prof_name_orig = settings[settings.NAME_KEY]
-        cli.setup_atlas_profiles(fallback[1], reset=False)
-        metric_sim = get_similarity_metric()
-        img_moved, transformix_filter = register_duo(
-            fixed_img, moving_img, name_prefix, fixed_mask, moving_mask)
-        cli.setup_atlas_profiles(atlas_prof_name_orig)
+    if transformix:
+        # re-transform using parameters from a prior registration
+        transformix_filter = transformix
+        transformix_filter.SetMovingImage(moving_img)
+        transformix_filter.Execute()
+        img_moved = transformix_filter.GetResultImage()
         dsc_sample = atlas_refiner.measure_overlap(
             fixed_img_orig, img_moved, thresh_img2=thresh_mov)
+        metric_sim = get_similarity_metric()
+    else:
+        # perform a new registration
+        try:
+            img_moved, transformix_filter = register_duo(
+                fixed_img, moving_img, name_prefix, fixed_mask, moving_mask)
+        except RuntimeError:
+            libmag.warn("Could not perform registration. Will retry with"
+                        "world info (spacing, origin, etc) set to that of the "
+                        "fixed image.")
+            # fall back to simply matching all world info
+            # TODO: consider matching world info by default since output is same
+            imgs = list(moving_imgs)
+            if fixed_mask is not None:
+                imgs.append(fixed_mask)
+            for img in imgs:
+                sitk_io.match_world_info(fixed_img, img)
+            img_moved, transformix_filter = register_duo(
+                fixed_img, moving_img, name_prefix, fixed_mask, moving_mask)
+    
+        # overlap stats comparing original and registered samples (eg histology)
+        print("DSC of original and registered sample images")
+        dsc_sample = atlas_refiner.measure_overlap(
+            fixed_img_orig, img_moved, thresh_img2=thresh_mov)
+        fallback = settings["metric_sim_fallback"]
+        metric_sim = get_similarity_metric()
+        if fallback and dsc_sample < fallback[0]:
+            # fall back to another atlas profile; update the current profile with
+            # this new profile and re-set-up the original profile afterward
+            print("Registration DSC below threshold of {}, will re-register "
+                  "using {} atlas profile".format(*fallback))
+            atlas_prof_name_orig = settings[settings.NAME_KEY]
+            cli.setup_atlas_profiles(fallback[1], reset=False)
+            metric_sim = get_similarity_metric()
+            img_moved, transformix_filter = register_duo(
+                fixed_img, moving_img, name_prefix, fixed_mask, moving_mask)
+            cli.setup_atlas_profiles(atlas_prof_name_orig)
+            dsc_sample = atlas_refiner.measure_overlap(
+                fixed_img_orig, img_moved, thresh_img2=thresh_mov)
     
     def make_labels(truncation=None):
         # apply the same transformation to labels via Transformix, with option
@@ -689,6 +708,8 @@ def register(fixed_file, moving_img_path, show_imgs=True, write_imgs=True,
     '''
     print("time elapsed for single registration (s): {}"
           .format(time() - start_time))
+    
+    return transformix_filter
 
 
 def register_rev(fixed_path, moving_path, reg_base=None, reg_names=None,
@@ -1798,13 +1819,28 @@ def main():
     
     elif reg in (
             config.RegisterTypes.SINGLE, config.RegisterTypes.NEW_ATLAS):
-        # "single", basic registration of 1st to 2nd image, transposing the 
-        # second image according to config.plane and config.flip_horiz; 
-        # "new_atlas" registers similarly but outputs new atlas files
+        # "single" (rather than groupwise) registration, or "new_atlas" to
+        # register and output files as a new atlas
         new_atlas = reg is config.RegisterTypes.NEW_ATLAS
-        register(
+        transformix = register(
             *config.filenames[:2], name_prefix=config.prefix,
             new_atlas=new_atlas, show_imgs=config.show)
+        
+        if len(config.filenames) > 2:
+            # apply the same transformation to additional atlas(es)
+            prefix = config.prefix if config.prefix else config.filenames[0]
+            for i, path in enumerate(config.filenames[2:]):
+                # use additional config.prefix for output names if available
+                name_prefix = libmag.get_if_within(config.prefixes, i + 1)
+                if not name_prefix:
+                    # fall back to combine first prefix or image path and atlas
+                    name_prefix = (
+                        f"{prefix}_"
+                        f"{libmag.splitext(os.path.basename(path))[0]}")
+                register(
+                    config.filenames[0], path, name_prefix=name_prefix,
+                    new_atlas=new_atlas, show_imgs=config.show,
+                    transformix=transformix)
     
     elif reg is config.RegisterTypes.GROUP:
         # groupwise registration, which assumes that the last image 

commit a3a6fa4f1290137cb57682eebc2d991207b1ecdf

Fix accessing SimpleElastix class during import
We have attempted to allow SimpleITK to be used as a substitute for SimpleElastix, but commit 8b553703342d35d1d8468e512291aa9805141f14 introduced a regression where a SimpleElastix class is referenced directly in a function definition for type hints, leading to error when importing the module if SimpleElastix is not present. Change this reference to a quoted type hint to allow the class to be skipped if SimpleITK rather than SimpleElastix is installed.

magmap/atlas/register.py
@@ -395,8 +395,8 @@ def register(
         fixed_file: str, moving_img_path: str, show_imgs: bool = True,
         write_imgs: bool = True, name_prefix: Optional[str] = None,
         new_atlas: bool = False,
-        transformix: Optional[sitk.TransformixImageFilter] = None
-) -> sitk.TransformixImageFilter:
+        transformix: Optional["sitk.TransformixImageFilter"] = None
+) -> "sitk.TransformixImageFilter":
     """Register an atlas to a sample image using the SimpleElastix library.
     
     Loads the images, applies any transformations to the moving image, and
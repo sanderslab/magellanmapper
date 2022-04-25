sanderslab/magellanmapper

commit f1b6579f6275280949bdbe6f4beb699b27deea26

Scale blob matches displayed in the ROI Editor
Blobs loaded from the NPZ archive are scaled when loading a main image that occupies a different space, but blob matches and their associated blobs in the database are not scaled since the access is slower. Scale these blob matches when displaying them in the GUI.

magmap/gui/visualizer.py
@@ -2141,25 +2141,35 @@ class Visualization(HasTraits):
             segs_all = detector.format_blobs(segs_all)
             segs_all, mask_chl = detector.blobs_in_channel(
                 segs_all, chls, return_mask=True)
+            
             if ColocalizeOptions.MATCHES.value in self._colocalize:
                 # get blob matches from whole-image match colocalization,
-                # shifting blobs to relative coordinates
-                matches = colocalizer.select_matches(
-                    config.db, chls, offset[::-1], roi_size[::-1])
+                # scaling the ROI to original blob space since blob matches
+                # are not scaled during loading
+                roi = [np.divide(a, config.blobs.scaling[:3])
+                       for a in (offset[::-1], roi_size[::-1])]
+                matches = colocalizer.select_matches(config.db, chls, *roi)
+                
                 # TODO: include all channel combos
                 if matches is not None and len(matches) > 0:
+                    # shift blobs to relative coordinates
                     matches = matches[tuple(matches.keys())[0]]
-                    shift = [n * -1 for n in offset[::-1]]
+                    shift = [n * -1 for n in roi[0]]
                     matches.update_blobs(detector.shift_blob_rel_coords, shift)
+                    matches.update_blobs(
+                        detector.multiply_blob_rel_coords,
+                        config.blobs.scaling[:3])
                     self.blobs.blob_matches = matches
-                print("loaded blob matches:\n", self.blobs.blob_matches)
+                _logger.debug(
+                    f"Loaded blob matches:\n{self.blobs.blob_matches}")
+            
             elif (ColocalizeOptions.INTENSITY.value in self._colocalize
                   and config.blobs.colocalizations is not None
                   and segs is None):
                 # get corresponding blob co-localizations unless showing
                 # blobs from database, which do not have colocs
                 colocs = config.blobs.colocalizations[mask][mask_chl]
-        print("segs_all:\n{}".format(segs_all))
+        _logger.debug(f"All blobs:\n{segs_all}\nTotal blobs: {len(segs_all)}")
         
         if segs is not None:
             # segs are typically loaded from DB for a sub-ROI within the

commit 06d1186c7981c745dd484e9defbf738e90f0bfbc

Fix log error when no blobs are detected
Check for existing detected blobs in the GUI when logging the total number of blobs to fix regression introduced in commit f1b6579f6275280949bdbe6f4beb699b27deea26. Also, add note that filling blobs in the padding region may be removed.

magmap/gui/visualizer.py
@@ -2185,11 +2185,14 @@ class Visualization(HasTraits):
                 # get corresponding blob co-localizations unless showing
                 # blobs from database, which do not have colocs
                 colocs = config.blobs.colocalizations[mask][mask_chl]
-        _logger.debug(f"All blobs:\n{segs_all}\nTotal blobs: {len(segs_all)}")
+        _logger.debug(
+            f"All blobs:\n{segs_all}\nTotal blobs: "
+            f"{0 if segs_all is None else len(segs_all)}")
         
         if segs is not None:
             # segs are typically loaded from DB for a sub-ROI within the
             # current ROI, so fill in the padding area from segs_all
+            # TODO: remove since blobs from different sources may be confusing?
             _, segs_in_mask = detector.get_blobs_in_roi(
                 segs_all, np.zeros(3), 
                 roi_size, np.multiply(self.border, -1))
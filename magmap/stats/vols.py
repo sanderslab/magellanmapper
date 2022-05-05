Fix column weighting error when no regions are present
Mapping measurements to labels applies a weighting column, but these weightings are missing if no regions are found. Check for existing regions in the data frame before attempting to process its weighting column.

magmap/stats/vols.py
@@ -1227,7 +1227,7 @@ def map_meas_to_labels(labels_img, df, meas, fn_avg, skip_nans=False,
         df_cond = df["Condition"]
         conds = sorted(np.unique(df_cond), reverse=reverse)
 
-    if col_wt is not None:
+    if col_wt is not None and len(df) > 0:
         # weight given column for the first condition and normalizing it to
         # its maximum value, or use the whole column if no conditions exist
         print("weighting stats by", col_wt)
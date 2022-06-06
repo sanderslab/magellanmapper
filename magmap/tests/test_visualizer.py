c136c400960780f367401eb5fdd8cb599eccfba0
David Young <david@textflex.com>
2022-06-06 16:18:35 +0800

    Refactor getting changed options with static function
    Add a static method to get and store dictionaries of combo box options state and changed status.

--- magmap/tests/test_visualizer.py
+++ magmap/tests/test_visualizer.py
@@ -32,6 +32,25 @@ class TestVisualizer(unittest.TestCase):
         pref = visualizer.Visualization.validate_pref(
             plane, default, visualizer.Visualization._DEFAULTS_PLANES_2D)
         self.assertEqual(pref, default)
+    
+    def test_get_changed_options(self):
+        curr_options = [visualizer.RegionOptions.BOTH_SIDES.value]
+        prev_options = {e: False for e in visualizer.RegionOptions}
+        options, changed = visualizer.Visualization.get_changed_options(
+            curr_options, prev_options, visualizer.RegionOptions)
+        self.assertTrue(options[visualizer.RegionOptions.BOTH_SIDES])
+        self.assertFalse(options[visualizer.RegionOptions.APPEND])
+        self.assertFalse(changed[visualizer.RegionOptions.APPEND])
+        self.assertTrue(changed[visualizer.RegionOptions.BOTH_SIDES])
+        
+        curr_options = [visualizer.RegionOptions.APPEND.value]
+        options, changed = visualizer.Visualization.get_changed_options(
+            curr_options, options, visualizer.RegionOptions)
+        self.assertFalse(options[visualizer.RegionOptions.BOTH_SIDES])
+        self.assertTrue(options[visualizer.RegionOptions.APPEND])
+        self.assertTrue(changed[visualizer.RegionOptions.APPEND])
+        self.assertTrue(changed[visualizer.RegionOptions.BOTH_SIDES])
+        self.assertFalse(changed[visualizer.RegionOptions.INCL_CHILDREN])
 
 
 if __name__ == "__main__":

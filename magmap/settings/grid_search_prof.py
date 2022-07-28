sanderslab/magellanmapper

commit acc1cbf7d9640373bce65fa1603827fbd8c1f949

Simplify grid searches to parse single runs
Return and parse a single run instead of multiple grid search runs for simplicity.

magmap/stats/mlearn.py
@@ -3,11 +3,14 @@
 """Machine learning and output for MagellanMapper.
 """
 
+# import annotations to allow sub-type hints
+from __future__ import annotations
 from collections import OrderedDict
 from enum import Enum
-from typing import Any, Callable, Sequence, Tuple
+from typing import Any, Callable, Dict, Sequence, Tuple
 
 import numpy as np
+import pandas as pd
 
 from magmap.settings import config
 from magmap.io import libmag
@@ -28,8 +31,10 @@ class GridSearchStats(Enum):
 
 
 def grid_search(
-        hyperparams: OrderedDict, fnc: Callable[[Any], Tuple[Any, Sequence]],
-        *fnc_args) -> OrderedDict:
+        hyperparams: OrderedDict[str, Sequence[float]],
+        fnc: Callable[[Any], Tuple[Any, Sequence]],
+        *fnc_args
+) -> OrderedDict[str, Tuple[Sequence, Sequence, str, OrderedDict]]:
     """Perform a grid search for hyperparameter optimization.
 
     A separate grid search will be performed for each item in ``roc_dict``.
@@ -49,7 +54,6 @@ def grid_search(
     """
     # gets the ROC settings
     settings = config.roi_profile
-    stats_dict = OrderedDict()
     file_summaries = []
     iterable_keys = []  # hyperparameters to iterate through
     iterable_dict = OrderedDict()  # group results
@@ -98,78 +102,86 @@ def grid_search(
                 stats, last_param_vals, key, parent_params)
     
     grid_iterate(0, hyperparams, None, OrderedDict())
-    stats_dict["grid"] = iterable_dict
     
     # summary of each file collected together
     for summary in file_summaries:
         print(summary)
-    return stats_dict
+    return iterable_dict
 
 
-def parse_grid_stats(stats_dict):
-    """Parse stats from multiple grid searches.
+def parse_grid_stats(
+        stats: OrderedDict[str, Tuple[Sequence, Sequence, str, OrderedDict]]
+) -> Tuple[Dict[str, Tuple[Sequence, Sequence, Sequence]], pd.DataFrame]:
+    """Parse stats from a grid search.
     
     Args:
-        stats_dict: Dictionary where key is a string with the parameters
+        stats: Dictionary where key is a string with the parameters
             up to the last parameter group, and each value is a tuple of 
             the raw stats as (pos, true_pos, false_pos); the array of
             values for the last parameter; the last parameter key; and an 
             ``OrderedDict`` of the parent parameters and their values for 
             the given set of stats.
+    
+    Returns:
+        Tuple of ``group_stats`` and ``df``:
+        - ``group_stats`` is a dictionary of stats, where keys
+          correspond go ``stats`` keys, and values are tuples of the
+          false discovery rate, sensitivity, and last parameter group value,
+          each as sequences
+        - ``df`` is a data frame summarizing the stats
+    
     """
-    parsed_stats = {}
-    dfs = []
+    
+    # parse a grid search
+    stats_for_df = {}
+    headers = None
+    group_dict = {}
     param_keys = []
-    for group, iterable_dicts in stats_dict.items():
-        # parse a grid search
-        stats_for_df = {}
-        headers = None
-        print("{}:".format(group))
-        group_dict = {}
-        parsed_stats[group] = group_dict
-        for key, value in iterable_dicts.items():
-            # parse stats from a set of parameters
-            grid_stats = np.array(value[0])  # raw stats
-            # last parameter is given separately since it is actively varying
-            last_param_vals, last_param_key, parent_params = value[1:]
-            if not headers:
-                # set up headers for each stat and insert parameter headers
-                # at the start
-                headers = [
-                    GridSearchStats.PARAM.value,
-                    GridSearchStats.PPV,
-                    GridSearchStats.SENS,
-                    GridSearchStats.POS,
-                    GridSearchStats.TP,
-                    GridSearchStats.FP,
-                    GridSearchStats.FDR,
-                ]
-                headers[0] = "_".join((headers[0], last_param_key))
-                for i, parent in enumerate(parent_params.keys()):
-                    headers.insert(
-                        i, "_".join((GridSearchStats.PARAM.value, parent)))
-                    param_keys.append(parent)
-                param_keys.append(last_param_key)
-            # false discovery rate, inverse of PPV, since don't have true negs
-            fdr = np.subtract(
-                1, np.divide(grid_stats[:, 1], 
-                             np.add(grid_stats[:, 1], grid_stats[:, 2])))
-            sens = np.divide(grid_stats[:, 1], grid_stats[:, 0])
-            for i, n in enumerate(last_param_vals):
-                stat_list = []
-                for parent_val in parent_params.values():
-                    stat_list.append(parent_val)
-                stat_list.extend(
-                    (last_param_vals[i], 1 - fdr[i], sens[i], 
-                     *grid_stats[i].astype(int), fdr[i]))
-                for header, stat in zip(headers, stat_list):
-                    stats_for_df.setdefault(header, []).append(stat)
-            group_dict[key] = (fdr, sens, last_param_vals)
-        print()
-        path_df = libmag.make_out_path(
-            "gridsearch_{}.csv".format("_".join(param_keys)))
-        dfs.append(df_io.dict_to_data_frame(stats_for_df, path_df, show=" "))
-    return parsed_stats, dfs
+    for key, value in stats.items():
+        # parse stats from a set of parameters
+        grid_stats = np.array(value[0])  # raw stats
+        # last parameter is given separately since it is actively varying
+        last_param_vals, last_param_key, parent_params = value[1:]
+        if not headers:
+            # set up headers for each stat and insert parameter headers
+            # at the start
+            headers = [
+                GridSearchStats.PARAM.value,
+                GridSearchStats.PPV,
+                GridSearchStats.SENS,
+                GridSearchStats.POS,
+                GridSearchStats.TP,
+                GridSearchStats.FP,
+                GridSearchStats.FDR,
+            ]
+            headers[0] = "_".join((headers[0], last_param_key))
+            for i, parent in enumerate(parent_params.keys()):
+                headers.insert(
+                    i, "_".join((GridSearchStats.PARAM.value, parent)))
+                param_keys.append(parent)
+            param_keys.append(last_param_key)
+        # false discovery rate, inverse of PPV, since don't have true negs
+        fdr = np.subtract(
+            1, np.divide(grid_stats[:, 1], 
+                         np.add(grid_stats[:, 1], grid_stats[:, 2])))
+        sens = np.divide(grid_stats[:, 1], grid_stats[:, 0])
+        for i, n in enumerate(last_param_vals):
+            stat_list = []
+            for parent_val in parent_params.values():
+                stat_list.append(parent_val)
+            stat_list.extend(
+                (last_param_vals[i], 1 - fdr[i], sens[i], 
+                 *grid_stats[i].astype(int), fdr[i]))
+            for header, stat in zip(headers, stat_list):
+                stats_for_df.setdefault(header, []).append(stat)
+        group_dict[key] = (fdr, sens, last_param_vals)
+    print()
+    
+    # generate a data frame to summarize stats and save to file
+    path_df = libmag.make_out_path(
+        "gridsearch_{}.csv".format("_".join(param_keys)))
+    df = df_io.dict_to_data_frame(stats_for_df, path_df, show=" ")
+    return group_dict, df
 
     
 if __name__ == "__main__":

magmap/io/cli.py
@@ -1089,10 +1089,10 @@ def _grid_search(series_list: List[int]):
             config.grid_search_profile.hyperparams, _detect_subimgs,
             config.filename, series, config.subimg_offsets,
             config.subimg_sizes)
-        parsed_dict, stats_dfs = mlearn.parse_grid_stats(stats_dict)
-        for stats_df in stats_dfs:
-            # plot ROC curve
-            plot_2d.plot_roc(stats_df, config.show)
+        parsed_dict, stats_df = mlearn.parse_grid_stats(stats_dict)
+        
+        # plot ROC curve
+        plot_2d.plot_roc(stats_df, config.show)
 
 
 def process_file(

commit 1580d773669b6034e9c4d193e1fb2fbfcd772fb9

Fix error importing `annotations` in Python 3.6
Commit acc1cbf7d9640373bce65fa1603827fbd8c1f949 introduced a regression since Python 3.6 cannot import `annotations`. This `__future__` feature was only added in Python 3.7. As a workaround, remove these imports and replace the type hints with string literals.

magmap/settings/grid_search_prof.py
@@ -2,8 +2,6 @@
 # Author: David Young, 2019, 2020
 """Profile settings for grid search hyperparameter tuning."""
 
-# import annotations to allow sub-type hints
-from __future__ import annotations
 from collections import OrderedDict
 import dataclasses
 from typing import Dict, List, Sequence
@@ -65,7 +63,7 @@ class GridSearchProfile(profiles.SettingsDict):
     
     #: Ordered dictionary of hyperparameters, which should consist of key-pairs
     #: in the format: ``<ROIProfile-key>: <seq-of-param-vals>``.
-    hyperparams: OrderedDict[str, Sequence[float]] = dataclasses.field(
+    hyperparams: "OrderedDict[str, Sequence[float]]" = dataclasses.field(
         default_factory=OrderedDict)
 
     def __init__(self, *args, **kwargs):

magmap/stats/mlearn.py
@@ -3,8 +3,6 @@
 """Machine learning and output for MagellanMapper.
 """
 
-# import annotations to allow sub-type hints
-from __future__ import annotations
 from collections import OrderedDict
 from enum import Enum
 from typing import Any, Callable, Dict, Sequence, Tuple
@@ -31,10 +29,10 @@ class GridSearchStats(Enum):
 
 
 def grid_search(
-        hyperparams: OrderedDict[str, Sequence[float]],
+        hyperparams: "OrderedDict[str, Sequence[float]]",
         fnc: Callable[[Any], Tuple[Any, Sequence]],
         *fnc_args
-) -> OrderedDict[str, Tuple[Sequence, Sequence, str, OrderedDict]]:
+) -> "OrderedDict[str, Tuple[Sequence, Sequence, str, OrderedDict]]":
     """Perform a grid search for hyperparameter optimization.
 
     A separate grid search will be performed for each item in ``roc_dict``.
@@ -110,7 +108,7 @@ def grid_search(
 
 
 def parse_grid_stats(
-        stats: OrderedDict[str, Tuple[Sequence, Sequence, str, OrderedDict]]
+        stats: "OrderedDict[str, Tuple[Sequence, Sequence, str, OrderedDict]]"
 ) -> Tuple[Dict[str, Tuple[Sequence, Sequence, Sequence]], pd.DataFrame]:
     """Parse stats from a grid search.
     
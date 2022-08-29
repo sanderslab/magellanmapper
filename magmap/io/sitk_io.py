sanderslab/magellanmapper

commit c06ee796334d5654540f9ee6bacb791f6adffdff

Load 2D registered images as 3D in image setup
Many operations such as the GUI assume that any registered images are in 3D. Convert any 2D images loaded by SimpleITK to 3D when setting up an image along with its registered images. This conversion is not default to allow other operations to handle 2D images.

magmap/io/sitk_io.py
@@ -6,7 +6,7 @@ Manage import and export of :class:`simpleitk.Image` objects.
 """
 import os
 import shutil
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Optional, Sequence, Tuple, Union
 
 import numpy as np
 import SimpleITK as sitk
@@ -175,7 +175,32 @@ def _load_reg_img_to_combine(path, reg_name, img_nps):
     return img_sitk, loaded_path
 
 
-def read_sitk_files(filename_sitk, reg_names=None, return_sitk=False):
+def _make_3d(img_sitk: sitk.Image, spacing_z=1) -> sitk.Image:
+    """Make a 2D image into a 3D image with a single plane.
+    
+    Args:
+        img_sitk: Image in SimpleITK format.
+        spacing_z: Z-axis spacing; defaults to 1.
+
+    Returns:
+        ``img_sitk`` converted to a 3D image if previously 2D.
+
+    """
+    spacing = img_sitk.GetSpacing()
+    if len(spacing) == 2:
+        # prepend an additional axis for 2D images to make them 3D
+        img_np = sitk.GetArrayFromImage(img_sitk)[None]
+        spacing = list(spacing) + [spacing_z]
+        img_sitk = sitk.GetImageFromArray(img_np)
+        img_sitk.SetSpacing(spacing)
+    return img_sitk
+
+
+def read_sitk_files(
+        filename_sitk: str,
+        reg_names: Optional[Union[str, Sequence[str]]] = None,
+        return_sitk: bool = False, make_3d: bool = False
+) -> Union[np.ndarray, Tuple[np.ndarray, sitk.Image]]:
     """Read image files through SimpleITK.
     
     Supports identifying files based on registered suffixes and combining
@@ -185,18 +210,19 @@ def read_sitk_files(filename_sitk, reg_names=None, return_sitk=False):
     :attr:`magmap.settings.config.resolutions` if not already set.
 
     Args:
-        filename_sitk (str): Path to file in a format that can be read by
+        filename_sitk: Path to file in a format that can be read by
             SimpleITK.
-        reg_names (Union[str, List[str]]): Path or sequence of paths of
+        reg_names: Path or sequence of paths of
             registered names. Can be a registered suffix or a full path.
             Defaults to None to open ``filename_sitk`` as-is through
             :meth:`read_sitk`.
-        return_sitk (bool): True to return the loaded SimpleITK Image object.
+        return_sitk: True to return the loaded SimpleITK Image object.
+        make_3d: True to convert 2D images to 3D; defaults to False.
 
     Returns:
-        :class:`numpy.ndarray`: Image array in Numpy 3D format (or 4D if
-        multi-channel). Associated metadata is loaded into :module:`config`
-        attributes.
+        ``img_np``: Image array in Numpy 3D format (or 4D if
+        multi-channel). Associated metadata is loaded into
+        :module:`magmap.settings.config` attributes.
         
         If ``return_sitk`` is True, also returns the first loaded image
         in SimpleITK format.
@@ -232,6 +258,11 @@ def read_sitk_files(filename_sitk, reg_names=None, return_sitk=False):
             raise FileNotFoundError(
                 "could not find file {}".format(filename_sitk))
         img_sitk, _ = read_sitk(filename_sitk)
+        img_np = sitk.GetArrayFromImage(img_sitk)
+    
+    if make_3d:
+        # convert 2D images to 3D
+        img_sitk = _make_3d(img_sitk)
         img_np = sitk.GetArrayFromImage(img_sitk)
     
     if config.resolutions is None:

magmap/io/np_io.py
@@ -320,8 +320,10 @@ def setup_images(
             path_lower = path.lower()
             import_only = proc_type is config.ProcessTypes.IMPORT_ONLY
             if path_lower.endswith(sitk_io.EXTS_3D):
-                # attempt to format supported by SimpleITK and prepend time axis
-                config.image5d = sitk_io.read_sitk_files(path)[None]
+                # load format supported by SimpleITK and prepend time axis;
+                # if 2D, convert to 3D
+                config.image5d = sitk_io.read_sitk_files(
+                    path, make_3d=True)[None]
                 config.img5d.img = config.image5d
                 config.img5d.path_img = path
                 config.img5d.img_io = config.LoadIO.SITK
@@ -391,7 +393,7 @@ def setup_images(
         try:
             # will take the place of any previously loaded image5d
             config.image5d = sitk_io.read_sitk_files(
-                path, reg_names=atlas_suffix)[None]
+                path, atlas_suffix, make_3d=True)[None]
             config.img5d.img = config.image5d
             config.img5d.img_io = config.LoadIO.SITK
         except FileNotFoundError as e:
@@ -406,7 +408,7 @@ def setup_images(
             # load labels image
             # TODO: need to support multichannel labels images
             config.labels_img, config.labels_img_sitk = sitk_io.read_sitk_files(
-                path, reg_names=annotation_suffix, return_sitk=True)
+                path, annotation_suffix, True, True)
         except FileNotFoundError as e:
             print(e)
             if config.image5d is not None:
@@ -448,7 +450,7 @@ def setup_images(
         # load borders image, which can also be another labels image
         try:
             config.borders_img = sitk_io.read_sitk_files(
-                path, reg_names=borders_suffix)
+                path, borders_suffix, make_3d=True)
         except FileNotFoundError as e:
             print(e)
     

commit 51d5efa865813b34028f8bc5dfb87f1c5219bef6

Fix loading multiple registered intensity images
Commit c06ee796334d5654540f9ee6bacb791f6adffdff introduced a regression where the SimpleITK image is not updated with the multi-channel NumPy array generated from multiple registered intensity images, fixed here.

magmap/io/sitk_io.py
@@ -172,7 +172,7 @@ def read_sitk(path, dryrun=False):
 
 
 def _load_reg_img_to_combine(path, reg_name, img_nps):
-    # load registered image in sitk format to combine with other images 
+    # load registered image in sitk format to combine with other images
     # by resizing to the shape of the first image
     img_np_base = None
     if img_nps:
@@ -260,18 +260,24 @@ def read_sitk_files(
         if not libmag.is_seq(reg_names):
             reg_names = [reg_names]
         for reg_name in reg_names:
-            # load each registered suffix into list of images with same shape, 
+            # load each registered suffix into list of images with same shape,
             # keeping first image in sitk format
             img, path = _load_reg_img_to_combine(
                 filename_sitk, reg_name, img_nps)
             if img_sitk is None:
                 img_sitk = img
                 loaded_path = path
+        
         if len(img_nps) > 1:
             # merge images into separate channels
             img_np = np.stack(img_nps, axis=img_nps[0].ndim)
         else:
             img_np = img_nps[0]
+        
+        if img_sitk:
+            # update sitk image with np array
+            img_sitk = replace_sitk_with_numpy(img_sitk, img_np)
+    
     else:
         # load filename_sitk directly
         if not os.path.exists(filename_sitk):
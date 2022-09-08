725a5a6c8f5e817fe5883bf3027eeb092d017d14
David Young <david@textflex.com>
2022-09-08 15:54:23 +0800

    Simplify rescaling and resizing to find the correct function based of argument
    The wrapper function for rescaling and resizing has accepted separate arguments for each scenario, analogous to the underlying Scikit-image functions. As a simplification in this wrapper, merge the `rescale` into the `target_size` parameter by determining the function to call based on the size of the `target_size` parameter. Also, remove the anti-aliasing argument since it will default to True when downsampling in both scenarios.

--- magmap/tests/test_cv_nd.py
+++ magmap/tests/test_cv_nd.py
@@ -12,9 +12,18 @@ from magmap.cv import cv_nd
 class TestCvNd(unittest.TestCase):
     
     def test_rescale_resize(self):
+        # set up test array
         img = np.zeros((2, 50, 200, 251, 3))
+        
+        # test rescaling array
         img_rescaled = cv_nd.rescale_resize(img[0], 0.5, multichannel=True)
         testing.assert_array_equal(img_rescaled.shape, (25, 100, 126, 3))
+        
+        # test resizing array
+        target_size = [22, 150, 300]
+        img_rescaled = cv_nd.rescale_resize(img[0], target_size, multichannel=True)
+        target_size.append(3)
+        testing.assert_array_equal(img_rescaled.shape, target_size)
 
 
 if __name__ == "__main__":

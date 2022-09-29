sanderslab/magellanmapper

commit bbf6b0803fd3066f049346191b663713bbbad05a

Fix loading multiple registered intensity images
Commit c06ee796334d5654540f9ee6bacb791f6adffdff introduced a regression where the SimpleITK image is not updated with the multi-channel NumPy array generated from multiple registered intensity images, fixed here.

magmap/io/sitk_io.py
@@ -172,7 +172,7 @@ def read_sitk(path, dryrun=False):
 
 
 def _load_reg_img_to_combine(path, reg_name, img_nps):
-    # load registered image in sitk format to combine with other images 
+    # load registered image in sitk format to combine with other images
     # by resizing to the shape of the first image
     img_np_base = None
     if img_nps:
@@ -260,18 +260,24 @@ def read_sitk_files(
         if not libmag.is_seq(reg_names):
             reg_names = [reg_names]
         for reg_name in reg_names:
-            # load each registered suffix into list of images with same shape, 
+            # load each registered suffix into list of images with same shape,
             # keeping first image in sitk format
             img, path = _load_reg_img_to_combine(
                 filename_sitk, reg_name, img_nps)
             if img_sitk is None:
                 img_sitk = img
                 loaded_path = path
+        
         if len(img_nps) > 1:
             # merge images into separate channels
             img_np = np.stack(img_nps, axis=img_nps[0].ndim)
         else:
             img_np = img_nps[0]
+        
+        if img_sitk:
+            # update sitk image with np array
+            img_sitk = replace_sitk_with_numpy(img_sitk, img_np)
+    
     else:
         # load filename_sitk directly
         if not os.path.exists(filename_sitk):

commit e23970587f4b93d5f2859af1a35ded44f183ea62

Retain multichannel status when replacing the array in a SimpleITK image
Commit bbf6b0803fd3066f049346191b663713bbbad05a introduced a regression where multichannel SimpleITK images would be treated as single-channel images when updating them with a NumPy array. Keep the multichannel status based on the given SimpleITK image.

magmap/io/sitk_io.py
@@ -64,7 +64,8 @@ def reg_out_path(file_path, reg_name, match_ext=False):
         return file_path_base + "_" + reg_name
 
 
-def replace_sitk_with_numpy(img_sitk, img_np):
+def replace_sitk_with_numpy(
+        img_sitk: sitk.Image, img_np: np.ndarray) -> sitk.Image:
     """Generate a :class:``sitk.Image`` object based on another Image object,
     but replace its array with a new Numpy array.
     
@@ -75,11 +76,18 @@ def replace_sitk_with_numpy(img_sitk, img_np):
     Returns:
         New :class:``sitk.Image`` object with same spacing, origin, and 
         direction as that of ``img_sitk`` and array replaced with ``img_np``.
+    
     """
+    # get original settings
     spacing = img_sitk.GetSpacing()
     origin = img_sitk.GetOrigin()
     direction = img_sitk.GetDirection()
-    img_sitk_back = sitk.GetImageFromArray(img_np)
+    
+    # treat as vector (multichannel) image if source is multichannel
+    img_sitk_back = sitk.GetImageFromArray(
+        img_np, True if img_sitk.GetNumberOfComponentsPerPixel() > 1 else None)
+    
+    # transfer original settings to new sitk Image
     img_sitk_back.SetSpacing(spacing)
     img_sitk_back.SetOrigin(origin)
     img_sitk_back.SetDirection(direction)
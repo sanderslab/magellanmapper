sanderslab/magellanmapper

commit 5c32b8449d6b8a5e3f1fa0ec2a2c8d22e8d8c291

Store input alpha values in case they differ from that in the currently displayed image
The values input for image display in the `PlotEditor` have not included the alpha values since it was extracted directly from the image instance. In preparation for displaying alternative alpha values, store the input alpha value as well.

magmap/gui/plot_editor.py
@@ -24,8 +24,11 @@ if TYPE_CHECKING:
 
 
 class PlotAxImg:
-    """Axes image storage class to contain additional information for
-    display such as brightness and contrast.
+    """Axes image storage class.
+    
+    Tracks settings that may differ between the currently displayed image and
+    input values, such as ``None`` to specify auto-intensity and values for
+    brightness and contrast that are not stored in the image itself.
 
     Attributes:
         ax_img: Displayed Matplotlib image.
@@ -35,6 +38,7 @@ class PlotAxImg:
             ``ax_img.norm.vmax`` for the output vmax.
         brightness: Brightness addend; defaults to 0.0.
         contrast: Contrast factor; defaults to 1.0.
+        alpha: Opacity level; defaults to None.
         img: The original underlying image data,
             copied to allow adjusting the array in ``ax_img`` while
             retaining the original data.
@@ -43,12 +47,18 @@ class PlotAxImg:
     def __init__(
             self, ax_img: "image.AxesImage", vmin: Optional[float] = None,
             vmax: Optional[float] = None):
+        
+        # set from arguments
         self.ax_img = ax_img
         self.vmin = vmin
         self.vmax = vmax
         
+        # additional image attributes
         self.brightness: float = 0.0
         self.contrast: float = 1.0
+        self.alpha: Optional[float] = None
+        
+        # original underlying image data
         self.img: np.ndarray = np.copy(self.ax_img.get_array())
 
 
@@ -397,7 +407,7 @@ class PlotEditor:
                         for p in self._plot_ax_imgs[0]]
             
             # use opacity, brightness, anc contrast from prior images
-            alphas[0] = [p.ax_img.get_alpha() for p in self._plot_ax_imgs[0]]
+            alphas[0] = [p.alpha for p in self._plot_ax_imgs[0]]
             brightnesses[0] = [p.brightness for p in self._plot_ax_imgs[0]]
             contrasts[0] = [p.contrast for p in self._plot_ax_imgs[0]]
         
@@ -428,7 +438,7 @@ class PlotEditor:
                 
                 # get alpha for last corresponding borders plane if available
                 ax_img = libmag.get_if_within(self._plot_ax_imgs, 2 + i, None)
-                alpha = (ax_img[i].ax_img.get_alpha() if ax_img else
+                alpha = (ax_img[i].alpha if ax_img else
                          libmag.get_if_within(config.alphas, 2 + i, 1))
                 alphas.append(alpha)
                 

magmap/gui/visualizer.py
@@ -1027,7 +1027,7 @@ class Visualization(HasTraits):
         # populate controls with intensity settings
         self._imgadj_brightness = plot_ax_img.brightness
         self._imgadj_contrast = plot_ax_img.contrast
-        self._imgadj_alpha = plot_ax_img.ax_img.get_alpha()
+        self._imgadj_alpha = plot_ax_img.alpha
 
         # populate intensity limits, auto-scaling, and current val (if not auto)
         self._adapt_imgadj_limits(plot_ax_img)

commit d16e64fb9199a3e3ac949a6733fab931e6739e2c

Fix to restore image opacity when scrolling through planes
Commit 5c32b8449d6b8a5e3f1fa0ec2a2c8d22e8d8c291 introduced a regression where Plot Editor images' alpha were no longer accessed directly from the image but from a stored setting to allow reaccessing this value after turning off alpha blending, but the alpha value was not stored when changed. Fix by storing this value during image display adjustments.

magmap/gui/plot_editor.py
@@ -830,8 +830,9 @@ class PlotEditor:
             PlotEditor.change_brightness_contrast(
                 plot_ax_img, brightness, contrast)
         if alpha is not None:
-            # adjust opacity
+            # adjust and store opacity
             plot_ax_img.ax_img.set_alpha(alpha)
+            plot_ax_img.alpha = alpha
         return plot_ax_img
 
     def update_alpha_blend(
@@ -867,7 +868,7 @@ class PlotEditor:
             maximum: float = np.nan, brightness: Optional[float] = None,
             contrast: Optional[float] = None, alpha: Optional[float] = None,
             alpha_blend: Optional[float] = None) -> PlotAxImg:
-        """Update dislayed image settings.
+        """Update displayed image settings.
 
         Args:
             imgi: Index of image group.
sanderslab/magellanmapper

commit 458da312667c9303a14876fee0a7563560b86fe4

Disconnect Plot Editor listeners when closing the ROI and Atlas Editors
Add a mixin function to disconnect all the listeners in all Plot Editors attached to a given ROI or Atlas Editor.

magmap/plot/plot_support.py
@@ -194,6 +194,15 @@ class ImageSyncMixin:
             if n != ed.max_intens_proj:
                 ed.max_intens_proj = n
                 if display: ed.update_coord()
+    
+    def on_close(self):
+        """Figure close handler.
+        
+        Disconnects all Plot Editors.
+
+        """
+        for plot_ed in self.plot_eds.values():
+            plot_ed.disconnect()
 
 
 class ImageOverlayer:

magmap/gui/atlas_editor.py
@@ -243,6 +243,7 @@ class AtlasEditor(plot_support.ImageSyncMixin):
             evt (:obj:`matplotlib.backend_bases.CloseEvent`): Close event.
 
         """
+        self.on_close()
         self.fn_close_listener(evt, self)
 
     def on_key_press(self, event):

magmap/gui/roi_editor.py
@@ -833,6 +833,7 @@ class ROIEditor(plot_support.ImageSyncMixin):
         fig.canvas.mpl_connect("scroll_event", scroll_overview)
         fig.canvas.mpl_connect("key_press_event", key_press)
         fig.canvas.mpl_connect("button_release_event", key_press)
+        fig.canvas.mpl_connect("close_event", self.on_close)
         # fig.canvas.mpl_connect("draw_event", lambda x: print("redraw"))
         
         if self.fn_redraw:

commit 9581125092eda447ab4d7896f380167e5a9da0ed

Fix missing border around zoomed plot in ROI Editor
Commit 458da312667c9303a14876fee0a7563560b86fe4 introduced a regression where the zoomed plot border that indicates the plot corresponding to the overview plots is missing because the plot framed is turned off. Add a parameter to toggle the frame, defaulting to turn it back on.

magmap/plot/plot_support.py
@@ -972,12 +972,18 @@ def scroll_plane(event, z_overview, max_size, jump=None, max_scroll=None):
     return z_overview_new
 
 
-def hide_axes(ax):
+def hide_axes(ax: "axes.Axes", frame_off: bool = False):
     """Hides x- and y-axes and the axes frame.
+    
+    Args:
+        ax: Plot axes.
+        frame_off: True to turn off the frame; defaults to False.
+    
     """
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    ax.set_frame_on(False)
+    if frame_off:
+        ax.set_frame_on(False)
 
 
 def scale_axes(ax, scale_x=None, scale_y=None):
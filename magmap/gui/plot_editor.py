sanderslab/magellanmapper

commit 0d6c4599df6c6051359138eafc11b705dff6a794

Support intersectional alpha blending in `PlotEditor`
Add parameters to specify alpha blending for overlapping parts of images.

magmap/gui/plot_editor.py
@@ -7,7 +7,7 @@ view of orthogonal planes.
 """
 
 import textwrap
-from typing import Optional, TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 
 from matplotlib import patches
 import numpy as np
@@ -39,6 +39,8 @@ class PlotAxImg:
         brightness: Brightness addend; defaults to 0.0.
         contrast: Contrast factor; defaults to 1.0.
         alpha: Opacity level; defaults to None.
+        alpha_blend: Opacity level of the first level in the area of blending
+            between two images; defaults to None.
         img: The original underlying image data,
             copied to allow adjusting the array in ``ax_img`` while
             retaining the original data.
@@ -57,6 +59,7 @@ class PlotAxImg:
         self.brightness: float = 0.0
         self.contrast: float = 1.0
         self.alpha: Optional[float] = None
+        self.alpha_blend: Optional[float] = None
         
         # original underlying image data
         self.img: np.ndarray = np.copy(self.ax_img.get_array())
@@ -393,6 +396,7 @@ class PlotEditor:
         self._channels = [config.channel]
         cmaps = [config.cmaps]
         alphas = [config.alphas[0]]
+        alpha_blends = [None]
         shapes = [self._img3d_shapes[0][1:3]]
         vmaxs = [None]
         vmins = [None]
@@ -408,6 +412,7 @@ class PlotEditor:
             
             # use opacity, brightness, anc contrast from prior images
             alphas[0] = [p.alpha for p in self._plot_ax_imgs[0]]
+            alpha_blends[0] = [p.alpha_blend for p in self._plot_ax_imgs[0]]
             brightnesses[0] = [p.brightness for p in self._plot_ax_imgs[0]]
             contrasts[0] = [p.contrast for p in self._plot_ax_imgs[0]]
         
@@ -419,6 +424,7 @@ class PlotEditor:
             alphas.append(
                 self._ax_img_labels.get_alpha() if self._ax_img_labels
                 else self.alpha)
+            alpha_blends.append(None)
             shapes.append(self._img3d_shapes[1][1:3])
             vmaxs.append(None)
             vmins.append(None)
@@ -441,6 +447,7 @@ class PlotEditor:
                 alpha = (ax_img[i].alpha if ax_img else
                          libmag.get_if_within(config.alphas, 2 + i, 1))
                 alphas.append(alpha)
+                alpha_blends.append(None)
                 
                 shapes.append(self._img3d_shapes[2][1:3])
                 vmaxs.append(None)
@@ -464,7 +471,8 @@ class PlotEditor:
         # reasons
         ax_imgs = plot_support.overlay_images(
             self.axes, self.aspect, self.origin, imgs2d, self._channels, cmaps,
-            alphas, vmins, vmaxs, check_single=(self._ax_img_labels is None))
+            alphas, vmins, vmaxs, check_single=(self._ax_img_labels is None),
+            alpha_blends=alpha_blends)
         if colorbar:
             self.axes.figure.colorbar(ax_imgs[0][0], ax=self.axes)
         self.axes.format_coord = pixel_display.PixelDisplay(
@@ -497,6 +505,9 @@ class PlotEditor:
                     self.change_brightness_contrast(
                         plot_ax_img, libmag.get_if_within(brightnesses[i], j),
                         libmag.get_if_within(contrasts[i], j))
+                plot_ax_img.alpha = libmag.get_if_within(alphas[i], j)
+                plot_ax_img.alpha_blend = libmag.get_if_within(
+                    alpha_blends[i], j)
                 plot_ax_imgs.append(plot_ax_img)
             self._plot_ax_imgs.append(plot_ax_imgs)
         
@@ -694,22 +705,26 @@ class PlotEditor:
             plot_ax_img.contrast = contrast
 
     @staticmethod
-    def update_plot_ax_img_display(plot_ax_img, minimum=np.nan, maximum=np.nan,
-                                   brightness=None, contrast=None, alpha=None):
+    def update_plot_ax_img_display(
+            plot_ax_img: PlotAxImg, minimum: float = np.nan,
+            maximum: float = np.nan, brightness: Optional[float] = None,
+            contrast: Optional[float] = None, alpha: Optional[float] = None,
+            **kwargs) -> Optional[PlotAxImg]:
         """Update plotted image display settings.
 
         Args:
-            plot_ax_img (:obj:`PlotAxImg`): Plotted image.
-            minimum (float): Vmin; can be None for auto setting; defaults
+            plot_ax_img: Plotted image.
+            minimum: Vmin; can be None for auto setting; defaults
                 to ``np.nan`` to ignore.
-            maximum (float): Vmax; can be None for auto setting; defaults
+            maximum: Vmax; can be None for auto setting; defaults
                 to ``np.nan`` to ignore.
-            brightness (float): Brightness addend; defaults to None.
-            contrast (float): Contrast multiplier; defaults to None.
-            alpha (float): Opacity value; defalts to None.
+            brightness: Brightness addend; defaults to None.
+            contrast: Contrast multiplier; defaults to None.
+            alpha: Opacity value; defalts to None.
+            **kwargs: Extra arguments, currently ignored.
         
         Returns:
-            :obj:`PlotAxImg`: The updated axes image plot.
+            The updated axes image plot.
 
         """
         if not plot_ax_img:
@@ -755,29 +770,77 @@ class PlotEditor:
             plot_ax_img.ax_img.set_alpha(alpha)
         return plot_ax_img
 
-    def update_img_display(self, imgi, chl=None, minimum=np.nan,
-                           maximum=np.nan, brightness=None, contrast=None,
-                           alpha=None):
+    def update_alpha_blend(
+            self, imgi: int, alpha_blend: float) -> List[PlotAxImg]:
+        """Update alpha blending between two images.
+
+        Args:
+            imgi: Index of image group. The first two images in this group
+                will be blended.
+            alpha_blend: Alpha opacity level for the blending.
+
+        Returns:
+            List of the updated axes image storage instances.
+
+        """
+        # get the first two channels in the image group
+        plot_ax_imgs = [self.get_displayed_img(imgi, c) for c in (0, 1)]
+        if None in plot_ax_imgs:
+            return plot_ax_imgs
+        
+        # blend the images
+        alpha1, alpha2 = plot_support.alpha_blend_intersection(
+            plot_ax_imgs[0].img, plot_ax_imgs[1].img, alpha_blend)
+        plot_ax_imgs[0].ax_img.set_alpha(alpha1)
+        plot_ax_imgs[1].ax_img.set_alpha(alpha2)
+        
+        # store the alpha blend level while retaining the original stored alpha
+        for p in plot_ax_imgs: p.alpha_blend = alpha_blend
+        return plot_ax_imgs
+
+    def update_img_display(
+            self, imgi: int, chl: Optional[int] = None, minimum: float = np.nan,
+            maximum: float = np.nan, brightness: Optional[float] = None,
+            contrast: Optional[float] = None, alpha: Optional[float] = None,
+            alpha_blend: Optional[float] = None) -> PlotAxImg:
         """Update dislayed image settings.
 
         Args:
-            imgi (int): Index of image.
-            chl (int): Index of channel; defaults to None.
-            minimum (float): Vmin; can be None for auto setting; defaults
+            imgi: Index of image group.
+            chl: Index of channel within the group; defaults to None.
+            minimum: Vmin; can be None for auto setting; defaults
                 to ``np.nan`` to ignore.
-            maximum (float): Vmax; can be None for auto setting; defaults
+            maximum: Vmax; can be None for auto setting; defaults
                 to ``np.nan`` to ignore.
-            brightness (float): Brightness addend; defaults to None.
-            contrast (float): Contrast multiplier; defaults to None.
-            alpha (float): Opacity value; defalts to None.
+            brightness: Brightness addend; defaults to None.
+            contrast: Contrast multiplier; defaults to None.
+            alpha: Opacity value; defaults to None.
+            alpha_blend: Opacity blending value; defaults to None. False
+                turns off alpha blending, resetting the images to their
+                stored alpha values.
         
         Returns:
-            :obj:`PlotAxImg`: The updated axes image plot.
+            The updated axes image plot.
 
         """
-        plot_ax_img = self.get_displayed_img(imgi, chl)
-        plot_ax_img = self.update_plot_ax_img_display(
-            plot_ax_img, minimum, maximum, brightness, contrast, alpha)
+        if alpha_blend is False:
+            # turn off alpha blending, assumed to be applied to first 2
+            # images, and reset them to stored alpha values
+            for c in (0, 1):
+                plot_ax_img = self.get_displayed_img(imgi, c)
+                if plot_ax_img is not None:
+                    plot_ax_img.alpha_blend = None
+                    plot_ax_img = self.update_img_display(
+                        imgi, c, minimum, maximum, brightness, contrast,
+                        plot_ax_img.alpha)
+        elif alpha_blend:
+            # alpha blend the images
+            plot_ax_img = self.update_alpha_blend(imgi, alpha_blend)[0]
+        else:
+            # update the selected image
+            plot_ax_img = self.get_displayed_img(imgi, chl)
+            plot_ax_img = self.update_plot_ax_img_display(
+                plot_ax_img, minimum, maximum, brightness, contrast, alpha)
         self.axes.figure.canvas.draw_idle()
         return plot_ax_img
     

commit ddfa8093c95b7ffc2795ff225eb6a757e53de1a5

Fix losing default opacity in displayed images
Commit 0d6c4599df6c6051359138eafc11b705dff6a794 introduced a regression where the opacity generated from the image overlayer when a default alpha is given is overridden, instead making all images fully opaque. Fix by getting the alpha value from the displayed image if the supplied alpha value is default, which gets the alpha set by the overlayer.

magmap/gui/plot_editor.py
@@ -501,6 +501,7 @@ class PlotEditor:
         self._channels = [config.channel]
         cmaps = [config.cmaps]
         alphas = [config.alphas[0]]
+        alpha_is_default = True
         alpha_blends = [None]
         shapes = [self._img3d_shapes[0][1:3]]
         vmaxs = [None]
@@ -517,6 +518,7 @@ class PlotEditor:
             
             # use opacity, brightness, anc contrast from prior images
             alphas[0] = [p.alpha for p in self._plot_ax_imgs[0]]
+            alpha_is_default = False
             alpha_blends[0] = [p.alpha_blend for p in self._plot_ax_imgs[0]]
             brightnesses[0] = [p.brightness for p in self._plot_ax_imgs[0]]
             contrasts[0] = [p.contrast for p in self._plot_ax_imgs[0]]
@@ -630,8 +632,13 @@ class PlotEditor:
                         plot_ax_img, libmag.get_if_within(brightnesses[i], j),
                         libmag.get_if_within(contrasts[i], j))
                 
+                # get alpha from image if default since image overlayer scales
+                # the default alpha for each channel
+                plot_ax_img.alpha = (
+                    plot_ax_img.ax_img.get_alpha() if alpha_is_default
+                    else libmag.get_if_within(alphas[i], j))
+                
                 # store rest of settings
-                plot_ax_img.alpha = libmag.get_if_within(alphas[i], j)
                 plot_ax_img.alpha_blend = libmag.get_if_within(
                     alpha_blends[i], j)
                 plot_ax_img.rgb = self.overlayer.rgb
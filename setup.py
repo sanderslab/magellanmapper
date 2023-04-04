eacf7b7ed14406bd13c26ad84e415500165d43c8
David Young <david@textflex.com>
2023-04-04 10:41:28 +0800

    Bump project version to 1.6b1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ config = {
     "url": "https://github.com/sanderslab/magellanmapper",
     "author_email": "david@textflex.com",
     "license": "BSD-3",
-    "version": "1.6a3",
+    "version": "1.6b1",
     "packages": setuptools.find_packages(),
     "scripts": [],
     "python_requires": ">=3.6",

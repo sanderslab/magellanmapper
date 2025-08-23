bbff6874ed55f16fa065abc7ae6c671953cd89a9
David Young <david@textflex.com>
2025-08-22 18:20:21 -0700

    Add unit tests for reading TIF, writing to NPY, and detecting blobs

--- magmap/tests/unit_testing.py
+++ magmap/tests/unit_testing.py
@@ -6,21 +6,42 @@
 import unittest
 
 from magmap.cv import stack_detect
-from magmap.io import cli
-from magmap.io import importer
+from magmap.io import cli, importer, np_io
 from magmap.settings import config
 
-TEST_IMG = "test.czi"
+TEST_IMG_CZI = "test.czi"
+TEST_IMG_BASE = "sample_region"
+TEST_IMG_TIFF = f"{TEST_IMG_BASE}.tif"
 
 
 class TestImageStackProcessing(unittest.TestCase):
     
     def setUp(self):
-        config.filename = TEST_IMG
+        config.filename = TEST_IMG_TIFF
         config.channel = None
         cli.setup_roi_profiles(["lightsheet,4xnuc"])
     
-    def test_load_image(self):
+    def test_read_tif_(self):
+        img5d = np_io.read_tif(TEST_IMG_TIFF)
+        config.img5d = img5d
+        assert(img5d is not None)
+        assert(img5d.img is not None)
+        assert(img5d.meta is not None)
+        config.resolutions = img5d.meta[config.MetaKeys.RESOLUTIONS]
+        self.assertEqual(img5d.img.shape, (1, 51, 200, 200, 2))
+    
+    def test_write_npy(self):
+        np_io.write_npy(config.img5d.img, config.img5d.meta, TEST_IMG_TIFF)
+    
+    def test_load_npy_image(self):
+        img5d = importer.read_file(TEST_IMG_BASE)
+        config.img5d = img5d
+        assert(img5d is not None)
+        assert(img5d.img is not None)
+        self.assertEqual(img5d.img.shape, (1, 51, 200, 200, 2))
+    
+    @unittest.skip("CZI files not yet supported in this test")
+    def test_import_npy_image(self):
         img5d = importer.read_file(
             config.filename, config.series)
         if img5d.img is None:
@@ -41,7 +62,7 @@ class TestImageStackProcessing(unittest.TestCase):
         _, _, blobs = stack_detect.detect_blobs_blocks(
             config.filename, img5d.img, (30, 30, 8), (70, 70, 10),
             config.channel)
-        self.assertEqual(len(blobs), 54)
+        self.assertEqual(len(blobs.blobs), 42)
 
 
 if __name__ == "__main__":

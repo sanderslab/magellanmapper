26094f0d6ab416458970cd9bdcfdb1ccc3d62ea2
David Young <david@textflex.com>
2025-09-11 13:57:55 -0700

    Import TIF files through the `tifffile` reader instead of Bioformats
    - Load TIF files through the dedicate TIF reader instead of using Bioformats in the import pathway
    - Implement fix for saving files in Numpy 2
    - Allow running the import pathway when Javabridge cannot be loaded and remove warning

--- magmap/gui/visualizer.py
+++ magmap/gui/visualizer.py
@@ -4057,7 +4057,7 @@ class Visualization(HasTraits):
             # extracted values
             res = md[config.MetaKeys.RESOLUTIONS]
             if res is not None:
-                self._import_res = [res[::-1]]
+                self._import_res = res[:, ::-1]
             mag = md[config.MetaKeys.MAGNIFICATION]
             if mag is not None:
                 self._import_mag = mag
